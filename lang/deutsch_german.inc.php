37a1a944548851878e0f721b6c95fadf05f13dd7
Heart1010 <Heart1010@gmail.com>
2006-10-13 07:06:53 +0000

    corrected some parts of german language file

--- lang/deutsch_german.inc.php
+++ lang/deutsch_german.inc.php
@@ -6,46 +6,46 @@ $wording_for_enabledField ="Aktiviert";
 $wording_for_disabledField ="Deaktiviert";
 
 #Index.php - front page
-$indx_welcome ="Welcome to ";
+$indx_welcome ="Willkommen zur ";
 
-$indx_shortcut ="Shortcut menu";
+$indx_shortcut ="Schnellzugriffsmen&uuml;";
 
-$indx_getting_started ="Getting started";
-$indx_faqs_what ="What is Simple Invoices?";
-$indx_faqs_need ="What do I need to start invoicing?";
-$indx_faqs_how ="How do I create invoices?";
-$indx_faqs_type ="How are the different types of invoices?";
+$indx_getting_started ="Einstieg";
+$indx_faqs_what ="Was ist \"Simple Invoices\"?";
+$indx_faqs_need ="Was wird ben&ouml;tigt, um mit der Rechnungserstellung starten zu k&ouml;nnen?";
+$indx_faqs_how ="Wie erstellt man Rechnungen?";
+$indx_faqs_type ="Was ist der Unterschied zwischen den verschiedenen Rechnungstypen?";
 
-$indx_create_invoice ="Create an invoice";
-$indx_invoice_total ="Total";
-$indx_invoice_itemised ="Itemised";
-$indx_invoice_consulting ="Consulting?";
+$indx_create_invoice ="Rechnung erstellen";
+$indx_invoice_total ="Freitext";
+$indx_invoice_itemised ="Artikelauflistung";
+$indx_invoice_consulting ="Detailierte Artikelauflistung";
 
-$indx_manage_existing_invoice ="Manage your existing invoices";
-$indx_manage_invoice ="Manage invoices";
+$indx_manage_existing_invoice ="Bestehende Rechnungen verwalten";
+$indx_manage_invoices ="Rechnungen verwalten";
 
-$indx_manage_data ="Manage your data";
-$indx_insert_customer = "Add Customer";
-$indx_insert_biller = "Add Biller";
-$indx_insert_product = "Add Product";
+$indx_manage_data ="Meine Daten verwalten";
+$indx_insert_customer = "Kunden verwalten";
+$indx_insert_biller = "Gl&auml;ubiger verwalten";
+$indx_insert_product = "Produkte verwalten";
 
-$indx_options ="Options";
-$indx_options_sys_defaults ="System Defaults";
-$indx_options_tax_rates ="Tax Rates";
-$indx_options_inv_pref ="Invoice Preferences";
-$indx_options_payment_types ="Payment Types";
-$indx_options_upgrade ="Database Upgrade Manager";
-$indx_options_backup ="Backup Database";
+$indx_options ="Einstellungen";
+$indx_options_sys_defaults ="System-Voreinstellungen";
+$indx_options_tax_rates ="Steuers&auml;tze";
+$indx_options_inv_pref ="Rechnungseinstellungen";
+$indx_options_payment_types ="Zahlungsarten";
+$indx_options_upgrade ="Datenbank Upgrade Manager";
+$indx_options_backup ="Datenbank sichern";
 
-$indx_help ="Help!!!";
+$indx_help ="Hilfe!!!";
 $indx_help_install ="Installation";
-$indx_help_upgrade ="Upgrading Simple Invoices";
-$indx_help_prepare ="Prepare Simple Invoices for use";
+$indx_help_upgrade ="Aktualisiere Simple Invoices";
+$indx_help_prepare ="Simple Invoices gebrauchsf&auml;hig machen";
 
-$indx_stats =" Quick stats";
-$indx_stats_debtor ="Largest debtor";
-$indx_stats_customer ="Top Customer - by amount invoiced";
-$indx_stats_biller ="Top Biller - by amount invoiced";
+$indx_stats =" Kurzstatistiken";
+$indx_stats_debtor ="Gr&ouml;&szlig;ter Schuldner";
+$indx_stats_customer ="Top Kunde - nach Rechnungssumme";
+$indx_stats_biller ="Top Gl&auml;ubiger - nach Rechnungssumme";
 
 #Manage Invoices
 $mi_page_title = " - Rechnungen verwalten";
@@ -53,7 +53,7 @@ $mi_page_header = "Rechnungen verwalten";
 $mi_no_invoices = "Es sind keine Rechnungen in der Datenbank vorhanden";
 $mi_table_action = "Aktion";
 $mi_table_id = "Re-Nr.";
-$mi_table_biller = "Glaeubiger";
+$mi_table_biller = "Gl&auml;ubiger";
 $mi_table_customer = "Kunde";
 $mi_table_total = "Total";
 $mi_table_paid = "Bezahlt";
@@ -61,19 +61,19 @@ $mi_table_owing = "Offen";
 $mi_table_type = "Zahlungsart";
 $mi_table_date = "Re-Datum";
 $mi_actions_quick_view = "Anzeigen";
-$mi_actions_quick_view_tooltip = "Schnellanzeige von"; 
+$mi_actions_quick_view_tooltip = "Schnellanzeige von";
 $mi_actions_edit_view = "Bearbeiten";
 $mi_actions_edit_view_toolkit = "bearbeiten";
 $mi_actions_print_preview_tooltip = "Druckvorschau der";
-$mi_actions_export_tooltip = "Exportiere"; 
+$mi_actions_export_tooltip = "Exportiere";
 $mi_actions_export_pdf_tooltip = "im PDF-Format";
 $mi_actions_format_tooltip = "Format";
 $mi_actions_export_xls_tooltip = "zu einem Tabellendokument im";
 $mi_actions_export_doc_tooltip = "zu einem Textdokument im";
-$mi_actions_process_payment = "Verbuche einen Zahlungseingang fuer";
-$mi_action_invoice_total = "Neue Rechnung - Total";
-$mi_action_invoice_itemised = "Neue Rechnung - Einzeln";
-$mi_action_invoice_consulting = "Neue Rechnung - Einzeln+";
+$mi_actions_process_payment = "Verbuche einen Zahlungseingang f&uuml;r";
+$mi_action_invoice_total = "Neue Rechnung - Freitext";
+$mi_action_invoice_itemised = "Neue Rechnung - Artikelaufl.";
+$mi_action_invoice_consulting = "Neue Rechnung - Detail. Artikelaufl.";
 
 
 #Manage Products
@@ -83,24 +83,24 @@ $mp_no_invoices = "Es sind keine Produkte in der Datenbank vorhanden";
 $mp_table_action = "Aktion";
 $mp_table_product_id = "Produkt ID";
 $mp_table_product_desc = "Produktbeschreibung";
-$mp_table_unit_price = "Stueckpreis";
+$mp_table_unit_price = "St&uuml;ckpreis";
 $mp_actions_view = "Anzeigen";
 $mp_actions_edit = "Bearbeiten";
 $mp_actions_new_product = "Neues Produkt anlegen";
 
 #Manage Billers
-$mb_page_title = " - Glaeubiger verwalten";
-$mb_page_header = "Glaeubiger verwalten";
-$mb_no_invoices = "Es sind keine Glaeubiger in der Datenbank vorhanden";
+$mb_page_title = " - Gl&auml;ubiger verwalten";
+$mb_page_header = "Gl&auml;ubiger verwalten";
+$mb_no_invoices = "Es sind keine Gl&auml;ubiger in der Datenbank vorhanden";
 $mb_table_action = "Aktion";
-$mb_table_biller_id = "Glaeubiger ID";
-$mb_table_biller_name = "Name des Glaeubigers";
+$mb_table_biller_id = "Gl&auml;ubiger ID";
+$mb_table_biller_name = "Name des Gl&auml;ubigers";
 $mb_table_phone = "Telefon-Nr.";
 $mb_table_mobile_phone = "Handy-Nr.";
 $mb_table_email = "Email";
 $mb_actions_view = "Anzeigen";
 $mb_actions_edit = "Bearbeiten";
-$mb_actions_new_biller = "Neuen Glaeubiger anlegen";
+$mb_actions_new_biller = "Neuen Gl&auml;ubiger anlegen";
 
 #Manage Customers
 $mc_page_title = " - Kunden verwalten";
@@ -131,7 +131,7 @@ $mip_actions_new_preference = "Neue Rechnungsborgabe anlegen";
 $msd_default_number_items = "Standard Anzahl an Zeilennummern:";
 $msd_js_alert_def_inv_template = "Standard Rechnungs Template";
 $msd_def_inv_template = "Standard Rechnungs Template ";
-$msd_no_tax = "Sorry, keine Steuersaetze vorhanden. Bitte anlegen!";
+$msd_no_tax = "Sorry, keine Steuers&auml;tze vorhanden. Bitte anlegen!";
 $msd_no_payment_type = "Sorry, kein Zahlungseingangs-Typ vorhanden. Bitte anlegen!";
 $msd_tax = "Steuersatz";
 $msd_payment_type = "Zahlungseingang";
@@ -143,13 +143,13 @@ $msd_submit_button  = "System Voreinstellungen abschicken";
 
 
 #Manage Account Payments
-$map_page_title = " - Zahlungseingaenge verwalten";
-$map_page_header = "Zahlungseingaenge verwalten";
-$map_no_invoices = "Es sind keine Zahlungseingaenge in der Datenbank vorhanden";
+$map_page_title = " - Zahlungseing&auml;nge verwalten";
+$map_page_header = "Zahlungseing&auml;nge verwalten";
+$map_no_invoices = "Es sind keine Zahlungseing&auml;nge in der Datenbank vorhanden";
 $map_table_action = "Aktion";
-$map_table_payment_id = "Zahlungseingang ID";
+$map_table_payment_id = "Zahlungseingangs ID";
 $map_table_payment_invoice_id = "Re-Nr.";
-$map_table_biller = "Glaeubiger";
+$map_table_biller = "Gl&auml;ubiger";
 $map_table_customer = "Kunde";
 $map_table_amount = "Betrag";
 $map_table_notes = "Bemerkungen";
@@ -157,14 +157,14 @@ $map_table_payment_type = "Zahlungseingang";
 $map_table_date = "Datum";
 $map_actions_view = "Anzeigen";
 $map_actions_process_payment = "Zahlungseingang verbuchen";
-$map_payments_filtered = "Zahlungseingaenge gefiltert nach Re-Nr.";
-$map_payments_filtered_invoice = "Zahlungseingang fuer diese Rechnung verbuchen";
-$map_payments_filtered_customer = "Zahlungseingaenge gefiltert nach Kunden-Nr.";
+$map_payments_filtered = "Zahlungseing&auml;nge gefiltert nach Re-Nr.";
+$map_payments_filtered_invoice = "Zahlungseingang f&uuml;r diese Rechnung verbuchen";
+$map_payments_filtered_customer = "Zahlungseing&auml;nge gefiltert nach Kunden-Nr.";
 
 #Manage Tax Rate
-$mtr_page_title = " - Steuersaetze verwalten";
-$mtr_page_header = "Steuersaetze verwalten";
-$mtr_no_invoices = "Es sind keine Steuersaetze in der Datenbank vorhanden";
+$mtr_page_title = " - Steuers&auml;tze verwalten";
+$mtr_page_header = "Steuers&auml;tze verwalten";
+$mtr_no_invoices = "Es sind keine Steuers&auml;tze in der Datenbank vorhanden";
 $mtr_table_action = "Aktion";
 $mtr_table_tax_id = "Steuersatz ID";
 $mtr_table_tac_desc = "Beschreibung des Steuersatzes";
