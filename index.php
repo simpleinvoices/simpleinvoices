4c0273d40026f87be13f1138818191c0e4ecdcf4
Justin Kelly <justin@kelly.org.au>
2006-11-01 11:02:21 +0000

    index page work

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include('./include/include_main.php'); 
 #include('./config/config.php'); 
-#include("./lang/$language.inc.php");
+include("./lang/$language.inc.php");
 #include('./include/menu.php');
 
 $conn = mysql_connect( $db_host, $db_user, $db_password );
@@ -81,6 +81,123 @@ while ($Array3 = mysql_fetch_array($result3)) {
 };
 #Top biller query - start
 
+$display_block ="
+
+                <div id=\"list1\">
+                <h2><img src=\"./images/reports.png\"></img>$LANG_stats</h2>
+                        <div id=\"item11\">
+
+                                <div class=\"title\">$LANG_stats_debtor</div>
+
+                                <div class=\"content\">
+			
+				$largest_debtor
+                                </div>
+                        </div>
+
+                        <div id=\"item12\">
+
+                                <div class=\"title\">$LANG_stats_customer</div>
+
+                                <div class=\"content\">
+
+				$top_customer
+
+                                </div>
+
+                        </div>
+
+                        <div id=\"item13\">
+
+                                <div class=\"title\">$LANG_stats_biller</div>
+
+                                <div class=\"content\">
+
+				$top_biller
+
+                                </div>
+
+                        </div>
+                </div>
+
+
+               <div id=\"list2\">
+
+                <h2><img src=\"./images/menu.png\">$LANG_shortcut</h2>
+
+                        <div id=\"item21\">
+                                <div class=\"mytitle\">$LANG_getting_started</div>
+                                <div class=\"mycontent\">
+                                        <a href=\"./inline_instructions.php#faqs-what\">$LANG_faqs_what</a><br/>
+                                        <a href=\"./inline_instructions.php#faqs-need\">$LANG_faqs_need</a><br/>
+                                        <a href=\"inline_instructions.php#faqs-how\">$LANG_faqs_how</a><br/>
+                                        <a href=\"inline_instructions.php#faqs-types\">$LANG_faqs_type</a>
+                                </div>
+                        </div>
+
+                        <div id=\"item22\">
+                                <div class=\"mytitle\">$LANG_create_invoice</div>
+                                <div class=\"mycontent\">
+					<table>
+					<tr>
+						<td width=10%>
+							<img src=\"manage.png\"></img>
+				                         <a href=\"invoice_itemised.php\">$LANG_itemised</a><br>soem stuff<bt>more stuff
+                                		</td>		
+						<td width=10%>
+							<img src=\"manage.png\"></img>
+				        		<a href=\"invoice_total.php\">$LANG_total</a>
+						</td>
+						<td width=10%>
+							<img src=\"manage.png\"></img>
+		                                        <a href=\"invoice_consulting.php\">$LANG_consulting</a>
+                				</td>
+					<tr>
+					</table>
+		                </div>
+                        </div>
+                        <div id=\"item23\">
+                                <div class=\"mytitle\">$LANG_manage_existing_invoice</div>
+                                <div class=\"mycontent\">
+							<img src=\"manage.png\"></img>
+                                        <a href=\"manage_invoices.php\">$LANG_manage_invoices</a>
+                                </div>
+                        </div>
+
+                        <div id=\"item24\">
+                                <div class=\"mytitle\">$LANG_manage_data</div>
+                                <div class=\"mycontent\">
+                                        <a href=\"insert_biller.php\">$indx_insert_biller</a><br/>
+                                        <a href=\"manage_biller.php\">Manage Billers</a><br/>
+                                        <a href=\"insert_customer.php\">$indx_insert_customer</a><br/>
+                                        <a href=\"manage_customers.php\">Manage Customers</a><br/>
+                                        <a href=\"insert_product.php\">$indx_insert_product</a><br/>
+                                        <a href=\"manage_products.php\">Manage Products</a><br/>
+                                </div>
+                        </div>
+                        <div id=\"item25\">
+                                <div class=\"mytitle\">$indx_options</div>
+                                <div class=\"mycontent\">
+                                        <a href=\"manage_system_defaults.php\">$indx_options_sys_defaults</a><br/>
+                                        <a href=\"manage_tax_rates.php\">$indx_options_tax_rates</a><br/>
+                                        <a href=\"manage_preferences.php\">$indx_options_inv_pref</a><br/>
+                                        <a href=\"manage_payment_types.php\">$indx_options_payment_types</a><br/>
+                                        <a href=\"database_sqlpatches.php\">$indx_options_upgrade</a> <br/>
+                                        <a href=\"backup_database.php\">$indx_options_backup</a>
+                                </div>
+                        </div>
+                        <div id=\"item26\">
+                                <div class=\"mytitle\">$indx_help</div>
+                                <div class=\"mycontent\">
+                                        <a href=\"inline_instructions.php#installation\">$indx_help_install<br/></a>
+                                        <a href=\"inline_instructions.php#upgrading\">$indx_help_upgrade<br/></a>
+                                        <a href=\"inline_instructions.php#prepare\">$indx_help_prepare<br/></a>
+                                </div>
+                        </div>
+                        </div>
+
+";
+
 
 ?>
 
@@ -89,6 +206,14 @@ while ($Array3 = mysql_fetch_array($result3)) {
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
                 <title><?php echo $title; ?></title>
+<script type="text/javascript" src="niftycube.js"></script>
+<script type="text/javascript">
+window.onload=function(){
+Nifty("div#container");
+Nifty("div#content,div#nav","same-height small");
+Nifty("div#header,div#footer","small");
+}
+</script>
 
                 <script type="text/javascript" src="./include/jquery.js"></script>
                 <script type="text/javascript" src="./include/jquery-accordian.js"></script>
@@ -101,13 +226,15 @@ while ($Array3 = mysql_fetch_array($result3)) {
                         .title { cursor:pointer; border:1px solid #CCCCCC; margin-top:0.5em; padding:0.1em; }
                         .on1  .title { background-color:#E4EFC7; }
                         .off1 .title { background-color:#E0E0E0; }
-                        .content    { background-color:#F5F5F5; padding:0.1em; border:1px solid #C0C0C0; border-top-width:0; }
+                        /*.content    { background-color:#F5F5F5; padding:0.1em; border:1px solid #C0C0C0; border-top-width:0; }*/
+                        .content    { background-color:#FFF; padding:0.1em; border:1px solid #C0C0C0; border-top-width:0; }
 
                         #list2 { width:48%; position:absolute; top:15%; left:1em; }
                         .mytitle { cursor:pointer; border:1px solid #CCCCCC; margin-top:0.5em; padding:0.1em; }
                         .on  .mytitle { background-color:#E4EFC7; }
                         .off .mytitle { background-color:#E0E0E0; }
-                        .mycontent    { background-color:#F5F5F5; padding:0.1em; border:1px solid #C0C0C0; border-top-width:0; }
+                        /*.mycontent    { background-color:#F5F5F5; padding:0.1em; border:1px solid #C0C0C0; border-top-width:0; }*/
+                        .mycontent    { background-color:#FFF; padding:0.1em; border:1px solid #C0C0C0; border-top-width:0; }
 
 			a:link {  text-decoration: none; }
 			a:visited { text-decoration: none; }
@@ -116,6 +243,7 @@ while ($Array3 = mysql_fetch_array($result3)) {
 
 			/*The CSS code for the fronta page of Simple Invoices - end*/
                 </style>
+<link rel="stylesheet" type="text/css" href="themes/<?php echo $theme; ?>/tables.css">
 
 </head>
 <BODY>
@@ -125,108 +253,43 @@ $mid->printMenu('hormenu1');
 $mid->printFooter();
 ?>
 
+
 <br>
 
+<div id="container">
+<div id=header>
+<!--                <h1 align=center><?php echo $LANG_welcome; echo $title; ?></h1>-->
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php echo $display_block; ?>
+<div id="footer"></div>
 
-                <h1 align=center><?php echo $LANG_welcome; echo $title; ?></h1>
-                <div id="list1">
-                <h2><img src="./images/reports.png"></img><?php echo $LANG_stats; ?></h2>
-                        <div id="item11">
-
-                                <div class="title"><?php echo $LANG_stats_debtor; ?></div>
-
-                                <div class="content">
-			
-				<?php echo $largest_debtor; ?>
-                                </div>
-                        </div>
-
-                        <div id="item12">
-
-                                <div class="title"><?php echo $LANG_stats_customer; ?></div>
-
-                                <div class="content">
-
-				<?php echo $top_customer; ?>
-
-                                </div>
-
-                        </div>
-
-                        <div id="item13">
-
-                                <div class="title"><?php echo $LANG_stats_biller; ?></div>
-
-                                <div class="content">
-
-				<?php echo $top_biller; ?>
-
-                                </div>
-
-                        </div>
                 </div>
 
-
-               <div id="list2">
-
-                <h2><img src="./images/menu.png"> <?php echo $LANG_shortcut; ?></h2>
-
-                        <div id="item21">
-                                <div class="mytitle"><?php echo $LANG_getting_started; ?></div>
-                                <div class="mycontent">
-                                        <a href="./inline_instructions.php#faqs-what"><?php echo $LANG_faqs_what; ?></a><br/>
-                                        <a href="./inline_instructions.php#faqs-need"><?php echo $LANG_faqs_need; ?></a><br/>
-                                        <a href="inline_instructions.php#faqs-how"><?php echo $LANG_faqs_how; ?></a><br/>
-                                        <a href="inline_instructions.php#faqs-types"><?php echo $LANG_faqs_type; ?></a>
-                                </div>
-                        </div>
-
-                        <div id="item22">
-                                <div class="mytitle"><?php echo $LANG_create_invoice; ?></div>
-                                <div class="mycontent">
-                                        <a href="invoice_itemised.php"><?php echo $LANG_itemised; ?></a><br/>
-                                        <a href="invoice_total.php"><?php echo $LANG_total; ?></a><br/>
-                                        <a href="invoice_consulting.php"><?php echo $LANG_consulting; ?></a><br/>
-                                </div>
-                        </div>
-                        <div id="item23">
-                                <div class="mytitle"><?php echo $LANG_manage_existing_invoice; ?></div>
-                                <div class="mycontent">
-                                        <a href="manage_invoices.php"><?php echo $LANG_manage_invoices; ?></a><br/>
-                                </div>
-                        </div>
-
-                        <div id="item24">
-                                <div class="mytitle"><?php echo $LANG_manage_data; ?></div>
-                                <div class="mycontent">
-                                        <a href="insert_biller.php"><?php echo $indx_insert_biller; ?></a><br/>
-                                        <a href="manage_biller.php"><?php echo "Manage Billers" ; ?></a><br/>
-                                        <a href="insert_customer.php"><?php echo $indx_insert_customer; ?></a><br/>
-                                        <a href="manage_customers.php"><?php echo "Manage Customers" ; ?></a><br/>
-                                        <a href="insert_product.php"><?php echo $indx_insert_product; ?></a><br/>
-                                        <a href="manage_products.php"><?php echo "Manage Products" ; ?></a><br/>
-                                </div>
-                        </div>
-                        <div id="item25">
-                                <div class="mytitle"><?php echo $indx_options; ?></div>
-                                <div class="mycontent">
-                                        <a href="manage_system_defaults.php"><?php echo $indx_options_sys_defaults; ?></a><br/>
-                                        <a href="manage_tax_rates.php"><?php echo $indx_options_tax_rates; ?></a><br/>
-                                        <a href="manage_preferences.php"><?php echo $indx_options_inv_pref; ?></a><br/>
-                                        <a href="manage_payment_types.php"><?php echo $indx_options_payment_types; ?></a><br/>
-                                        <a href="database_sqlpatches.php"><?php echo $indx_options_upgrade; ?></a> <br/>
-                                        <a href="backup_database.php"><?php echo $indx_options_backup; ?></a>
-                                </div>
-                        </div>
-                        <div id="item26">
-                                <div class="mytitle"><?php echo $indx_help; ?></div>
-                                <div class="mycontent">
-                                        <a href="inline_instructions.php#installation"><?php echo $indx_help_install; ?><br/></a>
-                                        <a href="inline_instructions.php#upgrading"><?php echo $indx_help_upgrade; ?><br/></a>
-                                        <a href="inline_instructions.php#prepare"><?php echo $indx_help_prepare; ?><br/></a>
-                                </div>
-                        </div>
-                </div>
+</div>
 
 </BODY>
 </HTML>
