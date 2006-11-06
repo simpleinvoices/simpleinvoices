a10040ccebe9a601226e6e5af469a9bb3e7c1382
Justin Kelly <justin@kelly.org.au>
2006-11-06 03:02:58 +0000

    adding galician translation

---
+++ lang/galego_galician.inc.php
@@ -0,0 +1,287 @@
+<?php
+
+#all
+$title = "Factura Simple";
+$wording_for_enabledField ="Activo";
+$wording_for_disabledField ="Desactivado";
+
+#Index.php - front page
+$indx_welcome ="Benvido/a a ";
+
+$indx_shortcut ="Men&uacute; de atallos";
+
+$indx_getting_started ="Comezando";
+$indx_faqs_what ="Que &eacute; Factura Simple?";
+$indx_faqs_need ="Que necesito para comezar a facturar?";
+$indx_faqs_how ="Como crear facturass?";
+$indx_faqs_type ="Como son os distintos tipos de facturas?";
+
+$indx_create_invoice ="Crear unha factura";
+$indx_invoice_total ="Total";
+$indx_invoice_itemised ="Desagregada";
+$indx_invoice_consulting ="An&aacute;lise";
+
+$indx_manage_existing_invoice ="Xesti&oacute;n de facturas existentes";
+$indx_manage_invoices ="Xestionar facturas";
+
+$indx_manage_data ="Xestionar os seus datos";
+$indx_insert_customer = "Engadir Cliente";
+$indx_insert_biller = "Engadir Emisor";
+$indx_insert_product = "Engadir Produto";
+
+$indx_options ="Opci&oacute;ns";
+$indx_options_sys_defaults ="Valores por defecto";
+$indx_options_tax_rates ="Taxa de imposto";
+$indx_options_inv_pref ="Preferencias de Factura";
+$indx_options_payment_types ="Tipos de Aboamento";
+$indx_options_upgrade ="Xestor de actualizaci&oacute;n da base de datos";
+$indx_options_backup ="Copia de seguridade da base de datos";
+
+$indx_help ="Axuda!!";
+$indx_help_install ="Instalaci&oacute;n";
+$indx_help_upgrade ="Actualizar Factura Simple";
+$indx_help_prepare ="Preparar Factura Simple para usar";
+
+$indx_stats =" Estat&iacute;sticas r&aacute;pidas";
+$indx_stats_debtor ="O maior debedor";
+$indx_stats_customer ="Gran Cliente - por cantidade facturada";
+$indx_stats_biller ="Gran Emisor - por cantidade facturada";
+
+#Manage Invoices
+$mi_page_title = " - Xestionar Facturas";
+$mi_page_header = "Xesti&oacute;n de Facturas";
+$mi_no_invoices = "Non hai facturas na base de datos";
+$mi_table_action = "Acci&oacute;n";
+$mi_table_id = "ID";
+$mi_table_biller = "Emisor";
+$mi_table_customer = "Cliente";
+$mi_table_total = "Total";
+$mi_table_paid = "Aboado";
+$mi_table_owing = "Xerado";
+$mi_table_type = "Tipo";
+$mi_table_date = "Data de creaci&oacute;n";
+$mi_actions_quick_view = "ver";
+$mi_actions_quick_view_tooltip = "Vista r&aacute;pida de";
+$mi_actions_edit_view = "editar";
+$mi_actions_edit_view_toolkit = "Editar";
+$mi_actions_print_preview_tooltip = "Imprimir vista previa de";
+$mi_actions_export_tooltip = "Exportar";
+$mi_actions_export_pdf_tooltip = "en formato PDF";
+$mi_actions_format_tooltip = "formato";
+$mi_actions_export_xls_tooltip = "para unha folla de c&aacute;lculo como";
+$mi_actions_export_doc_tooltip = "para un procesador de textos como ";
+$mi_actions_process_payment = "Procesar Aboamento para";
+$mi_action_invoice_total = "Engadir Factura nova - en estilo Total";
+$mi_action_invoice_itemised = "Engadir Factura nova - en estilo Desagregado";
+$mi_action_invoice_consulting = "Engadir Factura nova - en estilo An&aacute;lise";
+
+
+#Manage Products
+$mp_page_title = " - Xestionar Produtos";
+$mp_page_header = "Xesti&oacute;n de Produtos";
+$mp_no_invoices = "Non hai produtos na base de datos, engada alg&uacute;n";
+$mp_table_action = "Acci&oacute;n";
+$mp_table_product_id = "ID de Produto";
+$mp_table_product_desc = "Descrici&oacute;n de produtos";
+$mp_table_unit_price = "Prezo unitario";
+$mp_actions_view = "Vista";
+$mp_actions_edit = "Editar";
+$mp_actions_new_product = "Engadir Novo Produto";
+
+#Manage Billers
+$mb_page_title = " - Xestionar Emisores";
+$mb_page_header = "Xesti&oacute;n de Emisores";
+$mb_no_invoices = "Non hai Emisores na base de datos, engada alg&uacute;n";
+$mb_table_action = "Acci&oacute;n";
+$mb_table_biller_id = "ID de Emisor";
+$mb_table_biller_name = "Nome de Emisor";
+$mb_table_phone = "Telefono";
+$mb_table_mobile_phone = "M&oacute;bil";
+$mb_table_email = "Correo el.";
+$mb_actions_view = "Ver";
+$mb_actions_edit = "Editar";
+$mb_actions_new_biller = "Engadir Novo Emisor";
+
+#Manage Customers
+$mc_page_title = " - Xestionar Clientes";
+$mc_page_header = "Xesti&oacute;n de Clientes";
+$mc_no_invoices = "Non hai clientes na base de datos, engada alg&uacute;n";
+$mc_table_action = "Acci&oacute;n";
+$mc_table_customer_id = "ID de Cliente";
+$mc_table_customer_name = "Nome de Cliente";
+$mc_table_attention = "Avisos";
+$mc_table_phone = "Telefono";
+$mc_table_email = "M&oacute;bil";
+$mc_actions_view = "Ver";
+$mc_actions_edit = "Editar";
+$mc_actions_new_product = "Engadir Novo Cliente";
+
+#Manage Preferences
+$mip_page_title = " - Xestionar Preferencias";
+$mip_page_header = "Xesti&oacute;n de Preferencias";
+$mip_no_invoices = "Non hai preferencias de facturaci&oacute;n na base de datos, engada algunha";
+$mip_table_action = "Acci&oacute;n";
+$mip_table_preference_id = "ID de Preferencias";
+$mip_table_description = "Descrici&oacute;n";
+$mip_actions_view = "Ver";
+$mip_actions_edit = "Editar";
+$mip_actions_new_preference = "Engadir Preferencia de Facturaci&oacute;n";
+
+#Manage System Defautls manage_system_defaults.php
+$msd_default_number_items = "N&uacute;mero por defecto de &iacute;tems por liña:";
+$msd_js_alert_def_inv_template = "Modelo de factura por defecto";
+$msd_def_inv_template = "Modelo de factura por defecto";
+$msd_no_tax = "Desculpe, non hai ningunha taxa dispoñible, engada algunha";
+$msd_no_payment_type = "Desculpe, non hai ning&uacute;n tipo de Aboamento dispoñible, engada alg&uacute;n";
+$msd_tax = "Imposto";
+$msd_payment_type = "Tipo de Aboamento";
+$msd_invoice_preference = "Preferencia de Facturaci&oacute;n";
+$msd_no_defaults = "Non hai valores por defecto";
+$msd_page_title = " - Xesti&oacute;n de Valores por defecto do Sistema";
+$msd_heading = "Valores por defecto do Sistema";
+$msd_submit_button  = "Introducir valores por defecto";
+
+
+#Manage Account Payments
+$map_page_title = " - Xestionar Aboamentos";
+$map_page_header = "Xesti&oacute;n de Aboamentos";
+$map_no_invoices = "Non hai Aboamentos na base de datos";
+$map_table_action = "Acci&oacute;n";
+$map_table_payment_id = "ID de Aboamento";
+$map_table_payment_invoice_id = "Fact. ID";
+$map_table_biller = "Emisor";
+$map_table_customer = "Cliente";
+$map_table_amount = "Cantidade";
+$map_table_notes = "Notas";
+$map_table_payment_type = "Tipo de Aboamento";
+$map_table_date = "Data";
+$map_actions_view = "Ver";
+$map_actions_process_payment = "Procesar Aboamento";
+$map_payments_filtered = "Aboamentos filtrados por ID de Factura";
+$map_payments_filtered_invoice = "Procesar Aboamento para esta Factura";
+$map_payments_filtered_customer = "Aboamento filtrado por ID de Cliente";
+
+#Manage Tax Rate
+$mtr_page_title = " - Xestionar Porcentaxes de Imposto";
+$mtr_page_header = "Xesti&oacute;n de Porcentaxes de Imposto";
+$mtr_no_invoices = "Non hai  Porcentaxes de Taxas na base de datos, engda algunha";
+$mtr_table_action = "Acci&oacute;n";
+$mtr_table_tax_id = "ID de Impostos";
+$mtr_table_tac_desc = "Descrici&oacute;n de Imposto";
+$mtr_table_percentage = "Porcentaxe de Imposto";
+$mtr_actions_view = "Ver";
+$mtr_actions_edit = "Editar";
+$mtr_actions_new_tax = "Engadir Nova Porcentaxe de Imposto";
+
+
+#Manage Payment Types
+$mpt_page_title = " - Xestionar Tipos de Aboamento";
+$mpt_page_header = "Xesti&oacute;n de Tipos de Aboamento";
+$mpt_no_invoices = "Non hai Tipos de Aboamento na base de datos";
+$mpt_table_action = "Acci&oacute;n";
+$mpt_table_pt_id = "ID";
+$mpt_table_pt_description = "Descrici&oacute;n";
+$mpt_actions_view = "Ver";
+$mpt_actions_edit = "Editar";
+$mpt_actions_new_tax = "Engadir Tipo de Aboamento";
+
+#Print Preview - Invoice layout
+/*
+$pp_invoice_number ="Nun.";
+$pp_invoice_date ="data.";
+$pp_invoice_mobile ="Mob.";
+$pp_invoice_fax ="Fax";
+$pp_invoice_email ="Correo el.";
+$pp_invoice_customer ="Cliente";
+$pp_invoice_attention ="Attn.";
+$pp_invoice_phone ="Telf";
+$pp_invoice_description ="Descrici&oacute;n";
+$pp_invoice_gross_total ="Total bruto";
+$pp_invoice_tax ="Imposto";
+$pp_invoice_total ="TOTAL";
+$pp_invoice_unit_price ="Prezo Unitario";
+$pp_invoice_quantity ="Cant.";
+$pp_invoice_item ="Item";
+$pp_invoice_note ="Nota";
+$pp_invoice_total_tax ="Taxa Total inclu&iacute;da";
+$pp_invoice_amount ="Cont&iacute;a";
+$pp_invoice_summary ="Resumo";
+*/
+
+#New lang file style $lang followed by the word or description - not grouped by page
+$lang_account_info = "Informaci&oacute;n de Conta";
+$lang_actions = "Acci&oacute;ns";
+$lang_address = "Enderezo";
+$lang_age = "Idade";
+$lang_amount = "Cont&iacute;a";
+$lang_attention_short = "Attn.";
+$lang_biller = "Emisor";
+$lang_cancel = "Anular";
+$lang_city = "Cidade";
+$lang_country = "Pa&iacute;s";
+$lang_customer = "Cliente";
+$lang_customer_account = "Conta de Cliente";
+$lang_customer_add = "Engadir Novo Cliente";
+$lang_customer_contact = "Contacto de Cliente (Attn)";
+$lang_customer_name = "Nome de Cliente";
+$lang_customers = "Clientes";
+$lang_date= "data";
+$lang_days = "d&iacute;as";
+$lang_description = "Descrici&oacute;n";
+$lang_details = "detalles";
+$lang_edit = "Editar";
+$lang_email = "Correo el.";
+$lang_email_quick = "Correo el. r&aacute;pido";
+$lang_export_as = "Exportar como";
+$lang_export_pdf = "Exportar como PDF";
+$lang_fax = "Fax";
+$lang_id = "ID";
+$lang_included = "inclu&iacute;do";
+$lang_inv = "Factura";
+$lang_inv_total = " - Total";
+$lang_inv_itemised = " - Desagregado";
+$lang_inv_consulting = " - An&aacute;lise";
+$lang_inv_pref = "Preferencia de Factura";
+$lang_invoice_listings = "Listado de Factura";
+$lang_item = "Item";
+$lang_gross_total = "Total Bruto";
+$lang_mandatory_fields = "Todos os campos son obrigados";
+$lang_manage = "Xestinar";
+$lang_mobile_short = "M&oacute;b.";
+$lang_note = "Nota";
+$lang_notes = "Notas";
+$lang_notes_opt = "Notas (opcional)";
+$lang_number_short = "N&uacute;m.";
+$lang_owing = "Xerado";
+$lang_optional = "opcional";
+$lang_paid = "Pagado";
+$lang_phone = "Telf.";
+$lang_phone_short = "Tf.";
+$lang_telephone_short = "Tel.";
+$lang_print_preview = "Imprimir Vista Previa";
+$lang_process_payment = "Procesar Aboamento";
+$lang_products = "Produtos";
+$lang_provision_of = "Provisi&oacute;n de";
+$lang_quick_view_of = "Esta &eacute; un Vista R&aacute;pida de";
+$lang_save = "Gravar";
+$lang_state = "Pa&iacute;s";
+$lang_street = "Enderezo";
+$lang_sub_total = "Sub Total";
+$lang_sum = "Res";
+$lang_summary = "Resumo";
+$lang_summary_of_accounts = "Resumo de contas";
+$lang_tax = "Imposto";
+$lang_tax_total = "Imposto total inclu&iacute;do";
+$lang_total = "Total";
+$lang_total_uppercase = "TOTAL";
+$lang_total_invoices = "Total de Facturas";
+$lang_total_paid = "Total Pagado";
+$lang_total_owing = "Total Xerado";
+$lang_totals = "Totais";
+$lang_unit_price = "Prezo Unitario";
+$lang_view = "Ver";
+$lang_quantity = "Cont&iacute;a";
+$lang_quantity_short = "Cant";
+$lang_zip = "C&oacute;digo Postal";
+
+?>
