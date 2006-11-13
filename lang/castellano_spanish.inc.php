226f7b78cb484619ac0c18c55ec7a7f4c5ee41d0
Justin Kelly <justin@kelly.org.au>
2006-11-13 23:17:21 +0000

    adding spanish translation

---
+++ lang/castellano_spanish.inc.php
@@ -0,0 +1,376 @@
+﻿<?php
+
+#all
+$title = "Facturas Simples";
+$wording_for_enabledField ="Activado";
+$wording_for_disabledField ="Desactivado";
+
+
+#New lang file style $lang followed by the word or description - not grouped by page
+$LANG_account_info = "Informaci&oacute;n de la Cuenta";
+$LANG_actions = "Acciones";
+$LANG_add_biller = "A&ntilde;adir facturador";
+$LANG_add_new_invoice = "A&ntilde;adir Nueva Factura";
+$LANG_add_new_tax_rate = "A&ntilde;adir Nuevo Porcentaje de Impuesto";
+$LANG_address = "Direcci&oacute;n";
+$LANG_address_city = "Direcci&oacute;n: Ciudad";
+$LANG_address_country = "Direcci&oacute;n: Pais";
+$LANG_address_state = "Direcci&oacute;n: Estado";
+$LANG_address_street = "Direcci&oacute;n: Calle";
+$LANG_address_zip = "A&ntilde;adirress: Código Postal";
+$LANG_age = "Edad";
+$LANG_aging = "Antig&uuml;edad";
+$LANG_amount = "Cantidad";
+$LANG_attention_short = "Attn.";
+$LANG_biller = "Facturador";
+$LANG_biller_details = "Detalles del Facturador";
+$LANG_biller_id = "ID del Facturador";
+$LANG_biller_name = "Nombre del Facturador";
+$LANG_biller_to_add = "Facturador para a&ntilde;adir";
+$LANG_cancel = "Anular";
+$LANG_city = "Ciudad";
+$LANG_consulting = "Analizar";
+$LANG_consulting_style = "Estilo anal&iacute;tico";
+$LANG_country = "Pais";
+$LANG_currency_sign = "Simbolo de Moneda";
+$LANG_customer = "Cliente";
+$LANG_customer_account = "Cuenta de Cliente";
+$LANG_customer_add = "A&ntilde;adir Nuevo Cliente";
+$LANG_customer_contact = "Contacto de Cliente (Attn)";
+$LANG_customer_details = "Detalles del Cliente ";
+$LANG_customer_name = "Nombre del cliente";
+$LANG_customers = "Clientes";
+$LANG_date= "fecha";
+$LANG_date_created = "Fecha de creaci&oacute;n";
+$LANG_date_formatted = "Fecha (YYYY-MM-DD)";
+$LANG_days = "d&iacute;s";
+$LANG_description = "Descrici&oacute;on";
+$LANG_details = "Detalles";
+$LANG_edit = "Editar";
+$LANG_edit_view_tooltip = "Editar";
+$LANG_email = "Correo el.";
+$LANG_email_quick = "Correo r&aacute;pido";
+$LANG_export_as = "Exportar como";
+$LANG_export_doc_tooltip = "a un procesador de texto como";
+$LANG_export_pdf = "Exportar a PDF";
+$LANG_export_pdf_tooltip = "a formato PDF";
+$LANG_export_tooltip = "Exportar";
+$LANG_export_xls_tooltip = "a una hoja de c&aacute;lculo";
+$LANG_fax = "Fax";
+$LANG_format_tooltip = "formato";
+$LANG_gross_total = "Bruto Total";
+$LANG_hide_details = "Ocultar detalles";
+$LANG_id = "ID";
+$LANG_ie_10_for_10 = "* ie. 10 por 10%";
+$LANG_included = "inclu&iacute;do";
+$LANG_insert_biller = "Introducir Facturador";
+$LANG_insert_customer = "Introducir Cliente";
+$LANG_insert_payment_type = "Introducir Tipo de Pago";
+$LANG_insert_preference = "Introducir Preferencia";
+$LANG_insert_product = "Introducir Producto";
+$LANG_insert_tax_rate = "Introducir Porcentaje de Impuesto";
+$LANG_inv = "Factura";
+$LANG_inv_consulting = " - Anal&iacute;tico";
+$LANG_inv_itemised = " - Detallado";
+$LANG_inv_pref = "Factura Preference";
+$LANG_inv_total = " - Total";
+$LANG_invoice_detail_heading = "Detalle de cabecera de Factura ";
+$LANG_invoice_detail_line = "Linea de detalle de Factura";
+$LANG_invoice_footer = "Pi&eacute; de Factura";
+$LANG_invoice_heading = "Cabecera de Factura";
+$LANG_invoice_id = "ID de Factura";
+$LANG_invoice_listings = "Listado de Factura";
+$LANG_invoice_payment_line_1_name = "Factura de pago l&iacute;nea 1 nombre";
+$LANG_invoice_payment_line_1_value = "Factura payment l&iacute;nea 1 valor";
+$LANG_invoice_payment_line_2_name = "Factura payment l&iacute;nea 2 nombre";
+$LANG_invoice_payment_line_2_value = "Factura payment l&iacute;nea 2 valor";
+$LANG_invoice_payment_method = "M&eacute;todo de Factura de pago";
+$LANG_invoice_preference_to_add = "Preferencia de Factura a a&ntilde;adir";
+$LANG_invoice_summary = "Resumen de Factura";
+$LANG_invoice_type = "Tipo";
+$LANG_invoice_wording = "comentario de Factura";
+$LANG_item = "Item";
+$LANG_itemised = "Detallado";
+$LANG_itemised_style = "Estilo detallado";
+$LANG_logo_file = "Fichero de Logo";
+$LANG_manage = "Gesti&oacute;n";
+$LANG_manage_invoices = "Gestionar Facturas";
+$LANG_manage_payment_types = "Gestionar Tipos de Pago";
+$LANG_manage_tax_rates = "Gestionar Porcentajes de Impuesto";
+$LANG_mandatory_fields = "Todos los campos son obligatorios";
+$LANG_mobile_phone = "Tel&eacute;fono M&oacute;vil";
+$LANG_mobile_short = "M&oacute;v.";
+$LANG_no_invoices = "No hay facturas en la base de datos";
+$LANG_no_payment_types = "Lo siento, no hay tipos de pago disponibles, por favor introduzca uno";
+$LANG_no_tax_rates = "No hay porcentajes de impuestos en la base de datos, por facor introduzca uno";
+$LANG_note = "Nota";
+$LANG_notes = "Notas";
+$LANG_notes_opt = "Notas (opcional)";
+$LANG_number_short = "N&uacute;m.";
+$LANG_owing = "Pendiente";
+$LANG_optional = "opcional";
+$LANG_paid = "Pagado";
+$LANG_payment_type = "Tipo de Pago";
+$LANG_payment_type_description = "Descripci&oacute;n de tipo de pago";
+$LANG_payment_type_details = "Detalles de Tipo de Pago";
+$LANG_payment_type_id = "ID de Tipo de Pago";
+$LANG_payment_type_method = "Tipo/Method de Pago";
+$LANG_payment_type_to_add = "Tipo de Pago a a&ntilde;adir";
+$LANG_phone = "Tel&eacute;fono";
+$LANG_phone_short = "Telf.";
+$LANG_print_preview = "Imprimir Vista Previa";
+$LANG_print_preview_tooltip = "Vista Previa de";
+$LANG_process_payment = "Procesar Pago";
+$LANG_process_payment_for = "Procesar Pago para";
+$LANG_product_description = "Descripci&oacute;n de Producto";
+$LANG_product_enabled = "Producto Activo";
+$LANG_product_id = "ID de Producto";
+$LANG_product_to_add = "Producto a a&ntilde;adir";
+$LANG_product_unit_price = "Precio de Unidad de Producto";
+$LANG_products = "Productos";
+$LANG_provision_of = "Provisi&oacute;n de";
+$LANG_quick_view_of = "Esta es una vista r&aacute;pida de";
+$LANG_quick_view_tooltip = "Vista R&aacute;pida de";
+$LANG_save = "Grabar";
+$LANG_save_payment_type = "Grabar Tipo de Pago";
+$LANG_save_product = "Grabar Producto";
+$LANG_save_tax_rate = "Grabar Porcentaje de Impuesto";
+$LANG_select_invoice = "Por favor seleccione una factura";
+$LANG_show_details = "Mostrar detalles";
+$LANG_state = "Estado";
+$LANG_street = "Calle";
+$LANG_sub_total = "Sub Total";
+$LANG_sum = "Sum";
+$LANG_summary = "Resumen";
+$LANG_summary_of_accounts = "Resumen de cuentas";
+$LANG_tax = "Impuesto";
+$LANG_tax_description = "Descripci&oacute;n de Impuesto";
+$LANG_tax_id = "ID de Impuesto";
+$LANG_tax_percentage = "Porcentaje de Impuesto";
+$LANG_tax_rate = "Porcentaje de Impuesto";
+$LANG_tax_rate_details = "Detalles de porcentaje de Impuesto";
+$LANG_tax_rate_id = "ID de Porcentaje de Impuesto";
+$LANG_tax_rate_to_add = "Porcentaje de Impuesto a a&ntilde;adir";
+$LANG_tax_total = "Total de Impuestos inclu&iacute;dos";
+$LANG_telephone_short = "Tel";
+$LANG_total = "Total";
+$LANG_total_amount = "Cantidad Total";
+$LANG_total_invoices = "Total de Facturas";
+$LANG_total_paid = "Total Pagado";
+$LANG_total_owing = "Total Pendiente";
+$LANG_total_style = "Estilo de Total";
+$LANG_total_uppercase = "TOTAL";
+$LANG_totals = "Totales";
+$LANG_unit_price = "Precio Unitario";
+$LANG_view = "Vista";
+$LANG_quantity = "Cantidad";
+$LANG_quantity_short = "Cant";
+$LANG_welcome = "Bienvenido a ";
+$LANG_zip = "C&oacute;digo postal";
+
+
+#Index.php - front page
+
+$indx_welcome ="Bienvenido a";
+
+$indx_shortcut ="Men&uacute; de atajos";
+$LANG_shortcut ="Men&uacute; de atajos";
+
+$indx_getting_started ="Para empezar";
+$LANG_getting_started ="Para empezar";
+$indx_faqs_what ="¿Que es Facturas Simples?";
+$LANG_faqs_what ="¿Que es Facturas Simples?";
+$indx_faqs_need ="¿What necesito para empezar a facturar?";
+$LANG_faqs_need ="¿What necesito para empezar a facturar?";
+$indx_faqs_how ="¿Como puedo crear facturas?";
+$LANG_faqs_how ="¿Como puedo crear facturas?";
+$indx_faqs_type ="¿Que son los diferentes tipos de facturas?";
+$LANG_faqs_type ="¿Que son los diferentes tipos de facturas?";
+
+$LANG_create_invoice ="Crear una factura";
+$indx_invoice_total ="Total";
+$indx_invoice_itemised ="Detallada";
+$indx_invoice_consulting ="Anal&iacute;tica";
+
+$indx_manage_existing_invoice ="Gestionar sus facturas actuales";
+$LANG_manage_existing_invoice ="Gestionar sus facturas actuales";
+$indx_manage_invoices ="Gestionar facturas";
+$LANG_manage_invoices ="Gestionar facturas";
+
+$indx_manage_data ="Gestionar sus datos";
+$LANG_manage_data ="Gestionar sus datos";
+$indx_insert_customer = "A&ntilde;adir Cliente";
+$indx_insert_biller = "A&ntilde;adir Facturador";
+$indx_insert_product = "A&ntilde;adir Producto";
+
+$indx_options ="Opciones";
+$indx_options_sys_defaults ="Valores predeterminados del Sistema";
+$indx_options_tax_rates ="Porcentajes del Impuesto";
+$indx_options_inv_pref ="Preferencias de la Factura ";
+$indx_options_payment_types ="Tipos de Pago";
+$indx_options_upgrade ="Gestionar la actualizaci&oacute;n de la base de datos";
+$indx_options_backup ="Copia de Seguridad de la base de datos";
+
+$indx_help ="Ayuda!!!";
+$indx_help_install ="Instalaci&oacute;n";
+$indx_help_upgrade ="Actualizar Facturas Simples";
+$indx_help_prepare ="Preparar Facturas Simples para usar";
+
+$indx_stats ="Estad&iacute;sticas r&aacute;pidas";
+$LANG_stats ="Informes r&aacute;pidos";
+$indx_stats_debtor ="El mayor deudor";
+$LANG_stats_debtor ="El mayor deudor";
+$indx_stats_customer ="Mayor Cliente - por cantidad facturada";
+$LANG_stats_customer ="Mayor Cliente - por cantidad facturada";
+$indx_stats_biller ="Mayor Facturador - por cantidad facturada";
+$LANG_stats_biller ="Mayor Facturador - por cantidad facturada";
+
+#Gestionar Facturas
+$mi_page_title = " - Gestionar Facturas";
+$mi_page_header = "Gestionar Facturas";
+$mi_no_invoices = "No hay facturas en la base de datos";
+$mi_table_action = "Acci&oacute;";
+$mi_table_id = "ID";
+$mi_table_biller = "Facturador";
+$mi_table_customer = "Cliente";
+$mi_table_total = "Total";
+$mi_table_paid = "Pagado";
+$mi_table_owing = "Pendiente";
+$mi_table_type = "Tipo";
+$mi_table_date = "Fecha de creaci&oacute;n";
+$mi_actions_quick_view = "ver";
+$mi_actions_quick_view_tooltip = "Vista r&aacue;pida de";
+$mi_actions_edit_view = "editar";
+$mi_actions_edit_view_toolkit = "Editar";
+$mi_actions_print_preview_tooltip = "Imprimir vista previa de";
+$mi_actions_export_tooltip = "Exportar";
+$mi_actions_export_pdf_tooltip = "como formato PDF";
+$mi_actions_format_tooltip = "formato";
+$mi_actions_export_xls_tooltip = "a una hoja de c&aacute;lculo como";
+$mi_actions_export_doc_tooltip = "a un procesador de texto como";
+$mi_actions_process_payment = "Procesar pago por";
+$mi_action_invoice_total = "A&ntilde;adir nueva Factura - Estilo Total";
+$mi_action_invoice_itemised = "A&ntilde;adir nueva Factura - Estilo detallado";
+$mi_action_invoice_consulting = "A&ntilde;adir nueva Factura - Estilo anal&iacute;tico";
+
+
+#Gestionar Products
+$mp_page_title = " - Gestionar Productos";
+$mp_page_header = "Gestionar Productos";
+$mp_no_invoices = "No hay productos en la base de datos, por favor introduzca uno";
+$mp_table_action = "Acci&oacute,n";
+$mp_table_product_id = "ID de Producto";
+$mp_table_product_desc = "Descripci&oacute;n";
+$mp_table_unit_price = "Precio Unitario";
+$mp_actions_view = "Ver";
+$mp_actions_edit = "Editar";
+$mp_actions_new_product = "A&ntilde;adir Nuevo Producto";
+
+#Gestionar Facturadors
+$mb_page_title = " - Gestionar Facturadores";
+$mb_page_header = "Gestionar Facturadores";
+$mb_no_invoices = "No hay facturadores en la base de datos, por favor introduzca uno";
+$mb_table_action = "Acci&oacute;n";
+$mb_table_biller_id = "ID de Facturador";
+$mb_table_biller_name = "Nombre de Facturador";
+$mb_table_phone = "Tel&eacute;fono";
+$mb_table_mobile_phone = "Tel&eacute;fono m&oacutevil";
+$mb_table_email = "Correo el.";
+$mb_actions_view = "Vista";
+$mb_actions_edit = "Editar";
+$mb_actions_new_biller = "A&ntilde;adir Nuevo Facturador";
+
+#Gestionar Clientes
+$mc_page_title = " - Gestionar Clientes";
+$mc_page_header = "Gestionar Clientes";
+$mc_no_invoices = "No hay clientes en la base de datos, por favor introduzca uno";
+$mc_table_action = "Acci&oacute;n";
+$mc_table_customer_id = "ID de Cliente";
+$mc_table_customer_name = "Nombre de Cliente";
+$mc_table_attention = "Atenci&oacute;n";
+$mc_table_phone = "Tel&eacute;fono";
+$mc_table_email = "Correo el.";
+$mc_actions_view = "Ver";
+$mc_actions_edit = "Editar";
+$mc_actions_new_product = "A&ntilde;adir Nuevo Cliente";
+
+#Gestionar Preferences
+$mip_page_title = " - Gestionar Preferencias";
+$mip_page_header = "Gestionar Preferencias";
+$mip_no_invoices = "No hay preferencias de factura en la base de datos, por favor introduzca uno";
+$mip_table_action = "Acci&oacute;n";
+$mip_table_preference_id = "ID de Preferencia";
+$mip_table_description = "Descripci&oacute;n";
+$mip_actions_view = "Ver";
+$mip_actions_edit = "Editar";
+$mip_actions_new_preference = "A&ntilde;adir Preferencia de Nueva Factura";
+
+#Gestionar System Defautls manage_system_defaults.php
+$msd_default_number_items = "N&uacute;mero de l&iacute;nea de &iacute;tems:";
+$msd_js_alert_def_inv_template = "Modelo predeterminado de factura";
+$msd_def_inv_template = "Modelo predeterminado de factura";
+$msd_no_tax = "Lo siento, no hay porcentaje de impuesto disponible, por favor introduzca uno";
+$msd_no_payment_type = "Lo siento, no hay tipo de pago disponible, por favor introduzca uno";
+$msd_tax = "Impuesto";
+$msd_payment_type = "Tipo de Pago";
+$msd_invoice_preference = "Preferencia de Factura";
+$msd_no_defaults = "No hay predeterminados";
+$msd_page_title = " - Gestionar Predeterminados del Sistema";
+$msd_heading = "Predeterminados del Sistema";
+$msd_submit_button  = "Introducir Predeterminados";
+
+
+#Gestionar Cuenta Payments
+$map_page_title = " - Gestionar Pagos";
+$map_page_header = "Gestionar Pagos";
+$map_no_invoices = "No hay pagos en la base de datos";
+$map_table_action = "Acci&oacute;n";
+$map_table_payment_id = "ID de Pago";
+$map_table_payment_invoice_id = "ID de Fact.";
+$map_table_biller = "Facturador";
+$map_table_customer = "Cliente";
+$map_table_amount = "Cantidad";
+$map_table_notes = "Notas";
+$map_table_payment_type = "Tipo de Pago";
+$map_table_date = "Fecha";
+$map_actions_view = "Ver";
+$map_actions_process_payment = "Procesar Pago";
+$map_payments_filtered = "Pagos filtrados por ID de Factura";
+$map_payments_filtered_invoice = "Procesar Pago par esta Factura";
+$map_payments_filtered_customer = "Pagos filtrados por ID de Cliente";
+
+#Gestionar Pago Tipos
+$mpt_page_title = " - Gestionar Tipos de Pago";
+$mpt_page_header = "Gestionar Tipos de Pago";
+$mpt_no_invoices = "No hay tipos de pagos en la base de datos";
+$mpt_table_action = "Acci&oacute;n";
+$mpt_table_pt_id = "ID";
+$mpt_table_pt_description = "Descripci&oacute;n";
+$mpt_actions_view = "Ver";
+$mpt_actions_edit = "Editar";
+$mpt_actions_new_tax = "A&ntilde;adir Nuevo Tipo de Pago";
+
+#Print Preview - Factura layout
+/*
+$pp_invoice_number ="N&uacute;m.";
+$pp_invoice_date ="fecha.";
+$pp_invoice_mobile ="M&oacute;v.";
+$pp_invoice_fax ="Fax";
+$pp_invoice_email ="Correo el.";
+$pp_invoice_customer ="Cliente";
+$pp_invoice_attention ="Attn.";
+$pp_invoice_phone ="T&eacute;lf";
+$pp_invoice_description ="Descripci&oacute;n";
+$pp_invoice_gross_total ="Total Bruto";
+$pp_invoice_tax ="Impuesto";
+$pp_invoice_total ="TOTAL";
+$pp_invoice_unit_price ="Precio Unitario";
+$pp_invoice_quantity ="Cant.";
+$pp_invoice_item ="Item";
+$pp_invoice_note ="Nota";
+$pp_invoice_total_tax ="Impuestos totales inclu&iacute;dos";
+$pp_invoice_amount ="Cantidad";
+$pp_invoice_summary ="Resumen";
+*/
+
+?>
