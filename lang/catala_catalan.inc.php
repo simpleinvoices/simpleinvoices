87a7b26f71d2e95a24f3e3c8abe1f26196ada833
Justin Kelly <justin@kelly.org.au>
2006-11-21 22:51:27 +0000

    adding catalan language

---
+++ lang/catala_catalan.inc.php
@@ -0,0 +1,285 @@
+<?php
+
+#all
+$title = "Simple Invoices";
+$wording_for_enabledField ="Habilitat";
+$wording_for_disabledField ="Deshabilitat";
+
+#Index.php - front page
+$indx_welcome ="Benvingut a ";
+
+$indx_shortcut ="Drecera a menu";
+
+$indx_getting_started ="Comen&ccedil;a";
+$indx_faqs_what ="Qu&egrave; &eacute;s Simple Invoices?";
+$indx_faqs_need ="Qu&egrave; necessito per facturar?";
+$indx_faqs_how ="Com es creen factures?";
+$indx_faqs_type ="Quants tipus diferents hi ha de factures?";
+
+$indx_create_invoice ="Crear una factura";
+$indx_invoice_total ="Total";
+$indx_invoice_itemised ="Itemised";
+$indx_invoice_consulting ="Consulting";
+
+$indx_manage_existing_invoice ="Gestionar les teves factures";
+$indx_manage_invoices ="Gestionar Factures";
+
+$indx_manage_data ="Gestiona les teves dades";
+$indx_insert_customer = "Afegir Client";
+$indx_insert_biller = "Afegir Facturador";
+$indx_insert_product = "Afegir Producte";
+
+$indx_options ="Opcions";
+$indx_options_sys_defaults ="Valors sistema per defecte";
+$indx_options_tax_rates ="Tipus d'IVA";
+$indx_options_inv_pref ="Prefer&egrave;ncies de Factures";
+$indx_options_payment_types ="Tipus Pagaments";
+$indx_options_upgrade ="Actualitzaci&oacute; de la base de dades";
+$indx_options_backup ="C&ograve;pia seguretat de la base de dades";
+
+$indx_help ="Ajuda!!!";
+$indx_help_install ="Instalaci&oacute;";
+$indx_help_upgrade ="Actualitzaci&oacute; Simple Invoices";
+$indx_help_prepare ="Preparar Simple Invoices per usar";
+
+$indx_stats =" Estad&iacute;stiques";
+$indx_stats_debtor ="Grans Deudors";
+$indx_stats_customer ="Millors Clients - per total facturat";
+$indx_stats_biller ="Millor Facturador - per total facturat";
+
+#Manage Invoices
+$mi_page_title = " - Gesti&oacute; de Factures";
+$mi_page_header = "Gestionar Factures";
+$mi_no_invoices = "No hi ha factures a la base de dades";
+$mi_table_action = "Acci&oacute;";
+$mi_table_id = "ID";
+$mi_table_biller = "Facturador";
+$mi_table_customer = "Client";
+$mi_table_total = "Total";
+$mi_table_paid = "Pagament";
+$mi_table_owing = "El deure";
+$mi_table_type = "Tipus";
+$mi_table_date = "Data creaci&oacute;";
+$mi_actions_quick_view = "vista";
+$mi_actions_quick_view_tooltip = "Vista r&agrave; de"; 
+$mi_actions_edit_view = "editar";
+$mi_actions_edit_view_toolkit = "Editar";
+$mi_actions_print_preview_tooltip = "Imprimir Previsualitzaci&oacute;";
+$mi_actions_export_tooltip = "Exportar"; 
+$mi_actions_export_pdf_tooltip = "format PDF";
+$mi_actions_format_tooltip = "format";
+$mi_actions_export_xls_tooltip = "com full de c&agrave;lcul";
+$mi_actions_export_doc_tooltip = "com word";
+$mi_actions_process_payment = "Proc&eacute;s de pagament";
+$mi_action_invoice_total = "Nova Factura - Estil Total";
+$mi_action_invoice_itemised = "Nova Factura - Estil l&iacute;nia";
+$mi_action_invoice_consulting = "Nova Factura - Estil consulta";
+
+
+#Manage Products
+$mp_page_title = " - Gesti&oacute; de Productes";
+$mp_page_header = "Gestionar Productes";
+$mp_no_invoices = "No hi ha productes a la base de dades";
+$mp_table_action = "Acci&oacute;";
+$mp_table_product_id = "ID Producte";
+$mp_table_product_desc = "Descripci&oacute; producte";
+$mp_table_unit_price = "PReu Unitari";
+$mp_actions_view = "Veure";
+$mp_actions_edit = "Editar";
+$mp_actions_new_product = "Afegir Nou Producte";
+
+#Manage Billers
+$mb_page_title = " - Gesti&oacute; de Facturadors";
+$mb_page_header = "Gestionar Facturadors";
+$mb_no_invoices = "No hi ha facturadors, afegeix-ne un";
+$mb_table_action = "Acci&oacute;";
+$mb_table_biller_id = "ID Facturador";
+$mb_table_biller_name = "Nom Facturador";
+$mb_table_phone = "Tel&egrave;fon";
+$mb_table_mobile_phone = "M&ograve;bil";
+$mb_table_email = "Email";
+$mb_actions_view = "Veure";
+$mb_actions_edit = "Editar";
+$mb_actions_new_biller = "Afegir Nou Facturador";
+
+#Manage Customers
+$mc_page_title = " - Gesti&oacute; de Clients";
+$mc_page_header = "Gestionar Clients";
+$mc_no_invoices = "No hi ha Clients, sisplau afegeix-ne un";
+$mc_table_action = "Acci&ograve;";
+$mc_table_customer_id = "ID Client";
+$mc_table_customer_name = "Nom Client";
+$mc_table_attention = "Atenci&oacute;";
+$mc_table_phone = "Tel&egrave;fon";
+$mc_table_email = "Email";
+$mc_actions_view = "Veure";
+$mc_actions_edit = "Editar";
+$mc_actions_new_product = "Afegir Nou Client";
+
+#Manage Preferences
+$mip_page_title = " - Gesti&oacute; de Prefer&egrave;ncies";
+$mip_page_header = "Gestionar Prefer&egrave;ncies";
+$mip_no_invoices = "No hi ha prefer&egrave;ncies definides";
+$mip_table_action = "Acci&ograve;";
+$mip_table_preference_id = "ID Prefer&egrave;ncia";
+$mip_table_description = "Descripci&oacute;";
+$mip_actions_view = "Veure";
+$mip_actions_edit = "Editar";
+$mip_actions_new_preference = "Afegir Prefer&egrave;ncia Factura";
+
+#Manage System Defautls manage_system_defaults.php
+$msd_default_number_items = "Nombre de l&iacute;nies de productes:";
+$msd_js_alert_def_inv_template = "Plantilla factura per defecte";
+$msd_def_inv_template = "Plantilla factura per defecte ";
+$msd_no_tax = "Perdoni, ha de definir un tipus d'IVA, sisplau isereixi un";
+$msd_no_payment_type = "Perdoni, no est&agrave; disponible el m&egrave;tode de pagament, sisplau isereixi un";
+$msd_tax = "Tipus d'IVA";
+$msd_payment_type = "Tipus Pagament";
+$msd_invoice_preference = "Prefer&egrave;ncies Factura";
+$msd_no_defaults = "No hi ha configuracions per defecte";
+$msd_page_title = " - Gesti&oacute; de Configuracions per Defecte";
+$msd_heading = "Gestionar Configuracions per Defecte";
+$msd_submit_button  = "Enviar Configuracions per Defecte";
+
+
+#Manage Account Payments
+$map_page_title = " - Gesti&oacute; de Pagaments";
+$map_page_header = "Gestionar Pagaments";
+$map_no_invoices = "No hi ha sistemes de pagaments";
+$map_table_action = "Acci&ograve;";
+$map_table_payment_id = "ID PAgament";
+$map_table_payment_invoice_id = "ID Fact.";
+$map_table_biller = "Facturador";
+$map_table_customer = "Client";
+$map_table_amount = "Quantitat";
+$map_table_notes = "Notes";
+$map_table_payment_type = "Tipus Pagament";
+$map_table_date = "Data";
+$map_actions_view = "Veure";
+$map_actions_process_payment = "Proc&eacute;s Pagament";
+$map_payments_filtered = "Pagaments filtrats per Factura";
+$map_payments_filtered_invoice = "Proc&eacute;s de pagament per aquesta Factura";
+$map_payments_filtered_customer = "Pagaments filtrats per Client";
+
+#Manage Tax Rate
+$mtr_page_title = " - Gesti&oacute; de Tipus d'IVA";
+$mtr_page_header = "Gestionar Tipus d'IVA";
+$mtr_no_invoices = "No hi ha Tipus d'IVA a la base de dades";
+$mtr_table_action = "Acci&ograve;";
+$mtr_table_tax_id = "ID Tipus d'IVA";
+$mtr_table_tac_desc = "Descripci&oacute;";
+$mtr_table_percentage = "Percentatge";
+$mtr_actions_view = "Veure";
+$mtr_actions_edit = "Editar";
+$mtr_actions_new_tax = "Afegir Nou Tipus d'IVA";
+
+
+#Manage Payment Types
+$mpt_page_title = " - Gesti&oacute; de Tipus de Pagaments";
+$mpt_page_header = "Gestionar Tipus de Pagaments";
+$mpt_no_invoices = "No hiha tipus de pagaments a la base de dades";
+$mpt_table_action = "Acci&ograve;";
+$mpt_table_pt_id = "ID";
+$mpt_table_pt_description = "Descripci&oacute;";
+$mpt_actions_view = "Veure";
+$mpt_actions_edit = "Editar";
+$mpt_actions_new_tax = "Afegir Tipus de Pagament";
+
+#Print Preview - Invoice layout
+$pp_invoice_number ="N.";
+$pp_invoice_date ="data.";
+$pp_invoice_mobile ="Mob.";
+$pp_invoice_fax ="Fax";
+$pp_invoice_email ="Email";
+$pp_invoice_customer ="Client";
+$pp_invoice_attention ="Att.";
+$pp_invoice_phone ="Tlf";
+$pp_invoice_description ="Descripcio";
+$pp_invoice_gross_total ="Gross Total";
+$pp_invoice_tax ="Tax";
+$pp_invoice_total ="TOTAL";
+$pp_invoice_unit_price ="Preu Unitari";
+$pp_invoice_quantity ="Quatitat";
+$pp_invoice_item ="Prod.";
+$pp_invoice_note ="Nota";
+$pp_invoice_total_tax ="IVA Inclos";
+$pp_invoice_amount ="Quantitat";
+$pp_invoice_summary ="Resum";
+
+#New lang file style $lang followed by the word or description - not grouped by page
+$lang_account_info = "Informaci&oacute; compte";
+$lang_actions = "Accions";
+$lang_address = "Adre&ccedil;a";
+$lang_age = "Edat";
+$lang_amount = "Quantitat";
+$lang_attention_short = "Att.";
+$lang_biller = "Facturador";
+$lang_cancel = "Cancela";
+$lang_city = "Ciutat";
+$lang_country = "Pa&igrave;";
+$lang_customer = "Client";
+$lang_customer_account = "Compte Client";
+$lang_customer_add = "Afegir Nou Client";
+$lang_customer_contact = "Contactar Client (Att)";
+$lang_customer_name = "Nom Client";
+$lang_customers = "Clients";
+$lang_date= "data";
+$lang_days = "dies";
+$lang_description = "Descripci&oacute;";
+$lang_details = "detalls";
+$lang_edit = "Editar";
+$lang_email = "Email";
+$lang_email_quick = "Quick Email";
+$lang_export_as = "Exportar com";
+$lang_export_pdf = "Exportar a PDF";
+$lang_fax = "Fax";
+$lang_id = "ID";
+$lang_included = "inclo&ograve;s";
+$lang_inv = "Factura";
+$lang_inv_total = " - Total";
+$lang_inv_itemised = " - Itemised";
+$lang_inv_consulting = " - Consulting";
+$lang_inv_pref = "Prefer&egrave;ncies Factura";
+$lang_invoice_listings = "Llistat Factures";
+$lang_item = "Producte";
+$lang_gross_total = "Suma Total";
+$lang_mandatory_fields = "Tots els camps s&oacute;n obligatoris";
+$lang_manage = "Gestionar";
+$lang_mobile_short = "Mob.";
+$lang_note = "Nota";
+$lang_notes = "Notes";
+$lang_notes_opt = "Notes (opcional)";
+$lang_number_short = "No.";
+$lang_owing = "Deu";
+$lang_optional = "opcional";
+$lang_paid = "Pagat";
+$lang_phone = "Tel&egrave;fon";
+$lang_phone_short = "Tfn.";
+$lang_telephone_short = "Tel";
+$lang_print_preview = "Imprimir Previsualitzaci&oacute;";
+$lang_process_payment = "Proc&eacute;s pagament";
+$lang_products = "Productes";
+$lang_provision_of = "Provisi&oacute; de";
+$lang_quick_view_of = "Aix&ograve; &eacute;s la vista r&agrave;pida de";
+$lang_save = "Salvar";
+$lang_state = "Prov&iacute;ncia";
+$lang_street = "Carrer";
+$lang_sub_total = "Sub Total";
+$lang_sum = "Suma";
+$lang_summary = "Resum";
+$lang_summary_of_accounts = "Resum de comptes";
+$lang_tax = "IVA";
+$lang_tax_total = "IVA incl&ograve;s";
+$lang_total = "Total";
+$lang_total_uppercase = "TOTAL";
+$lang_total_invoices = "Total Factures";
+$lang_total_paid = "Total Pagat";
+$lang_total_owing = "Total Deuen";
+$lang_totals = "Totals";
+$lang_unit_price = "Preu Unitari";
+$lang_view = "Veure";
+$lang_quantity = "Quantitat";
+$lang_quantity_short = "Qtat";
+$lang_zip = "Codi Postal";
+
+?>
