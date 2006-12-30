21c3304d13589d7c3b140a8ddaa629249f27423c
Justin Kelly <justin@kelly.org.au>
2006-12-30 12:06:55 +0000

    move process payment

--- controller.php
+++ controller.php
@@ -4,11 +4,13 @@ $section = $_GET['section'];
 $view = $_GET['view'];
 $action = $_GET['action'];
 
+/* - not needed any more
 if (($section != null ) AND ($view != null) AND ($action != null)) {
         include("./src/$section/$view.php?$action");
 }
-else if (($section != null ) AND ($view != null)) {
-        include("./src/$section/$view.php");
+*/
+if (($module != null ) AND ($view != null)) {
+        include("./src/$module/$view.php");
 }
 else {
         include("start.php");
