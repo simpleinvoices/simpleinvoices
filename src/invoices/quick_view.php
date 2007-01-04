e14e55a80e92faaf8698364aa15db6bdd4c928d8
Justin Kelly <justin@kelly.org.au>
2007-01-04 03:11:53 +0000

    minor update for PDf

--- src/invoices/quick_view.php
+++ src/invoices/quick_view.php
@@ -733,7 +733,7 @@ $mid->printFooter();
 <?php echo $LANG_quick_view_of; echo " "; echo $pref_inv_wordingField; ?> <?php echo $master_invoice_id; ?>
 <br>
 	<?php 
-	$url_pdf = "$_SERVER[HTTP_HOST]$install_path/invoices/templates/$def_inv_templateField.php?submit=$inv_idField&action=view&invoice_style=$inv_ty_descriptionField";
+	$url_pdf = "$_SERVER[HTTP_HOST]$install_path/index.php?module=invoices&view=templates/$def_inv_templateField&submit=$inv_idField&action=view&invoice_style=$inv_ty_descriptionField";
 	$url_pdf_encoded = urlencode($url_pdf); 
 	$url_for_pdf = "pdf/html2ps.php?process_mode=single&renderfields=1&renderlinks=1&renderimages=1&scalepoints=1&pixels=$pdf_screen_size&media=$pdf_paper_size&leftmargin=$pdf_left_margin&rightmargin=$pdf_right_margin&topmargin=$pdf_top_margin&bottommargin=$pdf_bottom_margin&transparency_workaround=1&imagequality_workaround=1&output=1&URL=$url_pdf_encoded";
 
