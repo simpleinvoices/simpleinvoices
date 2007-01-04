707f82215c341b9a42c7d89c829bb1ade8c488a5
Justin Kelly <justin@kelly.org.au>
2007-01-04 03:13:04 +0000

    adding Dutch translation

---
+++ lang/dutch_NL.inc.php
@@ -0,0 +1,410 @@
+<?php
+
+#all
+$title = "Simple Invoices";
+$wording_for_enabledField ="Geactiveerd";
+$wording_for_disabledField ="Gedeactiveerd";
+
+
+#New lang file stijl $lang followed by the word or description - not grouped by page
+$LANG_account_info = "Account Informatie";
+$LANG_actions = "Acties";
+$LANG_add_biller = "Nieuwe bedrijf toevoegen";
+$LANG_add_new_invoice = "Nieuwe factuur toevoegen";
+$LANG_add_new_payment_type = "Nieuwe betaalmethode toevoegen";
+$LANG_add_new_preference = "Nieuwe factuur instelling toevoegen";
+$LANG_add_new_product = "Nieuw product toevoegen";
+$LANG_add_new_tax_rate = "Nieuw BTW tarief toevoegen";
+$LANG_address = "Adres";
+#might be able to delete address: variable - grep files
+$LANG_address_city = "Adres: Plaats";
+$LANG_address_country = "Adres: Land";
+$LANG_address_state = "Adres: Provincie";
+$LANG_address_street = "Adres: Straat";
+$LANG_address_zip = "Adres: Postcode";
+$LANG_age = "Leeftijd";
+$LANG_aging = "Leeftijd";
+$LANG_amount = "Aantal";
+$LANG_attention_short = "t.a.v.";
+$LANG_biller = "Bedrijf";
+$LANG_biller_details = "Details";
+$LANG_biller_id = "ID";
+$LANG_biller_name = "Naam";
+$LANG_biller_to_add = "Bedrijf toevoegen";
+$LANG_cancel = "Annuleer";
+$LANG_city = "Plaats";
+$LANG_consulting = "Informatieve";
+$LANG_consulting_style = "Informatieve stijl";
+$LANG_country = "Land";
+$LANG_currency_sign = "Valuta teken";
+$LANG_custom_field1 = "Gebruikersveld 1";
+$LANG_custom_field2 = "Gebruikersveld 2";
+$LANG_custom_field3 = "Gebruikersveld 3";
+$LANG_custom_field4 = "Gebruikersveld 4";
+$LANG_custom_field = "Gebruikersveld";
+$LANG_custom_field_db_field_name = "Database veldnaam";
+$LANG_custom_fields = "Gebruikersvelden";
+$LANG_custom_label = "Gebruikerslabel";
+$LANG_customer = "Klant";
+$LANG_customer_account = "Account klant";
+$LANG_customer_add = "Nieuwe klant toevoegen";
+$LANG_customer_contact = "Contactpersoon";
+$LANG_customer_details = "Klant details";
+$LANG_customer_id = "ID";
+$LANG_customer_name = "Naam";
+$LANG_customers = "Klanten";
+$LANG_date= "datum";
+$LANG_date_created = "Datum aangemaakt";
+$LANG_date_formatted = "Datum (YYYY-MM-DD)";
+$LANG_days = "days";
+$LANG_description = "Beschrijving";
+$LANG_details = "Details";
+$LANG_edit = "Wijzig";
+$LANG_edit_view_tooltip = "Wijzig";
+$LANG_email = "E-mail";
+$LANG_email_quick = "E-mail";
+$LANG_export_as = "Exporteren als";
+$LANG_export_doc_tooltip = "naar een word processor als";
+$LANG_export_pdf = "Exporteren naar PDF";
+$LANG_export_pdf_tooltip = "als PDF formaat";
+$LANG_export_tooltip = "Exporteren"; 
+$LANG_export_xls_tooltip = "naar een spreadsheet als";
+$LANG_fax = "Fax";
+$LANG_format_tooltip = "formaat";
+$LANG_gross_total = "bruto";
+$LANG_hide_details = "Verberg details";
+$LANG_id = "ID";
+$LANG_ie_10_for_10 = "* bijvoorbeeld. 10 voor 10%";
+$LANG_included = "inclusief";
+$LANG_insert_biller = "Bedrijf toevoegen";
+$LANG_insert_customer = "Klant toevoegen";
+$LANG_insert_payment_type = "Betaalmethode toevoegen";
+$LANG_insert_preference = "Instelling toevoegen";
+$LANG_insert_product = "Product toevoegen";
+$LANG_insert_tax_rate = "BTW tarief toevoegen";
+$LANG_inv = "Factuur";
+$LANG_inv_consulting = " - Informatief";
+$LANG_inv_itemised = " - Gedetailleerd";
+$LANG_inv_pref = "Factuur instelling";
+$LANG_inv_total = " - Totaal";
+$LANG_invoice_detail_heading = "Factuur detail kop";
+$LANG_invoice_detail_line = "Factuur detail regel";
+$LANG_invoice_footer = "Factuur voet";
+$LANG_invoice_heading = "Factuur kop";
+$LANG_invoice_id = "Factuur ID";
+$LANG_invoice_listings = "Factuurlijst";
+$LANG_invoice_payment_line_1_name = "Factuur betaling regel 1 naam";
+$LANG_invoice_payment_line_1_value = "Factuur betaling regel 1 waarde";
+$LANG_invoice_payment_line_2_name = "Factuur betaling regel 2 naam";
+$LANG_invoice_payment_line_2_value = "Factuur betaling regel 2 waarde";
+$LANG_invoice_payment_method = "Factuur betaalmethode";
+$LANG_invoice_preference_to_add = "Factuur instelling toevoegen";
+$LANG_invoice_summary = "Factuur overzicht";
+$LANG_invoice_type = "Type";
+$LANG_invoice_wording = "Factuur verwoording";
+$LANG_item = "Item";
+$LANG_itemised = "Gedetaileerd";
+$LANG_itemised_style = "Gedetaileerde stijl";
+$LANG_logo_file = "Logo bestand";
+$LANG_manage = "Beheren";
+$LANG_manage_custom_fields = "Gebruikersvelden beheren";
+$LANG_manage_customers = "Klanten beheren";
+$LANG_manage_invoices = "Facturene beheren";
+$LANG_manage_payment_types = "Betaalmethoden beheren";
+$LANG_manage_preferences = "Instellingen beheren";
+$LANG_manage_products = "Producten beheren";
+$LANG_manage_tax_rates = "BTW tarieven beheren";
+$LANG_mandatory_fields = "Alle velden zijn verplicht";
+$LANG_mobile_phone = "Mobiele telefoon";
+$LANG_mobile_short = "Mob.";
+$LANG_no_customers = "Er zijn geen klanten beschikbaar";
+$LANG_no_invoices = "Er zijn geen facturen beschikbaar";
+$LANG_no_payment_types = "Er zijn geen betaalmethodes beschikbaar";
+$LANG_no_preferences = "Er zijn geen factuur instellingen beschikbaar";
+$LANG_no_tax_rates = "Er zijn geen BTW tarieven beschikbaar.";
+$LANG_note = "Opmerking";
+$LANG_notes = "Opmerkingen";
+$LANG_notes_opt = "Opmerkingen (optioneel)";
+$LANG_number_short = "Nr.";
+$LANG_owing = "Schuldig";
+$LANG_optional = "optioneel";
+$LANG_paid = "Betaald";
+$LANG_payment_type = "Betaal methode";
+$LANG_payment_type_description = "Beschrijving betaalmethode";
+$LANG_payment_type_details = "Details betaalmethode";
+$LANG_payment_type_id = "ID";
+$LANG_payment_type_method = "Betaalmethode";
+$LANG_payment_type_to_add = "Betaalmethode toevoegen";
+$LANG_phone = "Telefoon";
+$LANG_phone_short = "Tel.";
+$LANG_preference_id = "Instellings ID";
+$LANG_print_preview = "Afdruk voorbeeld";
+$LANG_print_preview_tooltip = "Afdruk voorbeeld van";
+$LANG_process_payment = "Betalingen verwerken";
+$LANG_process_payment_for = "Verwerk betaling voor";
+$LANG_product = "Product";
+$LANG_product_description = "Product beschrijving";
+$LANG_product_enabled = "Product geactiveerd";
+$LANG_product_id = "Product ID";
+$LANG_product_to_add = "Product toevoegen";
+$LANG_product_unit_price = "Productprijs";
+$LANG_products = "Producten";
+$LANG_provision_of = "Provisie van";
+$LANG_quick_view_of = "Dit is een voorbeeld weergave van";
+$LANG_quick_view_tooltip = "Voorbeeld weergave van"; 
+$LANG_save = "Opslaan";
+$LANG_save_custom_field = "Gebruikersveld opslaan";
+$LANG_save_payment_type = "Betaalmethode opslaan";
+$LANG_save_product = "Product opslaan";
+$LANG_save_tax_rate = "BTW tarief opslaan";
+$LANG_select_invoice = "Selecteer een factuur";
+$LANG_show_details = "Bekijk details";
+$LANG_state = "Provincie";
+$LANG_street = "Straat";
+$LANG_street2 = "Straat adres 2";
+$LANG_sub_total = "Subtotaal";
+$LANG_sum = "Som";
+$LANG_summary = "Samenvatting";
+$LANG_summary_of_accounts = "Samenvatting van accounts";
+$LANG_tax = "BTW";
+$LANG_tax_description = "BTW beschrijving";
+$LANG_tax_id = "BTW ID";
+$LANG_tax_percentage = "BTW Percentage";
+$LANG_tax_rate = "BTW tarief";
+$LANG_tax_rate_details = "Details BTW tarief";
+$LANG_tax_rate_id = "BTW tarief ID";
+$LANG_tax_rate_to_add = "BTW tarief toevoegen";
+$LANG_tax_total = "Totale BTW";
+$LANG_telephone_short = "Tel";
+$LANG_total = "Totaal";
+$LANG_total_amount = "Totaal aantal";
+$LANG_total_invoices = "Totale facturen";
+$LANG_total_owing = "Totaal schuldig";
+$LANG_total_paid = "Totaal betaald";
+$LANG_total_style = "Totaal stijl";
+$LANG_total_uppercase = "TOTAAL";
+$LANG_totals = "Totalen";
+$LANG_unit_price = "Prijs";
+$LANG_view = "Bekijk";
+$LANG_quantity = "Aantal";
+$LANG_quantity_short = "Aantal";
+$LANG_welcome = "Welkom bij ";
+$LANG_zip = "Postcode";
+
+
+#Index.php - front page
+
+$indx_welcome ="Welkom bij ";
+
+$indx_shortcut ="Hoofdmenu";
+$LANG_shortcut ="Hoofdmenu";
+
+$indx_getting_started ="Start hier";
+$LANG_getting_started ="Start hier";
+$indx_faqs_what ="Wat is Simple Invoices?";
+$LANG_faqs_what ="Wat is Simple Invoices?";
+$indx_faqs_need ="Wat moet ik doen om te kunnen beginnen met factureren?";
+$LANG_faqs_need ="Wat moet ik doen om te kunnen beginnen met factureren?";
+$indx_faqs_how ="Hoe maak ik facturen?";
+$LANG_faqs_how ="Hoe maak ik facturen?";
+$indx_faqs_type ="Wat zijn de verschillende type facturen?";
+$LANG_faqs_type ="Wat zijn de verschillende type facturen?";
+
+$LANG_create_invoice ="Maak een factuur";
+$indx_invoice_total ="Totaal";
+$indx_invoice_itemised ="Gedetaileerd";
+$indx_invoice_consulting ="Informatief";
+
+$indx_manage_existing_invoice ="Beheer bestaande facturen";
+$LANG_manage_existing_invoice ="Beheer bestaande facturen";
+$indx_manage_invoices ="Beheer facturen";
+$LANG_manage_invoices ="Beheer facturen";
+
+$indx_manage_data ="Beheer je gegevens";
+$LANG_manage_data ="Beheer je gegevens";
+$indx_insert_customer = "Klant toevoegen";
+$indx_insert_biller = "Bedrijf toevoegen";
+$indx_insert_product = "Product toevoegen";
+
+$indx_options ="Opties";
+$indx_options_sys_defaults ="Standaard instellingen";
+$indx_options_tax_rates ="BTW tarieven";
+$indx_options_inv_pref ="Facturen";
+$indx_options_payment_types ="Betaalmethoden";
+$indx_options_upgrade ="Database Upgrade Manager";
+$indx_options_backup ="Backup Database";
+
+$indx_help ="Help!!!";
+$indx_help_install ="Installatie";
+$indx_help_upgrade ="Upgrading Simple Invoices";
+$indx_help_prepare ="Simple Invoices voorbereiden voor gebruik";
+
+$indx_stats =" Stats";
+$LANG_stats =" Rapporten";
+$indx_stats_debtor ="Grootste debiteur";
+$LANG_stats_debtor ="Grootste debiteur";
+$indx_stats_customer ="Beste klant - totaal gefactureerd";
+$LANG_stats_customer ="Beste klant - totaal gefactureerd";
+$indx_stats_biller ="Top bedrijf - totaal gefactureerd";
+$LANG_stats_biller ="Top bedrijf - totaal gefactureerd";
+
+#Facturen beheren
+$mi_page_title = " - Beheer facturen";
+$mi_page_header = "Beheer facturen";
+$mi_no_invoices = "Er zijn geen facturen.";
+$mi_table_action = "Actie";
+$mi_table_id = "ID";
+$mi_table_biller = "Bedrijf";
+$mi_table_customer = "Klant";
+$mi_table_total = "Totaal";
+$mi_table_paid = "Betaald";
+$mi_table_owing = "Schuldig";
+$mi_table_type = "Type";
+$mi_table_date = "Datum aangemaakt";
+$mi_actions_quick_view = "bekijk";
+$mi_actions_quick_view_tooltip = "Quick Bekijk of"; 
+$mi_actions_edit_view = "wijzig";
+$mi_actions_edit_view_toolkit = "Wijzig";
+$mi_actions_print_preview_tooltip = "Afdruk voorbeeld van";
+$mi_actions_export_tooltip = "Exporteren"; 
+$mi_actions_export_pdf_tooltip = "als PDF formaat";
+$mi_actions_format_tooltip = "formaat";
+$mi_actions_export_xls_tooltip = "naar een spreadsheet als";
+$mi_actions_export_doc_tooltip = "naar een word processor als";
+$mi_actions_process_payment = "Verwerk betaling voor";
+$mi_action_invoice_total = "Nieuwe factuur toevoegen - Totaal stijl";
+$mi_action_invoice_itemised = "Nieuwe factuur toevoegen - Gedetaileerde stijl";
+$mi_action_invoice_consulting = "Nieuwe factuur toevoegen - Informatieve stijl";
+
+#Producten beheren
+$mp_page_title = " - Producten beheren";
+$mp_page_header = "Producten beheren";
+$mp_no_invoices = "Er zijn geen producten.";
+$mp_table_action = "Actie";
+$mp_table_product_id = "ID";
+$mp_table_product_desc = "Beschrijving";
+$mp_table_unit_price = "Prijs";
+$mp_actions_view = "Bekijk";
+$mp_actions_edit = "Wijzig";
+$mp_actions_new_product = "Nieuw product toevoegen";
+
+#Beheer bedrijven
+$mb_page_title = " - Beheer bedrijven";
+$mb_page_header = "Beheer bedrijven";
+$mb_no_invoices = "Er zijn geen bedrijven beschikbaar";
+$mb_table_action = "Actie";
+$mb_table_biller_id = "ID";
+$mb_table_biller_name = "Naam";
+$mb_table_phone = "Telefoon";
+$mb_table_mobile_phone = "Mobiele telefoon";
+$mb_table_email = "E-mail";
+$mb_actions_view = "Bekijk";
+$mb_actions_edit = "Wijzig";
+$mb_actions_new_biller = "Nieuw bedrijf toevoegen";
+
+#Klanten beheren
+$mc_page_title = " - Klanten beheren";
+$mc_page_header = "Klanten beheren";
+$mc_no_invoices = "Er zijn geen klanten";
+$mc_table_action = "Actie";
+$mc_table_customer_id = "ID";
+$mc_table_customer_name = "Naam";
+$mc_table_attention = "Attentie";
+$mc_table_phone = "Telefoon";
+$mc_table_email = "E-mail";
+$mc_actions_view = "Bekijk";
+$mc_actions_edit = "Wijzig";
+$mc_actions_new_product = "Nieuwe klant toevoegen";
+
+#Instellingen beheren
+$mip_page_title = " - Instellingen beheren";
+$mip_page_header = "Instellingen beheren";
+$mip_no_invoices = "Er zijn geen factuur instellingen beschikbaar";
+$mip_table_action = "Actie";
+$mip_table_preference_id = "ID";
+$mip_table_description = "Beschrijving";
+$mip_actions_view = "Bekijk";
+$mip_actions_edit = "Wijzig";
+$mip_actions_new_preference = "Nieuwe factuur instelling";
+
+#Manage System Defautls manage_system_defaults.php
+$msd_default_number_items = "Standaard aantal item regels:";
+$msd_js_alert_def_inv_template = "Standaard factuur template";
+$msd_def_inv_template = "Standaard factuur template ";
+$msd_no_tax = "Geen BTW tarief beschikbaar";
+$msd_no_payment_type = "Geen betaalmethode beschikbaar";
+$msd_tax = "BTW";
+$msd_payment_type = "Betaalmethode";
+$msd_invoice_preference = "Factuur instelling";
+$msd_no_defaults = "Er is geen standaard ingesteld";
+$msd_page_title = " - Beheer basis instellingen";
+$msd_heading = "Basis instellingen";
+$msd_submit_button  = "Bevestig basis instellingen";
+
+
+#Betalingen beheren
+$map_page_title = " - Betalingen beheren";
+$map_page_header = "Betalingen beheren";
+$map_no_invoices = "Er zijn geen betalingen beschikbaar";
+$map_table_action = "Actie";
+$map_table_payment_id = "ID";
+$map_table_payment_invoice_id = "Factuur ID";
+$map_table_biller = "Bedrijf";
+$map_table_customer = "Klant";
+$map_table_amount = "Aantal";
+$map_table_notes = "Opmerking";
+$map_table_payment_type = "Betaalmethode";
+$map_table_date = "Datum";
+$map_actions_view = "Bekijk";
+$map_actions_process_payment = "Betaling verwerken";
+$map_payments_filtered = "Betalingen gefilterd op Factuur ID";
+$map_payments_filtered_invoice = "Verwerk betalingen voor deze factuur";
+$map_payments_filtered_customer = "Betalingen gefilterd op klant ID";
+
+#BTW tarieven beheren
+$mtr_page_title = " - BTW tarieven beheren";
+$mtr_page_header = "BTW tarieven beheren";
+$mtr_no_invoices = "Er zijn geen BTW tarieven";
+$mtr_table_action = "Actie";
+$mtr_table_tax_id = "ID";
+$mtr_table_tac_desc = "Beschrijving";
+$mtr_table_percentage = "Percentage";
+$mtr_actions_view = "Bekijk";
+$mtr_actions_edit = "Wijzig";
+$mtr_actions_new_tax = "Nieuw BTW tarief toevoegen";
+
+#Betaalmethoden beheren
+$mpt_page_title = " - Betaalmethoden beheren";
+$mpt_page_header = "Betaalmethoden beheren";
+$mpt_no_invoices = "Er zijn geen betaalmethoden";
+$mpt_table_action = "Actie";
+$mpt_table_pt_id = "ID";
+$mpt_table_pt_description = "Beschrijving";
+$mpt_actions_view = "Bekijk";
+$mpt_actions_edit = "Wijzig";
+$mpt_actions_new_tax = "Nieuwe betaalmethode toevoegen";
+
+#Print Preview - Invoice layout
+/*
+$pp_invoice_number ="No.";
+$pp_invoice_date ="date.";
+$pp_invoice_mobile ="Mob.";
+$pp_invoice_fax ="Fax";
+$pp_invoice_email ="Email";
+$pp_invoice_customer ="Customer";
+$pp_invoice_attention ="Attn.";
+$pp_invoice_phone ="Ph";
+$pp_invoice_description ="Description";
+$pp_invoice_gross_total ="Gross Total";
+$pp_invoice_tax ="Tax";
+$pp_invoice_total ="TOTAL";
+$pp_invoice_unit_price ="Unit Price";
+$pp_invoice_quantity ="Qty";
+$pp_invoice_item ="Item";
+$pp_invoice_note ="Note";
+$pp_invoice_total_tax ="Total tax included";
+$pp_invoice_amount ="Amount";
+$pp_invoice_summary ="Summary";
+*/
+
+?>
