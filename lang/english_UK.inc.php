fe6233ed4e0da994b74992d92e4abb8284f0fcc4
Justin Kelly <justin@kelly.org.au>
2007-01-24 11:44:52 +0000

    lang update

--- lang/english_UK.inc.php
+++ lang/english_UK.inc.php
@@ -3,6 +3,7 @@
 /*// 1 means that the variable has been translated and // zero means it hasnt been translated - this is used by a script to calculate how much of each file has been done
 regex :%s/;/ /1/;// 1\/\/1/g - remove the spaces
  */
+
 #all
 $title = "Simple Invoices";//1
 $wording_for_enabledField ="Enabled";//1
