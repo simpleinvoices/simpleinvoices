803903ba7d2a49bedf56f21eabf08a3c680fe9ac
Justin Kelly <justin@kelly.org.au>
2007-01-29 22:34:28 +0000

    minor mod

--- lang/deutsch_german.inc.php
+++ lang/deutsch_german.inc.php
@@ -7,7 +7,7 @@
 
 /*// 1 means that the variable has been translated and // zero means it hasnt been translated - this is used by a script to calculate how much of each file has been done
 regex :%s/;/ /1/;// 1\/\/1/g - remove the spaces
- */
+*/
 
 #all
 $title = "Rechnungsverwaltung";//1
