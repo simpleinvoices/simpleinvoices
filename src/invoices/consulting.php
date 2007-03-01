e8d39f9ad3da9bf29589e5d436c7bc7c6da1e94d
Nicolas Ruflin <ruflin@gmail.com>
2007-03-01 01:38:21 +0000

    Started changing the string-array stuff. Not complete until yet.

--- src/invoices/consulting.php
+++ src/invoices/consulting.php
@@ -44,17 +44,11 @@ $result_preferences = mysql_query($sql_preferences, $conn) or die(mysql_error())
 $sql_defaults = "SELECT * FROM si_defaults";
 $result_defaults = mysql_query($sql_defaults, $conn) or die(mysql_error());
 
-while ($Array_defaults = mysql_fetch_array($result_defaults) ) {
-                $def_idField = $Array_defaults['def_id'];
-                $def_billerField = $Array_defaults['def_biller'];
-                $def_customerField = $Array_defaults['def_customer'];
-                $def_taxField = $Array_defaults['def_tax'];
-                $def_inv_preferenceField = $Array_defaults['def_inv_preference'];
-                $def_number_line_itemsField = $Array_defaults['def_number_line_items'];
-};
+$def = mysql_fetch_array($result_defaults);
+
 #Get the names of the defaults from their id -start
 #default biller name query
-$sql_biller_default = "SELECT b_name FROM si_biller where b_id = $def_billerField and b_enabled != 0";
+$sql_biller_default = "SELECT b_name FROM si_biller where b_id = $def[def_biller] and b_enabled != 0";
 $result_biller_default = mysql_query($sql_biller_default , $conn) or die(mysql_error());
 
 while ($Array = mysql_fetch_array($result_biller_default) ) {
@@ -62,7 +56,7 @@ while ($Array = mysql_fetch_array($result_biller_default) ) {
 }
 
 #default customer name query
-$print_customer = "SELECT * FROM si_customers WHERE c_id = $def_customerField and c_enabled != 0";
+$print_customer = "SELECT * FROM si_customers WHERE c_id = $def[def_customer] and c_enabled != 0";
 $result_print_customer = mysql_query($print_customer, $conn) or die(mysql_error());
 
 while ($Array_customer = mysql_fetch_array($result_print_customer)) {
@@ -70,7 +64,7 @@ while ($Array_customer = mysql_fetch_array($result_print_customer)) {
 }
 
 #default tax description query
-$print_tax = "SELECT * FROM si_tax WHERE tax_id = $def_taxField and tax_enabled != 0";
+$print_tax = "SELECT * FROM si_tax WHERE tax_id = $def[def_tax] and tax_enabled != 0";
 $result_print_tax = mysql_query($print_tax, $conn) or die(mysql_error());
 
 while ($Array_tax = mysql_fetch_array($result_print_tax)) {
@@ -78,7 +72,7 @@ while ($Array_tax = mysql_fetch_array($result_print_tax)) {
 }
 
 #default invoice preference description query
-$print_inv_preference = "SELECT * FROM si_preferences WHERE pref_id = $def_inv_preferenceField and pref_enabled != 0";
+$print_inv_preference = "SELECT * FROM si_preferences WHERE pref_id = $def[def_inv_preference] and pref_enabled != 0";
 $result_inv_preference = mysql_query($print_inv_preference, $conn) or die(mysql_error());
 
 while ($Array_inv_preference = mysql_fetch_array($result_inv_preference)) {
@@ -98,17 +92,20 @@ if (mysql_num_rows($result) == 0) {
 
 } else {
         //has records, so display them
-        $display_block = "
-        <select name=\"sel_id\">
-        <option selected value=\"$def_billerField\" style=\"font-weight: bold\">$sql_biller_defaultField</option>
-        <option value=\"\"></option>";
+        $display_block = <<<EOD
+        <select name="sel_id">
+        <option selected value="$def[def_biller]" style="font-weight: bold">$sql_biller_defaultField</option>
+        <option value=""></option>
+EOD;
 
         while ($recs = mysql_fetch_array($result)) {
                 $id = $recs['b_id'];
                 $display_name = $recs['b_name'];
 
-                $display_block .= "<option value=\"$id\">
-                        $display_name</option>";
+                $display_block .= <<<EOD
+                <option value="$id">
+                        $display_name</option>
+EOD;
         }
 }
 
@@ -120,17 +117,20 @@ if (mysql_num_rows($result_customer) == 0) {
 
 } else {
         //has records, so display them
-        $display_block_customer = "
-        <select name=\"select_customer\">
-        <option selected value=\"$def_customerField\" style=\"font-weight: bold\">$c_nameField</option>
-        <option value=\"\"></option>";
+        $display_block_customer = <<<EOD
+        <select name="select_customer">
+        <option selected value="$der[def_customer]" style="font-weight: bold">$c_nameField</option>
+        <option value=""></option>
+EOD;
 
         while ($recs_customer = mysql_fetch_array($result_customer)) {
                 $id_customer = $recs_customer['c_id'];
                 $display_name_customer = $recs_customer['c_name'];
 
-                $display_block_customer .= "<option value=\"$id_customer\">
-                        $display_name_customer</option>";
+                $display_block_customer .= <<<EOD
+                <option value=$id_customer">
+                        $display_name_customer</option>
+EOD;
         }
 }
 
@@ -149,27 +149,30 @@ function line_items($line) {
 
 	} else {
 	        //has records, so display them
-	        $display_block_products = "
-	        <select name=\"select_products$line\">
-	        <option value=\"\"></option>";
+	        $display_block_products = <<<EOD
+	        <select name="select_products$line">
+	        <option value=""></option>
+EOD;
 
         	while ($recs_products = mysql_fetch_array($result_products)) {
     	               $id_products = $recs_products['prod_id'];
        		       $display_name_products = $recs_products['prod_description'];
 
-	                $display_block_products .= "<option value=\"$id_products\">
-        	                $display_name_products</option>";
+	                $display_block_products .= <<<EOD
+	                <option value="$id_products">
+        	                $display_name_products</option>
+EOD;
 	        }
 	        }
-                echo "<tr>
+                echo <<< EOD
+                <tr>
                 <td><input type=text name='i_quantity$line' size=5></td><td input type=text name='i_description$line' size=50>$display_block_products </td><td>
-<a href='#' class=\"show-text$line\" onClick=\"$('.text$line').show();$('.show-text$line').hide();\">$LANG_show_details Show note</a><a href='#' class=\"text hide\" onClick=\"$('.text$line').hide();$('.show-text$line').show();\">$LANG_hide_details Hide note</a>
+<a href='#' class="show-text$line" onClick="$('.text$line').show();$('.show-text$line').hide();">$LANG_show_details Show note</a><a href='#' class="text hide" onClick="$('.text$line').hide();$('.show-text$line').show();">$LANG_hide_details Hide note</a>
  </td></tr>
-<tr class=\"text$line hide\">
+<tr class="text$line hide">
         <td colspan=5 ><textarea input type=text name='line_item_description$line' rows=3 cols=80 WRAP=nowrap></textarea></td>
 </tr>
-
-";
+EOD;
 }
 
 
@@ -181,19 +184,22 @@ if (mysql_num_rows($result_tax) == 0) {
 
 } else {
         //has records, so display them
-        $display_block_tax = "
-        <select name=\"select_tax\">
+        $display_block_tax = <<<EOD
+        <select name="select_tax">
         
-	<option selected value=\"$def_taxField\" style=\"font-weight: bold\">$tax_descriptionField</option>
+	<option selected value="$def[def_tax]" style="font-weight: bold">$tax_descriptionField</option>
         
-	<option value=\"\"></option>";
+	<option value=""></option>
+EOD;
 
         while ($recs_tax = mysql_fetch_array($result_tax)) {
                 $id_tax = $recs_tax['tax_id'];
                 $display_name_tax = $recs_tax['tax_description'];
 
-                $display_block_tax .= "<option value=\"$id_tax\">
-                        $display_name_tax</option>";
+                $display_block_tax .= <<<EOD
+                <option value="$id_tax">
+                        $display_name_tax</option>
+EOD;
         }
 }
 
@@ -205,19 +211,22 @@ if (mysql_num_rows($result_preferences) == 0) {
 
 } else {
         //has records, so display them
-        $display_block_preferences = "
-        <select name=\"select_preferences\">
+        $display_block_preferences = <<<EOD
+        <select name="select_preferences">
         
-        <option selected value=\"$def_inv_preferenceField\" style=\"font-weight: bold\">$pref_descriptionField</option>
+        <option selected value="$def[def_inv_preference]" style="font-weight: bold">$pref_descriptionField</option>
 	
-	<option value=\"\"></option>";
+	<option value=""></option>
+EOD;
 
         while ($recs_preferences = mysql_fetch_array($result_preferences)) {
                 $id_preferences = $recs_preferences['pref_id'];
                 $display_name_preferences = $recs_preferences['pref_description'];
 
-                $display_block_preferences .= "<option value=\"$id_preferences\">
-                        $display_name_preferences</option>";
+                $display_block_preferences .= <<<EOD
+                <option value="$id_preferences">
+                        $display_name_preferences</option>
+EOD;
         }
 }
 
@@ -305,7 +314,7 @@ $show_custom_field_4 = show_custom_field(invoice_cf4,'',write,'',details_screen,
 		$dynamic_line_items = $_GET['get_num_line_items'];
 		} 
 	else {
-		$dynamic_line_items = $def_number_line_itemsField ;
+		$dynamic_line_items = $def['def_number_line_items'] ;
 	}	
 
 	$num = 0;
