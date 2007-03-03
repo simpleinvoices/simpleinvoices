a438f3f8be24e3b2475f4c4fa9a9cd428ccd732b
Justin Kelly <justin@kelly.org.au>
2007-03-03 23:38:14 +0000

    commiting the new consulting invoice with template

--- src/invoices/consulting.php
+++ src/invoices/consulting.php
@@ -44,40 +44,36 @@ $result_preferences = mysql_query($sql_preferences, $conn) or die(mysql_error())
 $sql_defaults = "SELECT * FROM si_defaults";
 $result_defaults = mysql_query($sql_defaults, $conn) or die(mysql_error());
 
+#defaults Array
 $def = mysql_fetch_array($result_defaults);
+
 
 #Get the names of the defaults from their id -start
 #default biller name query
 $sql_biller_default = "SELECT b_name FROM si_biller where b_id = $def[def_biller] and b_enabled != 0";
 $result_biller_default = mysql_query($sql_biller_default , $conn) or die(mysql_error());
 
-while ($Array = mysql_fetch_array($result_biller_default) ) {
-                $sql_biller_defaultField = $Array['b_name'];
-}
+$biller= mysql_fetch_array($result_biller_default);
 
 #default customer name query
 $print_customer = "SELECT * FROM si_customers WHERE c_id = $def[def_customer] and c_enabled != 0";
 $result_print_customer = mysql_query($print_customer, $conn) or die(mysql_error());
 
-while ($Array_customer = mysql_fetch_array($result_print_customer)) {
-       $c_nameField = $Array_customer['c_name'];
-}
+$customer = mysql_fetch_array($result_print_customer);
+
 
 #default tax description query
 $print_tax = "SELECT * FROM si_tax WHERE tax_id = $def[def_tax] and tax_enabled != 0";
 $result_print_tax = mysql_query($print_tax, $conn) or die(mysql_error());
 
-while ($Array_tax = mysql_fetch_array($result_print_tax)) {
-       $tax_descriptionField = $Array_tax['tax_description'];
-}
+$tax = mysql_fetch_array($result_print_tax);
 
 #default invoice preference description query
 $print_inv_preference = "SELECT * FROM si_preferences WHERE pref_id = $def[def_inv_preference] and pref_enabled != 0";
 $result_inv_preference = mysql_query($print_inv_preference, $conn) or die(mysql_error());
 
-while ($Array_inv_preference = mysql_fetch_array($result_inv_preference)) {
-       $pref_descriptionField = $Array_inv_preference['pref_description'];
-}
+$pref =  mysql_fetch_array($result_inv_preference);
+
 
 #Get the names of the defaults from their id -end
 #default biller name query
@@ -93,19 +89,12 @@ if (mysql_num_rows($result) == 0) {
         //has records, so display them
         $display_block = <<<EOD
         <select name="sel_id">
-        <option selected value="$def[def_biller]" style="font-weight: bold">$sql_biller_defaultField</option>
+        <option selected value="$def[def_biller]" style="font-weight: bold">$biller[b_name]</option>
         <option value=""></option>
 EOD;
-
-        while ($recs = mysql_fetch_array($result)) {
-                $id = $recs['b_id'];
-                $display_name = $recs['b_name'];
-
-                $display_block .= <<<EOD
-                <option value="$id">
-                        $display_name</option>
-EOD;
-        }
+		while ($recs = mysql_fetch_array($result)) {
+			$display_block .= "<option value=".$recs['b_id'].">".$recs['b_name']."</option>";
+		}
 }
 
 
@@ -120,17 +109,15 @@ if (mysql_num_rows($result_customer) == 0) {
         //has records, so display them
         $display_block_customer = <<<EOD
         <select name="select_customer">
-        <option selected value="$der[def_customer]" style="font-weight: bold">$c_nameField</option>
+        <option selected value="$der[def_customer]" style="font-weight: bold">$customer[c_name]</option>
         <option value=""></option>
 EOD;
 
-        while ($recs_customer = mysql_fetch_array($result_customer)) {
-                $id_customer = $recs_customer['c_id'];
-                $display_name_customer = $recs_customer['c_name'];
+	while ($recs_customer = mysql_fetch_array($result_customer)) {
 
                 $display_block_customer .= <<<EOD
-                <option value=$id_customer">
-                        $display_name_customer</option>
+                <option value=$recs_customer[c_id]">
+                        $recs_customer[c_name]</option>
 EOD;
         }
 }
@@ -143,6 +130,8 @@ function line_items($line) {
 
         $sql_products = "SELECT * FROM si_products where prod_enabled != 0 ORDER BY prod_description";
         $result_products = mysql_query($sql_products, $conn) or die(mysql_error());
+        
+        $string = "";
 
 	if (mysql_num_rows($result_products) == 0) {
 	        //no records
@@ -156,30 +145,27 @@ function line_items($line) {
 EOD;
 
         	while ($recs_products = mysql_fetch_array($result_products)) {
-    	               $id_products = $recs_products['prod_id'];
-       		       $display_name_products = $recs_products['prod_description'];
 
 	                $display_block_products .= <<<EOD
-	                <option value="$id_products">
-        	                $display_name_products</option>
+	                <option value="$recs_products[prod_id]">
+        	                $recs_products[prod_description]</option>
 EOD;
 	        }
 	        }
-                echo <<< EOD
+	        
+                $string .= <<< EOD
                 <tr>
                 <td>
 			<input type=text name='i_quantity$line' size=5></td><td input type=text name='i_description$line' size=50>$display_block_products 
 		</td>
-		<!--
-		<td>
-			<a href='#' class="show-text$line" onClick="$('.text$line').show();$('.show-text$line').hide();">$LANG_show_details Show note</a><a href='#' class="text hide" onClick="$('.text$line').hide();$('.show-text$line').show();">$LANG_hide_details Hide note</a>
-		</td>
-		-->
+
 		</tr>
 <tr class="text$line hide">
         <td colspan=2 ><textarea input type=text name='line_item_description$line' rows=3 cols=80 WRAP=nowrap></textarea></td>
 </tr>
 EOD;
+
+return $string;
 }
 
 
@@ -194,18 +180,15 @@ if (mysql_num_rows($result_tax) == 0) {
         $display_block_tax = <<<EOD
         <select name="select_tax">
         
-	<option selected value="$def[def_tax]" style="font-weight: bold">$tax_descriptionField</option>
+	<option selected value="$def[def_tax]" style="font-weight: bold">$tax[tax_description]</option>
         
 	<option value=""></option>
 EOD;
 
         while ($recs_tax = mysql_fetch_array($result_tax)) {
-                $id_tax = $recs_tax['tax_id'];
-                $display_name_tax = $recs_tax['tax_description'];
-
                 $display_block_tax .= <<<EOD
-                <option value="$id_tax">
-                        $display_name_tax</option>
+                <option value="$recs_tax[tax_id]">
+                        $recs_tax[tax_description]</option>
 EOD;
         }
 }
@@ -221,18 +204,15 @@ if (mysql_num_rows($result_preferences) == 0) {
         $display_block_preferences = <<<EOD
         <select name="select_preferences">
         
-        <option selected value="$def[def_inv_preference]" style="font-weight: bold">$pref_descriptionField</option>
+        <option selected value="$def[def_inv_preference]" style="font-weight: bold">$pref[pref_description]</option>
 	
 	<option value=""></option>
 EOD;
 
         while ($recs_preferences = mysql_fetch_array($result_preferences)) {
-                $id_preferences = $recs_preferences['pref_id'];
-                $display_name_preferences = $recs_preferences['pref_description'];
-
                 $display_block_preferences .= <<<EOD
-                <option value="$id_preferences">
-                        $display_name_preferences</option>
+                <option value=" $recs_preferences[pref_id]">
+                        $recs_preferences[pref_description]</option>
 EOD;
         }
 }
@@ -245,80 +225,9 @@ $show_custom_field_2 = show_custom_field(invoice_cf2,'',write,'',details_screen,
 $show_custom_field_3 = show_custom_field(invoice_cf3,'',write,'',details_screen,'','','');
 $show_custom_field_4 = show_custom_field(invoice_cf4,'',write,'',details_screen,'','','');
 
-
-?>
-
+include('./config/config.php');
 
 
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<script language="javascript" type="text/javascript" src="include/tiny_mce/tiny_mce_src.js"></script>
-<script language="javascript" type="text/javascript" src="include/tiny-mce.conf.js"></script>
-
-<link rel="stylesheet" type="text/css" href="include/jquery.datePicker.css" title="default" media="screen" />
-<link rel="stylesheet" href="./src/include/css/ibox.css" type="text/css"  media="screen"/>
-
-<script type="text/javascript" src="include/jquery.js"></script>
-<script type="text/javascript" src="include/jquery.dom_creator.js"></script>
-<script type="text/javascript" src="include/jquery.datePicker.js"></script>
-<script type="text/javascript" src="include/jquery.datePicker.conf.js"></script>
-<script type="text/javascript" src="./src/include/js/ibox.js"></script>
-
-<!--
-    <script type="text/javascript">
-	$(document).ready(function() {
-	 // hides the customer and biller details as soon as the DOM is ready (a little sooner that page load)
-	  $('.hide').hide();
-  	});
-    </script>
--->
-
-</head>
-	<title><?php echo $title; echo " :: "; echo $LANG_inv; echo $LANG_inv_consulting; ?></title>
-<?php include('./config/config.php'); ?>
-<BODY>
-
-<FORM name="frmpost" ACTION="index.php?module=invoices&view=save" METHOD=POST onsubmit="return frmpost_Validator(this)">
-
-<b><?php echo $LANG_inv; echo $LANG_inv_consulting; ?></b>
-<hr></hr>
-
-<table align=center>
-<tr>
-	<td class="details_screen">
-		<?php echo $mb_table_biller_name; ?>
-	</td>
-	<td input type=text name="biller_block" size=25>
-		<?php echo $display_block; ?>
-	</td>
-</tr>
-<tr>
-	<td class="details_screen">
-		<?php echo $mc_table_customer_name; ?>
-	</td>
-	<td input type=text name="customer_block" size=25 >
-		<?php echo $display_block_customer; ?>
-	</td>
-</tr>
-<tr>
-        <td class="details_screen"><?php echo $LANG_date_formatted; ?></td>
-        <td>
-                        <input type="text" class="date-picker" name="select_date" id="date1" value='<?php echo $today = date("Y-m-d"); ?>'></input>
-        </td>
-</tr>
-
-
-<tr>
-	<td class="details_screen">
-		<?php echo $LANG_quantity;?>
-	</td>
-	<td class="details_screen">
-		<?php echo $LANG_description;?>
-	</td>
-</tr>
-<?php
 /* check the def number of line items and do the print and entry field for that number of items */
    /*Preparation work fro being able to dynamically add line items during an itemised invoice  */
 	/*get the number of line items from the GET or if not set from the default in the database */
@@ -330,56 +239,32 @@ $show_custom_field_4 = show_custom_field(invoice_cf4,'',write,'',details_screen,
 	}	
 
 	$num = 0;
-
-        while ($num < $dynamic_line_items ) :
-                echo line_items($num);
-                echo "<a/td></tr>";
-                        $num++;
-
-                        endwhile;
-?>
-
-
-<?php 
-	echo $show_custom_field_1;
-	echo $show_custom_field_2;
-	echo $show_custom_field_3;
-	echo $show_custom_field_4;
-?>
-<tr>
-        <td colspan=2 class="details_screen"><?php echo $LANG_notes;?></td>
-</tr>
-
-<tr>
-        <td colspan=2 ><textarea input type=text name="invoice_consulting_note" rows=5 cols=80 WRAP=nowrap></textarea></td>
-</tr>
-
-<tr>
-	<td class="details_screen"><?php echo $LANG_tax;?></td><td input type=text name="inv_it_tax" size=15> <?php echo $display_block_tax; ?></td>
-</tr>
-
-<tr>
-	<td class="details_screen"><?php echo $LANG_inv_pref;?></td><td input type=text name="inv_preferences"><?php echo $display_block_preferences; ?></td>
-</tr>
-<tr>
-	<td align=left colspan=2> 
-		<a href="./documentation/info_pages/invoice_custom_fields.html" rel="ibox&height=400"><?php echo $LANG_want_more_fields; ?><img src="./images/common/help-small.png"></img></a>
-
-	</td>
 	
-</tr>
-<!--Add more line items while in an itemeised invoice - Get style - has problems- wipes the current values of the existing rows - not good
-<tr>
-<td>
-<a href="?get_num_line_items=10">Add 5 more line items<a>
-</tr>
--->
-</table>
-<!-- </div> -->
-<hr></hr>
-		<input type=hidden name="max_items" value="<?php echo $num; ?>">
-		<input type=submit name="submit" value="<?php echo $LANG_save;echo " "; echo $LANG_inv;?>">
-		<input type=hidden name="invoice_style" value="insert_invoice_consulting">
+	$lineitems = "";
 
-</FORM>
-<!-- ./src/include/design/footer.inc.php gets called here by controller srcipt -->
+        while ($num < $dynamic_line_items ) {
+                $lineitems .= line_items($num);
+                $lineitems .= "</td></tr>";
+                $num++;
+
+        }
+        
+     
+        
+        
+
+            
+    $today = date("Y-m-d");
+    
+    
+    
+    $temp = file_get_contents('./src/invoices/consulting.tpl');
+	$temp = addslashes($temp);	$content = "";
+	
+	eval ('$content = "'.$temp.'";');
+	
+	echo $content;
+	
+	
+
+?>
---
+++ src/invoices/consulting.tpl
@@ -0,0 +1,113 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script language="javascript" type="text/javascript" src="include/tiny_mce/tiny_mce_src.js"></script>
+<script language="javascript" type="text/javascript" src="include/tiny-mce.conf.js"></script>
+
+<link rel="stylesheet" type="text/css" href="include/jquery.datePicker.css" title="default" media="screen" />
+<link rel="stylesheet" href="./src/include/css/ibox.css" type="text/css"  media="screen"/>
+
+<script type="text/javascript" src="include/jquery.js"></script>
+<script type="text/javascript" src="include/jquery.dom_creator.js"></script>
+<script type="text/javascript" src="include/jquery.datePicker.js"></script>
+<script type="text/javascript" src="include/jquery.datePicker.conf.js"></script>
+<script type="text/javascript" src="./src/include/js/ibox.js"></script>
+
+<!--
+    <script type="text/javascript">
+	$(document).ready(function() {
+	 // hides the customer and biller details as soon as the DOM is ready (a little sooner that page load)
+	  $('.hide').hide();
+  	});
+    </script>
+-->
+
+</head>
+	<title>$title ::  $LANG_inv $LANG_inv_consulting></title>
+<body>
+
+<form name="frmpost" action="index.php?module=invoices&view=save" method="post" onsubmit="return frmpost_Validator(this)">
+
+<b><$LANG_inv $LANG_inv_consulting</b>
+<hr></hr>
+
+<table align=center>
+<tr>
+	<td class="details_screen">
+		$mb_table_biller_name
+	</td>
+	<td input type=text name="biller_block" size=25>
+		$display_block
+	</td>
+</tr>
+<tr>
+	<td class="details_screen">
+		$mc_table_customer_name
+	</td>
+	<td input type=text name="customer_block" size=25 >
+		$display_block_customer
+	</td>
+</tr>
+<tr>
+        <td class="details_screen">$LANG_date_formatted</td>
+        <td>
+                        <input type="text" class="date-picker" name="select_date" id="date1" value="$today"></input>
+        </td>
+</tr>
+
+
+<tr>
+	<td class="details_screen">
+		$LANG_quantity
+	</td>
+	<td class="details_screen">
+		$LANG_description
+	</td>
+</tr>
+
+	$lineitems
+
+ 
+	 $show_custom_field_1
+	 $show_custom_field_2
+	 $show_custom_field_3
+	 $show_custom_field_4
+
+<tr>
+        <td colspan=2 class="details_screen">$LANG_notes</td>
+</tr>
+
+<tr>
+        <td colspan=2 ><textarea input type=text name="invoice_consulting_note" rows=5 cols=80 WRAP=nowrap></textarea></td>
+</tr>
+
+<tr>
+	<td class="details_screen">$LANG_tax</td><td input type=text name="inv_it_tax" size=15>$display_block_tax</td>
+</tr>
+
+<tr>
+	<td class="details_screen">$LANG_inv_pref</td><td input type=text name="inv_preferences">$display_block_preferences</td>
+</tr>
+<tr>
+	<td align=left colspan=2> 
+		<a href="./documentation/info_pages/invoice_custom_fields.html" rel="ibox&height=400">$LANG_want_more_fields<img src="./images/common/help-small.png"></img></a>
+
+	</td>
+	
+</tr>
+<!--Add more line items while in an itemeised invoice - Get style - has problems- wipes the current values of the existing rows - not good
+<tr>
+<td>
+<a href="?get_num_line_items=10">Add 5 more line items<a>
+</tr>
+-->
+</table>
+<!-- </div> -->
+<hr></hr>
+		<input type=hidden name="max_items" value="$num">
+		<input type=submit name="submit" value="$LANG_save $LANG_inv">
+		<input type=hidden name="invoice_style" value="insert_invoice_consulting">
+
+</FORM>
+<!-- ./src/include/design/footer.inc.php gets called here by controller srcipt -->
