e1be21d659116ba53205a5ffd3298b6a01d60520
Nicolas Ruflin <ruflin@gmail.com>
2007-03-19 08:52:37 +0000

    biller manage-template added

--- src/billers/manage.php
+++ src/billers/manage.php
@@ -10,46 +10,15 @@ $sql = "SELECT * FROM {$tb_prefix}biller ORDER BY b_name";
 $result = mysql_query($sql, $conn) or die(mysql_error());
 $number_of_rows = mysql_num_rows($result);
 
-
+include("./templates/default/billers/manage.tpl");
 if (mysql_num_rows($result) == 0) {
-	$display_block = "<P><em>$mb_no_invoices.</em></p>";
+	$block = $display_block_vide;
 }
 else {
-$display_block = <<<EOD
-<b>$mb_page_header :: <a href='index.php?module=billers&view=add'>$mb_actions_new_biller</a></b>
- <hr></hr>
 
-
-<table class="ricoLiveGrid manage" id="rico_biller" align="center">
-<colgroup>
-<col style='width:15%;' />
-<col style='width:10%;' />
-<col style='width:40%;' />
-<!--
-<col style='width:10%;' />
-<col style='width:10%;' />
--->
-<col style='width:25%;' />
-<col style='width:10%;' />
-</colgroup>
-<thead>
-<tr class="sortHeader">
-<th class="noFilter sortable">$mb_table_action</th>
-<th class=" index_table sortable">$mb_table_biller_id</th>
-<th class="index_table sortable">$mb_table_biller_name</th>
-<!--
-<th class="index_table">$mb_table_phone</th>
-<th class="index_table">$mb_table_mobile_phone</th>
--->
-<th class="index_table sortable">$mb_table_email</th>
-<th class="noFilter index_table sortable">$wording_for_enabledField</th>
-</tr>
-</thead>
-EOD;
-
-
-
-while ($biller = mysql_fetch_array($result)) {
+	
+	$display_block_lines = "";
+	while ($biller = mysql_fetch_array($result)) {
 	
         if ($biller['b_enabled'] == 1) {
                 $wording_for_enabled = $wording_for_enabledField;
@@ -57,31 +26,15 @@ while ($biller = mysql_fetch_array($result)) {
                 $wording_for_enabled = $wording_for_disabledField;
         }
 
-
-
-	$display_block .= "
-	<tr class='index_table'>
-	<td class='index_table'>
-	<a class='index_table' href='index.php?module=billers&view=details&submit={$biller['b_id']}&action=view'>
-	{$mb_actions_view}</a>
-	 :: 
-	<a class='index_table' href='index.php?module=billers&view=details&submit={$biller['b_id']}&action=edit'>
-	{$mb_actions_edit}</a></td>
-	<td class='index_table'>{$biller['b_id']}</td>
-	<td class='index_table'>{$biller['b_name']}</td>
-	<!--
-	<td class='index_table'>{$biller['b_phone']}</td>
-	<td class='index_table'>{$biller['b_mobile_phone']}</td>
-	-->
-	<td class='index_table'>{$biller['b_email']}</td>
-	<td class='index_table'>{$wording_for_enabled}</td>
-	</tr>";
-
-                
+		include("./templates/default/billers/manage.tpl");
+		$display_block_lines .= $display_block_line;           
 	
-		}	
+	}
+	
+	include("./templates/default/billers/manage.tpl");
 
-        $display_block .="</table>";
+	$block = $display_block;
+
 }
 
 require("./src/include/js/lgplus/php/chklang.php");
@@ -89,5 +42,5 @@ require("./src/include/js/lgplus/php/settings.php");
 
 getRicoLiveGrid("rico_biller","{ type:'number', decPlaces:0, ClassName:'alignleft'}");
 	
-echo $display_block; 
+echo $block; 
 ?>
---
+++ templates/default/billers/manage.tpl
@@ -0,0 +1,56 @@
+<?php
+
+$display_block_line = <<<EOD
+	<tr class='index_table'>
+	<td class='index_table'>
+	<a class='index_table' href='index.php?module=billers&view=details&submit={$biller['b_id']}&action=view'>
+	{$mb_actions_view}</a>
+	 :: 
+	<a class='index_table' href='index.php?module=billers&view=details&submit={$biller['b_id']}&action=edit'>
+	{$mb_actions_edit}</a></td>
+	<td class='index_table'>{$biller['b_id']}</td>
+	<td class='index_table'>{$biller['b_name']}</td>
+	<!--
+	<td class='index_table'>{$biller['b_phone']}</td>
+	<td class='index_table'>{$biller['b_mobile_phone']}</td>
+	-->
+	<td class='index_table'>{$biller['b_email']}</td>
+	<td class='index_table'>{$wording_for_enabled}</td>
+	</tr>
+EOD;
+
+$display_block_vide = "<P><em>$mb_no_invoices.</em></p>";
+
+$display_block = <<<EOD
+	<b>$mb_page_header :: <a href='index.php?module=billers&view=add'>$mb_actions_new_biller</a></b>
+	<hr></hr>
+	<table class="ricoLiveGrid manage" id="rico_biller" align="center">
+<colgroup>
+<col style='width:15%;' />
+<col style='width:10%;' />
+<col style='width:40%;' />
+<!--
+<col style='width:10%;' />
+<col style='width:10%;' />
+-->
+<col style='width:25%;' />
+<col style='width:10%;' />
+</colgroup>
+<thead>
+<tr class="sortHeader">
+<th class="noFilter sortable">$mb_table_action</th>
+<th class=" index_table sortable">$mb_table_biller_id</th>
+<th class="index_table sortable">$mb_table_biller_name</th>
+<!--
+<th class="index_table">$mb_table_phone</th>
+<th class="index_table">$mb_table_mobile_phone</th>
+-->
+<th class="index_table sortable">$mb_table_email</th>
+<th class="noFilter index_table sortable">$wording_for_enabledField</th>
+</tr>
+</thead>
+$display_block_lines
+</table>
+EOD;
+
+?>
