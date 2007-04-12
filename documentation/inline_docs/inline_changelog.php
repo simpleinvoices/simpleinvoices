7e6275ae34e23caf27f888e31babc06953e3bd26
Nicolas Ruflin <ruflin@gmail.com>
2007-04-12 06:37:58 +0000

    documentation readded in the right place

---
+++ documentation/inline_docs/inline_changelog.php
@@ -0,0 +1,30 @@
+<?php
+
+include('./include/include_main.php');
+
+
+$fp = fopen( "./documentation/ChangeLog.html", "r" );
+if(!$fp)
+{
+    echo "Couldn't open the data file. Try again later.";
+    exit;
+}
+$filename ="./documentation/ChangeLog.html";
+$display_block = fread( $fp, filesize( $filename ) );
+?>
+
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<body>
+<b>Simple Invoices Change Log</b>
+ <hr></hr>
+       <div id="left">
+
+<?php 
+	echo $display_block;
+	fclose( $fp ); 
+?>
+</div>
+<!-- ./src/include/design/footer.inc.php gets called here by controller srcipt -->
---
+++ documentation/inline_docs/inline_license.php
@@ -0,0 +1,29 @@
+<?php
+
+include('./include/include_main.php');
+
+
+$fp = fopen( "./documentation/gpl.html", "r" );
+if(!$fp)
+{
+    echo "Couldn't open the data file. Try again later.";
+    exit;
+}
+$filename ="./documentation/gpl.html";
+$display_block = fread( $fp, filesize( $filename ) );
+?>
+
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php include('./config/config.php'); ?>
+<body>
+       <div id="left">
+
+<?php 
+	echo $display_block;
+	fclose( $fp );
+?>
+</div>
+<!-- ./src/include/design/footer.inc.php gets called here by controller srcipt -->
