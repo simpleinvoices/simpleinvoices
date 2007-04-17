ddbb9dc9eeb320d3ab81505f4b1d27e00d60bb81
Justin Kelly <justin@kelly.org.au>
2007-04-17 11:46:27 +0000

    md5 code added but turned off for now

--- login.php
+++ login.php
@@ -111,7 +111,7 @@ if($ChallengeLife>0) {
 
 <?php if($MD5Auth==True){?>
 
-    <script src="/src/include/md5-2.2alpha.js"></script>
+    <script src="./include/md5/md5-2.2alpha.js"></script> -->
     <script language="JavaScript"><!--
 
     function login(f) {
