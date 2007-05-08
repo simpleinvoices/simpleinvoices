335ff28a601f57f7b0b5c6d38e11d56e2c0a0ede
Nicolas Ruflin <ruflin@gmail.com>
2007-05-08 18:44:31 +0000

    removed manage.inc.php file (why this one was introduced?) changed overdue day to exact date. Would negative days make sense to sort invoices by something like "priority"?

--- modules/customers/manage.php
+++ modules/customers/manage.php
@@ -9,7 +9,7 @@ $sql = "SELECT * FROM {$tb_prefix}customers ORDER BY name";
 $result = mysql_query($sql, $conn) or die(mysql_error());
 
 $customers = null;
-$invoices = null;
+//$invoices = null;
 
 for($i=0;$customer = mysql_fetch_array($result);$i++) {
 	
@@ -34,13 +34,13 @@ for($i=0;$customer = mysql_fetch_array($result);$i++) {
 		#amount owing calc - end
 		
 		$customers[$i] = $customer;
-		$invoices[$i] = $invoice;
+		//$invoices[$i] = $invoice;
 
 }
 
 $smarty -> assign("result",$result);
 $smarty -> assign("customers",$customers);
-$smarty -> assign("invoices",$invoices);
+//$smarty -> assign("invoices",$invoices);
 
 getRicoLiveGrid("rico_customer","	{ type:'number', decPlaces:0, ClassName:'alignleft' },,{ type:'number', decPlaces:2, ClassName:'alignleft' },{ type:'number', decPlaces:2, ClassName:'alignleft' }");
 
