159598e4e12ef277e45de11d525d901638b64df4
Nicolas Ruflin <ruflin@gmail.com>
2007-06-15 10:16:11 +0000

    automatic path reading for pdf's

--- include/include_main.php
+++ include/include_main.php
@@ -4,6 +4,9 @@
 require_once("./modules/include/js/lgplus/php/chklang.php");
 require_once("./modules/include/js/lgplus/php/settings.php");
 
+$path = pathinfo($_SERVER['REQUEST_URI']);
+$install_path = $path['dirname'];
+
 if (isset($_GET['location']) && $_GET['location'] === 'pdf' ) {
 	include('./include/include_auth.php');
 }
