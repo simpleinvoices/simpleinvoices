c71ce0b7b316097b6e4e23a7046b244695fce42c
Justin Kelly <justin@kelly.org.au>
2007-07-12 05:45:14 +0000

    report fix

--- modules/reports/report_debtors_owing_by_customer.php
+++ modules/reports/report_debtors_owing_by_customer.php
@@ -23,18 +23,22 @@ if (!defined("BROWSE")) {
    include "./modules/reports/PHPReportMaker.php";
    include "config/config.php";
 
+
    $sSQL = "SELECT
-        ".TB_PREFIX."customers.id as CID,
-        ".TB_PREFIX."customers.name as Customer,
-        (select sum(".TB_PREFIX."invoice_items.total) from ".TB_PREFIX."invoice_items,".TB_PREFIX."invoices where  ".TB_PREFIX."invoice_items.invoice_id = ".TB_PREFIX."invoices.id and ".TB_PREFIX."invoices.customer_id = ".TB_PREFIX."customers.id) as INV_TOTAL,
-        (select IF ( isnull(sum(ac_amount)), '0', sum(ac_amount)) from ".TB_PREFIX."account_payments,".TB_PREFIX."invoices where ".TB_PREFIX."account_payments.ac_inv_id = ".TB_PREFIX."invoices.id and ".TB_PREFIX."invoices.customer_id = ".TB_PREFIX."customers.id) as INV_PAID,
+        {$tb_prefix}customers.id as CID,
+        {$tb_prefix}customers.name as Customer,
+        (select IF ( isnull(sum({$tb_prefix}invoice_items.total)), '0', sum({$tb_prefix}invoice_items.total)) from {$tb_prefix}invoice_items,{$tb_prefix}invoices where  {$tb_prefix}invoice_items.invoice_id = {$tb_prefix}invoices.id and {$tb_prefix}invoices.customer_id = CID) as INV_TOTAL,
+        (select IF ( isnull(sum(ac_amount)), '0', sum(ac_amount)) from {$tb_prefix}account_payments,{$tb_prefix}invoices where {$tb_prefix}account_payments.ac_inv_id = {$tb_prefix}invoices.id and {$tb_prefix}invoices.customer_id = CID) as INV_PAID,
         (select (INV_TOTAL - INV_PAID)) as INV_OWING
 
 FROM
-        ".TB_PREFIX."customers,".TB_PREFIX."invoices,".TB_PREFIX."invoice_items
+        {$tb_prefix}customers,{$tb_prefix}invoices,{$tb_prefix}invoice_items
 WHERE
-        ".TB_PREFIX."invoice_items.invoice_id = ".TB_PREFIX."invoices.id and ".TB_PREFIX."invoices.customer_id = ".TB_PREFIX."customers.id
+        {$tb_prefix}invoice_items.invoice_id = {$tb_prefix}invoices.id and {$tb_prefix}invoices.customer_id = {$tb_prefix}customers.id
+
 GROUP BY
+        {$tb_prefix}customers.id
+ORDER BY
         INV_OWING DESC;
 
 
