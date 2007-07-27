e261f977c531ed85daab232a182406d3ae87fa1e
Justin Kelly <justin@kelly.org.au>
2007-07-27 03:16:34 +0000

    invoice delete stuff - starting to be added

---
+++ modules/invoices/delete.php
@@ -0,0 +1,59 @@
+<?php
+/*
+* Script: delete.php
+* 	Do the deletion of an invoice page
+*
+* Authors:
+*	 Justin Kelly, Nicolas Ruflin
+*
+* Last edited:
+* 	 2007-07-27
+*
+* License:
+*	 GPL v2 or above
+*
+* Website:
+* 	http://www.simpleinvoices.org
+ */
+//stop the direct browsing to this file - let index.php handle which files get displayed
+checkLogin();
+
+
+$billers = getActiveBillers();
+$customers = getActiveCustomers();
+$taxes = getTaxes();
+$products = getActiveProducts();
+$preferences = getActivePreferences();
+$defaults = getSystemDefaults();
+
+$invoicePayments = getInvoicePayments($id)
+
+$defaultBiller = getDefaultBiller();
+$defaultCustomer = getDefaultCustomer();
+$defaultTax = getDefaultTax();
+$defaultPreference = getDefaultPreference();
+
+
+if (!empty( $_GET['get_num_line_items'] )) {
+	$dynamic_line_items = $_GET['get_num_line_items'];
+} 
+else {
+	$dynamic_line_items = $defaults['line_items'] ;
+}
+
+for($i=1;$i<=4;$i++) {
+	$show_custom_field[$i] = show_custom_field("invoice_cf$i",'',"write",'',"details_screen",'','','');
+}
+
+
+$smarty -> assign("billers",$billers);
+$smarty -> assign("customers",$customers);
+$smarty -> assign("taxes",$taxes);
+$smarty -> assign("products",$products);
+$smarty -> assign("preferences",$preferences);
+$smarty -> assign("dynamic_line_items",$dynamic_line_items);
+$smarty -> assign("show_custom_field",$show_custom_field);
+
+$smarty -> assign("defaults",$defaults);
+
+?>
