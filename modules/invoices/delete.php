b94f4c0ae5479c95188dce972da5daccb56dcb0c
Justin Kelly <justin@kelly.org.au>
2007-07-27 04:45:08 +0000

    delete invoice stuff - plenty to go :)

--- modules/invoices/delete.php
+++ modules/invoices/delete.php
@@ -18,42 +18,22 @@
 //stop the direct browsing to this file - let index.php handle which files get displayed
 checkLogin();
 
+#get the invoice id
+$invoice_id = $_GET['invoice'];
 
-$billers = getActiveBillers();
-$customers = getActiveCustomers();
-$taxes = getTaxes();
-$products = getActiveProducts();
-$preferences = getActivePreferences();
+$invoice = getInvoice($invoice_id);
+$preference = getPreference($invoice['preference_id']);
+
 $defaults = getSystemDefaults();
 
-$invoicePayments = getInvoicePayments($id)
-
-$defaultBiller = getDefaultBiller();
-$defaultCustomer = getDefaultCustomer();
-$defaultTax = getDefaultTax();
-$defaultPreference = getDefaultPreference();
+$invoicePayments = getInvoicePayments($invoice_id);
 
 
-if (!empty( $_GET['get_num_line_items'] )) {
-	$dynamic_line_items = $_GET['get_num_line_items'];
-} 
-else {
-	$dynamic_line_items = $defaults['line_items'] ;
-}
 
-for($i=1;$i<=4;$i++) {
-	$show_custom_field[$i] = show_custom_field("invoice_cf$i",'',"write",'',"details_screen",'','','');
-}
-
-
-$smarty -> assign("billers",$billers);
-$smarty -> assign("customers",$customers);
-$smarty -> assign("taxes",$taxes);
-$smarty -> assign("products",$products);
-$smarty -> assign("preferences",$preferences);
-$smarty -> assign("dynamic_line_items",$dynamic_line_items);
-$smarty -> assign("show_custom_field",$show_custom_field);
+$smarty -> assign("invoice",$invoice);
+$smarty -> assign("preference",$preference);
 
 $smarty -> assign("defaults",$defaults);
+$smarty -> assign("invoicePayments",$invoicePayments);
 
 ?>
