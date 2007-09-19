a6b106cabb8b6dc5c83e71f515783d542fe11f20
Nicolas Ruflin <ruflin@gmail.com>
2007-09-19 21:28:35 +0000

    now we got 1'000 commits in our svn repo. A lot of more commits will follow...

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ if(file_exists("./templates/default/$file.tpl")) {
 }
 // If no smarty template - add message - onyl uncomment for dev - commented out for release
 else {
-	error_log("NOTEMPLATE!!");
+	error_log("NOTEMPLATE!!!");
 }
 
 $smarty -> display("../templates/default/footer.tpl");
