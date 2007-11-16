e660681b439b4f089fd6d0cd9f737657e43e7d9b
Justin Kelly <justin@kelly.org.au>
2007-11-16 09:07:01 +0000

    lang cleanup

--- documentation/en-gb/stuff/about.php
+++
@@ -1,9 +0,0 @@
-
-<b>About</b>
- <hr></hr>
-       <div id="left">
-<br>
-Simple Invoices:
-<br>Version: <?php echo($version); ?>
-<br>Theme: <?php echo($theme); ?>
-<br>Homepage: <a href='http://www.simpleinvoices.org'>http://www.simpleinvoices.org</a>
