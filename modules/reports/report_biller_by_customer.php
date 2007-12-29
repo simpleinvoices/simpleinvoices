b9848ef4756504b207659be7e57c44bdba7c7f98
apamuthu <apamuthu@gmail.com>
2007-12-29 09:51:16 +0000

    sql correction

--- modules/reports/report_biller_by_customer.php
+++ modules/reports/report_biller_by_customer.php
@@ -4,12 +4,11 @@ require_once("./include/reportlib.php");
 
 // Adjusted for NULL on computation with NULL values
 
-
 	$sSQL  = "SELECT sum(ivt.total) as SUM_TOTAL, b.name as Biller, c.name as Customer ";
 	$sSQL .= "FROM ".TB_PREFIX."biller b, ".TB_PREFIX."customers c, ";
 	$sSQL .= 		 TB_PREFIX."invoice_items ivt, ".TB_PREFIX."invoices iv ";
 	$sSQL .= "WHERE iv.customer_id = c.id AND iv.biller_id = b.id AND iv.id = ivt.invoice_id ";
-	$sSQL .= "GROUP BY ivt.total ORDER BY b.name";
+	$sSQL .= "GROUP BY b.name, c.name";
 	
 	$oRpt->setXML("./modules/reports/report_biller_by_customer.xml");
 
