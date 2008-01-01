66d8d39a9cb5f07f80de62dcaee33afe0e40754e
Justin Kelly <justin@kelly.org.au>
2008-01-01 00:26:03 +0000

    db layer thing for reports

--- include/reportlib.php
+++ include/reportlib.php
@@ -21,18 +21,23 @@ if (!defined("BROWSE")) {
 	$oRpt->setPassword($db_password);
 	$oRpt->setDatabase($db_name);
 	
+
+if($db_layer == "")
+{
 // Non PDO usage
 	$oRpt->setConnection($db_host); 
 	$oRpt->setDatabaseInterface($db_server);
 // End Non PDO usage
-
+}
+if($db_layer == "pdo")
+{
 // PDO Usage
-//   $oRpt->setDatabaseInterface("pdo");
-//   if ($db_server == 'pgsql') {
-//      $oRpt->setConnection("pgsql:host=$db_host");
-//   } else {
-//      $oRpt->setConnection("mysql:host=$db_host");
-//   }
+   $oRpt->setDatabaseInterface("pdo");
+   if ($db_server == 'pgsql') {
+      $oRpt->setConnection("pgsql:host=$db_host");
+   } else {
+      $oRpt->setConnection("mysql:host=$db_host");
+   }
 // End PDO Usage
-
+}
 ?>
