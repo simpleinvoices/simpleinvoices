0c6cfcb45a6ab5d1f2ebc9d2e1ca4ba510390bca
Justin Kelly <justin@kelly.org.au>
2008-06-07 12:21:05 +0000

    fix edit invoice- add items problem

--- modules/invoices/add_invoice_item.php
+++ modules/invoices/add_invoice_item.php
@@ -18,7 +18,7 @@
 
 
 if(isset($_POST['submit'])) {
-	insertInvoiceItem($_POST['invoice_id'],$_POST['quantity'],$_POST['product'],$_POST['tax_id'],$_POST['description']);
+	invoice::insertInvoiceItem($_POST['invoice_id'],$_POST['quantity'],$_POST['product'],$_POST['tax_id'],$_POST['description']);
 }
 else {
 
