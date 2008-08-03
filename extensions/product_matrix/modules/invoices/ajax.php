cd773553b601b121cc622054bbd41d08905f46bc
Justin Kelly <justin@kelly.org.au>
2008-08-03 08:00:00 +0000

    sync

--- extensions/product_matrix/modules/invoices/ajax.php
+++ extensions/product_matrix/modules/invoices/ajax.php
@@ -7,7 +7,7 @@ $json = array();
 // Lets get the variables we passed
 $search = isset($_GET['search']) ? (string) $_GET['search'] : '';
 
-$str = isset($_GET['str']) ? (string) $_GET['str'] : '';
+$str = isset($_GET['_value']) ? (string) $_GET['_value'] : '';
 
 // Cleans query string before we run the selects below.
 // Allows A-Z, a-z, 0-9, whitespace, minus/hyphen, equals, ampersand, underscore, and period/full stop.
@@ -19,27 +19,32 @@ switch ($search) {
 
 
 	  $sql = "select 
-					CONCAT(a.id, '-', v.id) as id, 
+					CONCAT(p.id, '-', a.id, '-', v.id) as id, 
 					CONCAT(a.name, '-',v.value) as display 
 				from 
 					si_products_attributes a, 
-					si_products_values v 
+					si_products_values v,
+					si_products_matrix m,
+					si_products p
+
 				where 
-					a.id = v.attribute_id 
-					AND 
-					a.id= '{$str}'
+					p.id = m.product_id 
+					and 
+					a.id = m.attribute_id 
+					and 
+					v.attribute_id = a.id 
+					and
+					p.id= '{$str}'
 				";
 
 	  $sth =  dbQuery($sql);
 //	  $matrix = $sth->fetchAll();
 
 
-      while ($sth->nextRowset()) {
-
-         $json[] = '{"optionValue": "'.$db->f("id").'", "optionDisplay": "'.$db->f("display").'"}';
+		foreach($sth as $row) {
+         $json[] =  array($row['id'] => $row['display']);
       }
-      echo '[' . implode(',', $json) . ']';
-
+	  echo json_encode( $json );
    break;
 
    case "state":
@@ -47,27 +52,39 @@ switch ($search) {
 	$str_before = substr($str, 0, strpos($str, '-'));     
 	$str_after = substr($str, -1, strpos($str, '-'));      
 	
-		$db->query("select 
+		$sql = "select 
 					CONCAT(a.id, '-', v.id) as id, 
 					CONCAT(a.name, '-',v.value) as display 
 				from 
+					si_products_matrix m, 
 					si_products_attributes a, 
+					si_products p, 
 					si_products_values v 
 				where 
-					a.id = v.attribute_id 
-					AND 
-					a.id= '{$str_before}'
-					AND
+					p.id = m.product_id 
+					and 
+					a.id = m.attribute_id 
+					and 
+					v.attribute_id = a.id 
+					and 
+					p.id = '{$str_before}'
+					and
 					v.id != '{$str_after}'
-				");
-
+				";
+		$sth =  dbQuery($sql);
+		foreach($sth as $row) {
+         $json[] =  array($row['id'] => $row['display']);
+      }
+	  echo json_encode( $json );
+/*
       while ($db->next_record()) {
 
          $json[] = '{"optionValue": "'.$db->f("id").'", "optionDisplay": "'.$db->f("display").'"}';
       }
 
-      echo '[' . implode(',', $json) . ']';
+	  echo json_encode( $json );
 
+	*/
    break;
 
    default:
