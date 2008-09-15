ccb123206326708e492845179280df170a5c14b8
Justin Kelly <justin@kelly.org.au>
2008-09-15 10:39:46 +0000

    grid udpate

--- modules/billers/xml.php
+++ modules/billers/xml.php
@@ -64,8 +64,8 @@ $xml .= "<total>$count</total>";
 foreach ($customers as $row) {
 	$xml .= "<row id='".$row['iso']."'>";
 	$xml .= "<cell><![CDATA[
-	<a class='index_table' title='$LANG[view] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-	<a class='index_table' title='$LANG[edit] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[id]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+	<a class='index_table' title='$LANG[view] ".utf8_encode($row['name'])."' href='index.php?module=billers&view=details&id=$row[id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+	<a class='index_table' title='$LANG[edit] ".utf8_encode($row['name'])."' href='index.php?module=billers&view=details&id=$row[id]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
 	]]></cell>";
 	$xml .= "<cell><![CDATA[".$row['id']."]]></cell>";		
 	$xml .= "<cell><![CDATA[".utf8_encode($row['name'])."]]></cell>";
--- modules/customers/xml.php
+++ modules/customers/xml.php
@@ -84,8 +84,8 @@ $count = $resultCount[0];
 	foreach ($customers as $row) {
 		$xml .= "<row id='".$row['CID']."'>";
 		$xml .= "<cell><![CDATA[
-			<a class='index_table' title='$LANG[view] $LANG[biller] ".utf8_encode($row['name'])."' href='index.php?module=billers&view=details&id=$row[CID]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-			<a class='index_table' title='$LANG[edit] $LANG[biller] ".utf8_encode($row['name'])."' href='index.php?module=billers&view=details&id=$row[CID]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+			<a class='index_table' title='$LANG[view] $LANG[customer] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[CID]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+			<a class='index_table' title='$LANG[edit] $LANG[customer] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[CID]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
 		]]></cell>";		
 		$xml .= "<cell><![CDATA[".$row['CID']."]]></cell>";		
 		$xml .= "<cell><![CDATA[".utf8_encode($row['name'])."]]></cell>";
