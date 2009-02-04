f796a3e92504e0accb28476e7ad31d9b197cefe5
Justin Kelly <justin@kelly.org.au>
2009-02-04 12:10:11 +0000

    fix all manage pages

--- modules/invoices/xml.php
+++ modules/invoices/xml.php
@@ -8,115 +8,129 @@ $sort = (isset($_POST['sortname'])) ? $_POST['sortname'] : "id" ;
 $rp = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
 $page = (isset($_POST['page'])) ? $_POST['page'] : "1" ;
 
-//SC: Safety checking values that will be directly subbed in
-if (intval($start) != $start) {
-	$start = 0;
-}
-if (intval($limit) != $limit) {
-	$limit = 15;
-}
-
-/*SQL Limit - start*/
-$start = (($page-1) * $rp);
-$limit = "LIMIT $start, $rp";
-/*SQL Limit - end*/
-
-/*SQL where - start*/
-$query = $_POST['query'];
-$qtype = $_POST['qtype'];
-
-$where = " WHERE iv.domain_id = :domain_id ";
-if ($query) $where = " WHERE iv.domain_id = :domain_id AND $qtype LIKE '%$query%' ";
-/*SQL where - end*/
-
-/*Check that the sort field is OK*/
-$validFields = array('iv.id', 'biller', 'customer', 'invoice_total','owing','date','aging','type');
-
-if (in_array($sort, $validFields)) {
-	$sort = $sort;
-} else {
-	$sort = "id";
-}
-
-
 //$sql = "SELECT * FROM ".TB_PREFIX."invoices LIMIT $start, $limit";
+function sql($type='', $dir, $sort, $rp, $page )
+{
+	global $config;
+	global $auth_session;
 
-if ($db_server == 'pgsql') {
-       $sql = "
-		SELECT
-			 iv.id,
-			 b.name AS Biller,
-			 c.name AS Customer,
-			 sum(ii.total) AS INV_TOTAL,
-			 coalesce(SUM(ap.ac_amount), 0)  AS INV_PAID,
-			 (SUM(ii.total) - coalesce(sum(ap.ac_amount), 0)) AS INV_OWING ,
-			 to_char(date,'YYYY-MM-DD') AS Date ,
-			 (SELECT now()::date - iv.date) AS Age,
-			 (CASE WHEN now()::date - iv.date <= '14 days'::interval THEN '0-14'
-			  WHEN now()::date - iv.date <= '30 days'::interval THEN '15-30'
-			  WHEN now()::date - iv.date <= '60 days'::interval THEN '31-60'
-			  WHEN now()::date - iv.date <= '90 days'::interval THEN '61-90'
-			  ELSE '90+'
-			 END) AS Aging,
-			 iv.type_id As type_id,
-			 p.pref_description AS Type
-		FROM
-			 " . TB_PREFIX . "invoices iv
-			 LEFT JOIN " . TB_PREFIX . "payment ap ON ap.ac_inv_id = iv.id
-			 LEFT JOIN " . TB_PREFIX . "invoice_items ii ON ii.invoice_id = iv.id
-			 LEFT JOIN " . TB_PREFIX . "biller b ON b.id = iv.biller_id
-			 LEFT JOIN " . TB_PREFIX . "customers c ON c.id = iv.customer_id
-			 LEFT JOIN " . TB_PREFIX . "preferences p ON p.pref_id = iv.preference_id
-		$where
-		GROUP BY
-			iv.id, b.name, c.name, date, age, aging, type
-		ORDER BY
+
+	//SC: Safety checking values that will be directly subbed in
+	if (intval($start) != $start) {
+		$start = 0;
+	}
+	if (intval($limit) != $limit) {
+		$limit = 15;
+	}
+
+	/*SQL Limit - start*/
+	$start = (($page-1) * $rp);
+	$limit = "LIMIT $start, $rp";
+	/*SQL Limit - end*/
+
+	/*SQL where - start*/
+	$query = $_POST['query'];
+	$qtype = $_POST['qtype'];
+
+	$where = " WHERE iv.domain_id = :domain_id ";
+	if ($query) $where = " WHERE iv.domain_id = :domain_id AND $qtype LIKE '%$query%' ";
+	/*SQL where - end*/
+
+	/*Check that the sort field is OK*/
+	$validFields = array('iv.id', 'biller', 'customer', 'invoice_total','owing','date','aging','type');
+
+	if (in_array($sort, $validFields)) {
+		$sort = $sort;
+	} else {
+		$sort = "id";
+	}
+
+	if($type =="count")
+	{
+		unset($limit);
+	}
+
+
+	switch ($config->database->adapter)
+	{
+		case "pdo_pgsql":
+		   $sql = "
+			SELECT
+				 iv.id,
+				 b.name AS Biller,
+				 c.name AS Customer,
+				 sum(ii.total) AS INV_TOTAL,
+				 coalesce(SUM(ap.ac_amount), 0)  AS INV_PAID,
+				 (SUM(ii.total) - coalesce(sum(ap.ac_amount), 0)) AS INV_OWING ,
+				 to_char(date,'YYYY-MM-DD') AS Date ,
+				 (SELECT now()::date - iv.date) AS Age,
+				 (CASE WHEN now()::date - iv.date <= '14 days'::interval THEN '0-14'
+				  WHEN now()::date - iv.date <= '30 days'::interval THEN '15-30'
+				  WHEN now()::date - iv.date <= '60 days'::interval THEN '31-60'
+				  WHEN now()::date - iv.date <= '90 days'::interval THEN '61-90'
+				  ELSE '90+'
+				 END) AS Aging,
+				 iv.type_id As type_id,
+				 p.pref_description AS Type
+			FROM
+				 " . TB_PREFIX . "invoices iv
+				 LEFT JOIN " . TB_PREFIX . "payment ap ON ap.ac_inv_id = iv.id
+				 LEFT JOIN " . TB_PREFIX . "invoice_items ii ON ii.invoice_id = iv.id
+				 LEFT JOIN " . TB_PREFIX . "biller b ON b.id = iv.biller_id
+				 LEFT JOIN " . TB_PREFIX . "customers c ON c.id = iv.customer_id
+				 LEFT JOIN " . TB_PREFIX . "preferences p ON p.pref_id = iv.preference_id
+			$where
+			GROUP BY
+				iv.id, b.name, c.name, date, age, aging, type
+			ORDER BY
+				$sort $dir
+			LIMIT $limit OFFSET $start";
+			break;
+		case "pdo_mysql":
+		default:
+		   $sql ="
+			SELECT  
+				   iv.id,
+				   b.name AS biller,
+				   c.name AS customer,
+				   (SELECT SUM(coalesce(ii.total,  0)) FROM " .
+			TB_PREFIX . "invoice_items ii WHERE ii.invoice_id = iv.id) AS invoice_total,
+				   (SELECT SUM(coalesce(ac_amount, 0)) FROM " .
+			TB_PREFIX . "payment ap WHERE ap.ac_inv_id = iv.id) AS INV_PAID,
+				   (SELECT (coalesce(invoice_total,0) - coalesce(INV_PAID,0))) As owing,
+				   DATE_FORMAT(date,'%Y-%m-%d') AS date,
+				   (SELECT IF((owing = 0), 0, DateDiff(now(), date))) AS Age,
+				   (SELECT (CASE   WHEN Age = 0 THEN ''
+												   WHEN Age <= 14 THEN '0-14'
+												   WHEN Age <= 30 THEN '15-30'
+												   WHEN Age <= 60 THEN '31-60'
+												  WHEN Age <= 90 THEN '61-90'
+												   ELSE '90+'  END)) AS aging,
+				   iv.type_id As type_id,
+				   pf.pref_description AS preference
+			FROM   " . TB_PREFIX . "invoices iv
+						   LEFT JOIN " . TB_PREFIX . "biller b ON b.id = iv.biller_id
+						   LEFT JOIN " . TB_PREFIX . "customers c ON c.id = iv.customer_id
+						   LEFT JOIN " . TB_PREFIX . "preferences pf ON pf.pref_id = iv.preference_id
+			$where
+			GROUP BY
+				iv.id
+			ORDER BY
 			$sort $dir
-		LIMIT $limit OFFSET $start";
-} else {
-       $sql ="
-		SELECT  
-			   iv.id,
-		       b.name AS biller,
-		       c.name AS customer,
-		       (SELECT SUM(coalesce(ii.total,  0)) FROM " .
-		TB_PREFIX . "invoice_items ii WHERE ii.invoice_id = iv.id) AS invoice_total,
-		       (SELECT SUM(coalesce(ac_amount, 0)) FROM " .
-		TB_PREFIX . "payment ap WHERE ap.ac_inv_id = iv.id) AS INV_PAID,
-		       (SELECT (coalesce(invoice_total,0) - coalesce(INV_PAID,0))) As owing,
-		       DATE_FORMAT(date,'%Y-%m-%d') AS date,
-		       (SELECT IF((owing = 0), 0, DateDiff(now(), date))) AS Age,
-		       (SELECT (CASE   WHEN Age = 0 THEN ''
-		                                       WHEN Age <= 14 THEN '0-14'
-		                                       WHEN Age <= 30 THEN '15-30'
-		                                       WHEN Age <= 60 THEN '31-60'
-		                                      WHEN Age <= 90 THEN '61-90'
-		                                       ELSE '90+'  END)) AS aging,
-		       iv.type_id As type_id,
-		       pf.pref_description AS preference
-		FROM   " . TB_PREFIX . "invoices iv
-		               LEFT JOIN " . TB_PREFIX . "biller b ON b.id = iv.biller_id
-		               LEFT JOIN " . TB_PREFIX . "customers c ON c.id = iv.customer_id
-		               LEFT JOIN " . TB_PREFIX . "preferences pf ON pf.pref_id = iv.preference_id
-		$where
-		GROUP BY
-			iv.id
-		ORDER BY
-		$sort $dir
-		$limit";
+			$limit";
+			break;
+	}
+	
+	$result =  dbQuery($sql,':domain_id', $auth_session->domain_id) or die(end($dbh->errorInfo()));
+	return $result;
 }
 
-$sth = dbQuery($sql,':domain_id', $auth_session->domain_id) or die(end($dbh->errorInfo()));
+$sth = sql('', $dir, $sort, $rp, $page);
+$sth_count_rows = sql('count',$dir, $sort, $rp, $page);
+
 $invoices = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-global $dbh;
-
-//$sqlTotal = "SELECT count(id) AS count FROM ".TB_PREFIX."invoices";
-//$tth = dbQuery($sqlTotal) or die(end($dbh->errorInfo()));
-//$resultCount = $tth->fetch();
-//$count = $resultCount[0];
-$count = $sth->rowCount();
-//echo sql2xml($invoices, $count);
+$count = $sth_count_rows->rowCount();
 
 	$xml .= "<rows>";
 	$xml .= "<page>$page</page>";
--- modules/payments/xml.php
+++ modules/payments/xml.php
@@ -2,102 +2,119 @@
 
 header("Content-type: text/xml");
 
-$start = (isset($_POST['start'])) ? $_POST['start'] : "0" ;
+//$start = (isset($_POST['start'])) ? $_POST['start'] : "0" ;
 $dir = (isset($_POST['sortorder'])) ? $_POST['sortorder'] : "DESC" ;
 $sort = (isset($_POST['sortname'])) ? $_POST['sortname'] : "ap.id" ;
-$limit = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
+$rp = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
 $page = (isset($_POST['page'])) ? $_POST['page'] : "1" ;
 
-//SC: Safety checking values that will be directly subbed in
-if (intval($start) != $start) {
-	$start = 0;
+function sql($type='', $dir, $sort, $rp, $page )
+{
+	global $config;
+	global $auth_session;
+
+	//SC: Safety checking values that will be directly subbed in
+	if (intval($start) != $start) {
+		$start = 0;
+	}
+	if (intval($limit) != $limit) {
+		$limit = 25;
+	}
+	if (!preg_match('/^(asc|desc)$/iD', $dir)) {
+		$dir = 'DESC';
+	}
+
+	$query = $_POST['query'];
+	$qtype = $_POST['qtype'];
+
+	/*SQL Limit - start*/
+	$start = (($page-1) * $rp);
+	$limit = "LIMIT $start, $rp";
+
+	if($type =="count")
+	{
+		unset($limit);
+	}
+	/*SQL Limit - end*/
+
+	$where = " WHERE ap. domain_id = :domain_id";
+	if ($query) $where = " WHERE ap.domain_id = :domain_id AND $qtype LIKE '%$query%' ";
+
+
+	/*Check that the sort field is OK*/
+	$validFields = array('ap.id', 'ac_inv_id', 'description', 'unit_price','enabled');
+
+	if (in_array($sort, $validFields)) {
+		$sort = $sort;
+	} else {
+		$sort = "ap.id";
+	}
+
+	$query = null;
+	#if coming from another page where you want to filter by just one invoice
+	if (!empty($_GET['id'])) {
+
+		$id = $_GET['id'];
+		//$query = getInvoicePayments($_GET['id']);
+		
+		$sql = "SELECT ap.*, c.name as cname, b.name as bname from ".TB_PREFIX."payment ap, ".TB_PREFIX."invoices iv, ".TB_PREFIX."customers c, ".TB_PREFIX."biller b where ap.ac_inv_id = iv.id and iv.customer_id = c.id and iv.biller_id = b.id and ap.ac_inv_id = :id ORDER BY ap.id DESC";
+		
+		$sth = dbQuery($sql, ':id', $id) or die(htmlspecialchars(end($dbh->errorInfo())));
+		
+	}
+	#if coming from another page where you want to filter by just one customer
+	elseif (!empty($_GET['c_id'])) {
+		
+		//$query = getCustomerPayments($_GET['c_id']);
+		$id = $_GET['c_id'];
+		$sql = "SELECT ap.*, c.name as cname, b.name as bname from ".TB_PREFIX."payment ap, ".TB_PREFIX."invoices iv, ".TB_PREFIX."customers c, ".TB_PREFIX."biller b where ap.ac_inv_id = iv.id and iv.customer_id = c.id and iv.biller_id = b.id and c.id = :id ORDER BY ap.id DESC";
+
+		$sth = dbQuery($sql, ':id', $id) or die(htmlspecialchars(end($dbh->errorInfo())));
+		
+	}
+	#if you want to show all invoices - no filters
+	else {
+		//$query = getPayments();
+		
+		$sql = "SELECT 
+					ap.*, 
+					c.name as cname, 
+					b.name as bname,
+					pt.pt_description AS description,
+					ac_notes AS notes,
+					DATE_FORMAT(ac_date,'%Y-%m-%d') AS date
+				FROM 
+					".TB_PREFIX."payment ap, 
+					".TB_PREFIX."invoices iv, 
+					".TB_PREFIX."customers c, 
+					".TB_PREFIX."biller b ,
+					".TB_PREFIX."payment_types pt 
+				WHERE 
+					ap.ac_inv_id = iv.id 
+					AND 
+						iv.customer_id = c.id 
+					AND 
+						iv.biller_id = b.id 
+					AND
+						ap.ac_payment_type = pt.pt_id 
+					AND
+						ap.domain_id = :domain_id
+				ORDER BY 
+					$sort $dir 
+				$limit
+					";
+					
+	}
+
+	$result =  dbQuery($sql,':domain_id', $auth_session->domain_id) or die(end($dbh->errorInfo()));
+	return $result;
 }
-if (intval($limit) != $limit) {
-	$limit = 25;
-}
-if (!preg_match('/^(asc|desc)$/iD', $dir)) {
-	$dir = 'DESC';
-}
 
-$query = $_POST['query'];
-$qtype = $_POST['qtype'];
+$sth = sql('', $dir, $sort, $rp, $page);
+$sth_count_rows = sql('count',$dir, $sort, $rp, $page);
 
-$where = " WHERE ap. domain_id = :domain_id";
-if ($query) $where = " WHERE ap.domain_id = :domain_id AND $qtype LIKE '%$query%' ";
-
-
-/*Check that the sort field is OK*/
-$validFields = array('ap.id', 'ac_inv_id', 'description', 'unit_price','enabled');
-
-if (in_array($sort, $validFields)) {
-	$sort = $sort;
-} else {
-	$sort = "ap.id";
-}
-
-$query = null;
-#if coming from another page where you want to filter by just one invoice
-if (!empty($_GET['id'])) {
-
-	$id = $_GET['id'];
-	//$query = getInvoicePayments($_GET['id']);
-	
-	$sql = "SELECT ap.*, c.name as cname, b.name as bname from ".TB_PREFIX."payment ap, ".TB_PREFIX."invoices iv, ".TB_PREFIX."customers c, ".TB_PREFIX."biller b where ap.ac_inv_id = iv.id and iv.customer_id = c.id and iv.biller_id = b.id and ap.ac_inv_id = :id ORDER BY ap.id DESC";
-	
-	$sth = dbQuery($sql, ':id', $id) or die(htmlspecialchars(end($dbh->errorInfo())));
-	$count = $sth->rowCount();
-	
-}
-#if coming from another page where you want to filter by just one customer
-elseif (!empty($_GET['c_id'])) {
-	
-	//$query = getCustomerPayments($_GET['c_id']);
-	$id = $_GET['c_id'];
-	$sql = "SELECT ap.*, c.name as cname, b.name as bname from ".TB_PREFIX."payment ap, ".TB_PREFIX."invoices iv, ".TB_PREFIX."customers c, ".TB_PREFIX."biller b where ap.ac_inv_id = iv.id and iv.customer_id = c.id and iv.biller_id = b.id and c.id = :id ORDER BY ap.id DESC";
-
-	$sth = dbQuery($sql, ':id', $id) or die(htmlspecialchars(end($dbh->errorInfo())));
-	$count = $sth->rowCount();
-	
-}
-#if you want to show all invoices - no filters
-else {
-	//$query = getPayments();
-	
-	$sql = "SELECT 
-				ap.*, 
-				c.name as cname, 
-				b.name as bname,
-				pt.pt_description AS description,
-				ac_notes AS notes,
-				DATE_FORMAT(ac_date,'%Y-%m-%d') AS date
-			FROM 
-				".TB_PREFIX."payment ap, 
-				".TB_PREFIX."invoices iv, 
-				".TB_PREFIX."customers c, 
-				".TB_PREFIX."biller b ,
-				".TB_PREFIX."payment_types pt 
-			WHERE 
-				ap.ac_inv_id = iv.id 
-				AND 
-					iv.customer_id = c.id 
-				AND 
-					iv.biller_id = b.id 
-				AND
-					ap.ac_payment_type = pt.pt_id 
-				AND
-					ap.domain_id = :domain_id
-			ORDER BY 
-				$sort $dir 
-			LIMIT 
-				$start, $limit
-				";
-				
-	$sth = dbQuery($sql,':domain_id', $auth_session->domain_id) or die(htmlspecialchars(end($dbh->errorInfo())));
-	$count = $sth->rowCount();
-}
-	$payments = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-global $dbh;
+$payments = $sth->fetchAll(PDO::FETCH_ASSOC);
+$count = $sth_count_rows->rowCount();
 /*
 $sqlTotal = "SELECT count(id) AS count FROM ".TB_PREFIX."payment";
 $tth = dbQuery($sqlTotal) or die(end($dbh->errorInfo()));
