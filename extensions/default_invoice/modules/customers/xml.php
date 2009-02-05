269f4c8f8c09aad1c6f5eba386e5dd27ff9d93f5
Marcel VanDorp <marcelvandorp@gmail.com>
2009-02-05 12:10:04 +0000

    'Last invoice' in manage customers is no clickable, and shows a quick view of the invoice BTW: The customers list is again limited to 25 entries

--- extensions/default_invoice/modules/customers/xml.php
+++ extensions/default_invoice/modules/customers/xml.php
@@ -86,12 +86,12 @@ $count = $sth->rowCount();
 	foreach ($customers as $row) {
 		$xml .= "<row id='".$row['CID']."'>";
 		$xml .= "<cell><![CDATA[
-			<a class='index_table' title='$LANG[view] $LANG[customer] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[CID]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-			<a class='index_table' title='$LANG[edit] $LANG[customer] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[CID]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+			<a class='index_table' title='$LANG[view] $LANG[customer] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[CID]&action=view'><img src='images/common/view.png' class='action' /></a>
+			<a class='index_table' title='$LANG[edit] $LANG[customer] ".utf8_encode($row['name'])."' href='index.php?module=customers&view=details&id=$row[CID]&action=edit'><img src='images/common/edit.png' class='action' /></a>
 		]]></cell>";		
 		$xml .= "<cell><![CDATA[".$row['CID']."]]></cell>";		
 		$xml .= "<cell><![CDATA[".utf8_encode($row['name'])."]]></cell>";
-		$xml .= "<cell><![CDATA[".utf8_encode($row['last_invoice'])."]]></cell>";
+		$xml .= "<cell><![CDATA[<a class='index_table' title='quick view' href='index.php?module=invoices&view=quick_view&id=".utf8_encode($row['last_invoice'])."'>".utf8_encode($row['last_invoice'])."</a>]]></cell>";
 		$xml .= "<cell><![CDATA[".utf8_encode(siLocal::number($row['customer_total']))."]]></cell>";
 		$xml .= "<cell><![CDATA[".utf8_encode(siLocal::number($row['owing']))."]]></cell>";
 		$xml .= "<cell><![CDATA[".utf8_encode($row['enabled'])."]]></cell>";				
