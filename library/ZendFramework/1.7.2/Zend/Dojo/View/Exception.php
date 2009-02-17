87ad4daab04da543de163e219c7aedbea8d1f98c
Nicolas Ruflin <ruflin@gmail.com>
2009-02-17 13:38:45 +0000

    ZF removed for changing structure

--- library/ZendFramework/1.7.2/Zend/Dojo/View/Exception.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'Zend/Dojo/Exception.php';
-
-class Zend_Dojo_View_Exception extends Zend_Dojo_Exception
-{
-}
--- library/ZendFramework/1.7.2/Zend/Dom/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** Zend_Exception */
-require_once 'Zend/Exception.php';
-
-class Zend_Dom_Exception extends Zend_Exception
-{
-}
