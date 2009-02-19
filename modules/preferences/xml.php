9e7f08071330a148850d1ca673861f261339c865
Justin Kelly <justin@kelly.org.au>
2009-02-19 11:49:58 +0000

    prep for 2009.1 beta 3 release

--- modules/preferences/xml.php
+++ modules/preferences/xml.php
@@ -5,72 +5,93 @@ header("Content-type: text/xml");
 $start = (isset($_POST['start'])) ? $_POST['start'] : "0" ;
 $dir = (isset($_POST['sortorder'])) ? $_POST['sortorder'] : "ASC" ;
 $sort = (isset($_POST['sortname'])) ? $_POST['sortname'] : "pref_description" ;
-$limit = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
+$rp = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
 $page = (isset($_POST['page'])) ? $_POST['page'] : "1" ;
 
 
+function sql($type='', $dir, $sort, $rp, $page )
+{
+	global $config;
+	global $LANG;
+	global $auth_session;
 
-//SC: Safety checking values that will be directly subbed in
-if (intval($start) != $start) {
-	$start = 0;
-}
-if (intval($limit) != $limit) {
-	$limit = 25;
-}
-if (!preg_match('/^(asc|desc)$/iD', $dir)) {
-	$dir = 'ASC';
-}
-
-$query = $_POST['query'];
-$qtype = $_POST['qtype'];
-
-$where = " WHERE domain_id = :domain_id ";
-if ($query) $where = " WHERE domain_id = :domain_id AND $qtype LIKE '%$query%' ";
-
-
-/*Check that the sort field is OK*/
-$validFields = array('pref_id', 'pref_description','enabled');
-
-if (in_array($sort, $validFields)) {
-	$sort = $sort;
-} else {
-	$sort = "pref_description";
-}
-
-	$sql = "SELECT 
-				pref_id, 
-				pref_description,
-				(SELECT (CASE  WHEN pref_enabled = 0 THEN '".$LANG['disabled']."' ELSE '".$LANG['enabled']."' END )) AS enabled
-			FROM 
-				".TB_PREFIX."preferences 
-			$where
-			ORDER BY 
-				$sort $dir 
-			LIMIT 
-				$start, $limit";
-
-
-	$sth = dbQuery($sql, ':domain_id', $auth_session->domain_id) or die(htmlspecialchars(end($dbh->errorInfo())));
-	$preferences = $sth->fetchAll(PDO::FETCH_ASSOC);
-	$count = $sth->rowCount();
-	 
-
-	$xml .= "<rows>";
-	$xml .= "<page>$page</page>";
-	$xml .= "<total>$count</total>";
-	
-	foreach ($preferences as $row) {
-		$xml .= "<row id='".$row['pref_id']."'>";
-		$xml .= "<cell><![CDATA[
-			<a class='index_table' title='$LANG[view] $LANG[preference] ".utf8_encode($row['pref_description'])."' href='index.php?module=preferences&view=details&id=$row[pref_id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-			<a class='index_table' title='$LANG[edit] $LANG[preference] ".utf8_encode($row['pref_description'])."' href='index.php?module=preferences&view=details&id=$row[pref_id]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-		]]></cell>";
-		$xml .= "<cell><![CDATA[".utf8_encode($row['pref_id'])."]]></cell>";		
-		$xml .= "<cell><![CDATA[".utf8_encode($row['pref_description'])."]]></cell>";
-		$xml .= "<cell><![CDATA[".utf8_encode($row['enabled'])."]]></cell>";				
-		$xml .= "</row>";		
+	//SC: Safety checking values that will be directly subbed in
+	if (intval($start) != $start) {
+		$start = 0;
 	}
-	$xml .= "</rows>";
+	if (intval($limit) != $limit) {
+		$limit = 25;
+	}
+
+	/*SQL Limit - start*/
+	$start = (($page-1) * $rp);
+	$limit = "LIMIT $start, $rp";
+
+	if($type =="count")
+	{
+		unset($limit);
+	}
+	/*SQL Limit - end*/	
+
+	if (!preg_match('/^(asc|desc)$/iD', $dir)) {
+		$dir = 'ASC';
+	}
+
+	$query = $_POST['query'];
+	$qtype = $_POST['qtype'];
+
+	$where = " WHERE domain_id = :domain_id ";
+	if ($query) $where = " WHERE domain_id = :domain_id AND $qtype LIKE '%$query%' ";
+
+
+	/*Check that the sort field is OK*/
+	$validFields = array('pref_id', 'pref_description','enabled');
+
+	if (in_array($sort, $validFields)) {
+		$sort = $sort;
+	} else {
+		$sort = "pref_description";
+	}
+
+		$sql = "SELECT 
+					pref_id, 
+					pref_description,
+					(SELECT (CASE  WHEN pref_enabled = 0 THEN '".$LANG['disabled']."' ELSE '".$LANG['enabled']."' END )) AS enabled
+				FROM 
+					".TB_PREFIX."preferences 
+				$where
+				ORDER BY 
+					$sort $dir 
+				$limit";
+
+
+	$result = dbQuery($sql, ':domain_id', $auth_session->domain_id) or die(htmlspecialchars(end($dbh->errorInfo())));
+	return $result;
+}
+
+$sth = sql('', $dir, $sort, $rp, $page);
+$sth_count_rows = sql('count',$dir, $sort, $rp, $page);
+
+$preferences = $sth->fetchAll(PDO::FETCH_ASSOC);
+$count = $sth->rowCount();
+ 
+
+$xml .= "<rows>";
+$xml .= "<page>$page</page>";
+$xml .= "<total>$count</total>";
+
+foreach ($preferences as $row) {
+	$xml .= "<row id='".$row['pref_id']."'>";
+	$xml .= "<cell><![CDATA[
+		<a class='index_table' title='$LANG[view] $LANG[preference] ".utf8_encode($row['pref_description'])."' href='index.php?module=preferences&view=details&id=$row[pref_id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+		<a class='index_table' title='$LANG[edit] $LANG[preference] ".utf8_encode($row['pref_description'])."' href='index.php?module=preferences&view=details&id=$row[pref_id]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+	]]></cell>";
+	$xml .= "<cell><![CDATA[".utf8_encode($row['pref_id'])."]]></cell>";		
+	$xml .= "<cell><![CDATA[".utf8_encode($row['pref_description'])."]]></cell>";
+	$xml .= "<cell><![CDATA[".utf8_encode($row['enabled'])."]]></cell>";				
+	$xml .= "</row>";		
+}
+$xml .= "</rows>";
 
 echo $xml;
 
--- modules/tax_rates/xml.php
+++ modules/tax_rates/xml.php
@@ -5,74 +5,95 @@ header("Content-type: text/xml");
 $start = (isset($_POST['start'])) ? $_POST['start'] : "0" ;
 $dir = (isset($_POST['sortorder'])) ? $_POST['sortorder'] : "ASC" ;
 $sort = (isset($_POST['sortname'])) ? $_POST['sortname'] : "tax_description" ;
-$limit = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
+$rp = (isset($_POST['rp'])) ? $_POST['rp'] : "25" ;
 $page = (isset($_POST['page'])) ? $_POST['page'] : "1" ;
 
 
 
-//SC: Safety checking values that will be directly subbed in
-if (intval($start) != $start) {
-	$start = 0;
-}
-if (intval($limit) != $limit) {
-	$limit = 25;
-}
-if (!preg_match('/^(asc|desc)$/iD', $dir)) {
-	$dir = 'ASC';
-}
+function sql($type='', $dir, $sort, $rp, $page )
+{
+	global $config;
+	global $LANG;
+	global $auth_session;
 
-$query = $_POST['query'];
-$qtype = $_POST['qtype'];
+	//SC: Safety checking values that will be directly subbed in
+	if (intval($start) != $start) {
+		$start = 0;
+	}
+	if (intval($rp) != $rp) {
+		$rp = 25;
+	}
+	/*SQL Limit - start*/
+	$start = (($page-1) * $rp);
+	$limit = "LIMIT $start, $rp";
 
-$where = " WHERE domain_id = :domain_id";
-if ($query) $where = " WHERE domain_id = :domain_id AND $qtype LIKE '%$query%' ";
+	if($type =="count")
+	{
+		unset($limit);
+	}
+	/*SQL Limit - end*/	
+	if (!preg_match('/^(asc|desc)$/iD', $dir)) {
+		$dir = 'ASC';
+	}
+
+	$query = $_POST['query'];
+	$qtype = $_POST['qtype'];
+
+	$where = " WHERE domain_id = :domain_id";
+	if ($query) $where = " WHERE domain_id = :domain_id AND $qtype LIKE '%$query%' ";
 
 
-/*Check that the sort field is OK*/
-$validFields = array('tax_id', 'tax_description','tax_percentage','enabled');
+	/*Check that the sort field is OK*/
+	$validFields = array('tax_id', 'tax_description','tax_percentage','enabled');
 
-if (in_array($sort, $validFields)) {
-	$sort = $sort;
-} else {
-	$sort = "tx_description";
-}
+	if (in_array($sort, $validFields)) {
+		$sort = $sort;
+	} else {
+		$sort = "tx_description";
+	}
 
 	$sql = "SELECT 
-				tax_id, 
-				tax_description,
-				tax_percentage,
-				type,
-				(SELECT (CASE  WHEN tax_enabled = 0 THEN '".$LANG['disabled']."' ELSE '".$LANG['enabled']."' END )) AS enabled
-			FROM 
-				".TB_PREFIX."tax
-			$where
-			ORDER BY 
-				$sort $dir 
-			LIMIT 
-				$start, $limit";
+					tax_id, 
+					tax_description,
+					tax_percentage,
+					type,
+					(SELECT (CASE  WHEN tax_enabled = 0 THEN '".$LANG['disabled']."' ELSE '".$LANG['enabled']."' END )) AS enabled
+				FROM 
+					".TB_PREFIX."tax
+				$where
+				ORDER BY 
+					$sort $dir 
+			    $limit";
 
 
-	$sth = dbQuery($sql, ':domain_id', $auth_session->domain_id) or die(htmlspecialchars(end($dbh->errorInfo())));
-	$tax = $sth->fetchAll(PDO::FETCH_ASSOC);
-	$count = $sth->rowCount();
+	$result = dbQuery($sql, ':domain_id', $auth_session->domain_id) or die(htmlspecialchars(end($dbh->errorInfo())));
+	return $result;
+
+}
+
+$sth = sql('', $dir, $sort, $rp, $page);
+$sth_count_rows = sql('count',$dir, $sort, $rp, $page);
+
+$tax = $sth->fetchAll(PDO::FETCH_ASSOC);
+$count = $sth->rowCount();
 	 
 
-	$xml .= "<rows>";
-	$xml .= "<page>$page</page>";
-	$xml .= "<total>$count</total>";
-	
-	foreach ($tax as $row) {
-		$xml .= "<row id='".$row['tax_id']."'>";
-		$xml .= "<cell><![CDATA[
-			<a class='index_table' title='$LANG[view] $LANG[tax_rate] ".utf8_encode($row['tax_description'])."' href='index.php?module=tax_rates&view=details&id=$row[tax_id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-			<a class='index_table' title='$LANG[edit] $LANG[tax_rate] ".utf8_encode($row['tax_description'])."' href='index.php?module=tax_rates&view=details&id=$row[tax_id]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-		]]></cell>";
-		$xml .= "<cell><![CDATA[".utf8_encode($row['tax_id'])."]]></cell>";		
-		$xml .= "<cell><![CDATA[".utf8_encode($row['tax_description'])."]]></cell>";
-		$xml .= "<cell><![CDATA[".utf8_encode(siLocal::number($row['tax_percentage']))." ".utf8_encode($row['type'])."]]></cell>";
-		$xml .= "<cell><![CDATA[".utf8_encode($row['enabled'])."]]></cell>";				
-		$xml .= "</row>";		
-	}
-	$xml .= "</rows>";
+$xml .= "<rows>";
+$xml .= "<page>$page</page>";
+$xml .= "<total>$count</total>";
+
+foreach ($tax as $row) {
+	$xml .= "<row id='".$row['tax_id']."'>";
+	$xml .= "<cell><![CDATA[
+		<a class='index_table' title='$LANG[view] $LANG[tax_rate] ".utf8_encode($row['tax_description'])."' href='index.php?module=tax_rates&view=details&id=$row[tax_id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+		<a class='index_table' title='$LANG[edit] $LANG[tax_rate] ".utf8_encode($row['tax_description'])."' href='index.php?module=tax_rates&view=details&id=$row[tax_id]&action=edit'><img src='images/common/edit.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+	]]></cell>";
+	$xml .= "<cell><![CDATA[".utf8_encode($row['tax_id'])."]]></cell>";		
+	$xml .= "<cell><![CDATA[".utf8_encode($row['tax_description'])."]]></cell>";
+	$xml .= "<cell><![CDATA[".utf8_encode(siLocal::number($row['tax_percentage']))." ".utf8_encode($row['type'])."]]></cell>";
+	$xml .= "<cell><![CDATA[".utf8_encode($row['enabled'])."]]></cell>";				
+	$xml .= "</row>";		
+}
+$xml .= "</rows>";
 
 echo $xml;
