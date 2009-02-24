2954d2106e1bf8259ac817dd3a694d0e108a8437
vanderlaan <vanderlaan.pm@gmail.com>
2009-02-24 21:58:36 +0000

    Added <head> tag in dev/install/pereferences.php

--- dev/install/header.php
+++ dev/install/header.php
@@ -26,10 +26,9 @@ else {
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
+<head>
 <title>Simple Invoices | Installer</title>
-
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="./css/screen.css" media="all"/>
-
+<link rel="stylesheet" type="text/css" href="./css/screen.css" media="all" />
+</head>
 <body>
