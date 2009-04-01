660c1ca5956a2e39feceba32bf1bc3c8656aa446
Marcel VanDorp <marcelvandorp@gmail.com>
2009-04-01 06:18:15 +0000

    Last line, an UNTESTED change to append the extension info to $config, instead of replacing it Currently I have no time to verify (perhaps in the next few days)

--- include/init.php
+++ include/init.php
@@ -151,4 +151,6 @@ $sth = dbQuery($sql,':id', $auth_session->domain_id ) or die(htmlspecialchars(en
 while ( $this_extension = $sth->fetch()) { $DB_extensions[$this_extension[name]] = $this_extension; }
 $config->extension = $DB_extensions;
 
+//If using the folowing line, the DB settings should be appended to the config array, instead of replacing it (NOT TESTED!)
+//$config->extension() = $DB_extensions;
 
