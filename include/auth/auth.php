483a4ce500fd756361bd92ab910eed0c0547e18a
sanguisdex <sanguisdex@gmail.com>
2009-04-08 19:48:25 +0000

    fix line if statment so that it was closed in the right place

--- include/auth/auth.php
+++ include/auth/auth.php
@@ -1,10 +1,10 @@
 <?php
 
-if (!isset($auth_session->id))
-  if(!isset($_GET['module'])) $_GET['module'] = '';
-{
-	if  ($_GET['module'] !== "auth")  
-	{
+if (!isset($auth_session->id)){
+  if(!isset($_GET['module'])) {
+    $_GET['module'] = '';
+  }
+	if  ($_GET['module'] !== "auth") {
 		header('Location: index.php?module=auth&view=login');       
 		exit;
 	}
