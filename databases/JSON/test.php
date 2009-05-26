78b5e9af0aa8c9e54b7a2f10f459aef1ac92204d
Justin Kelly <justin@kelly.org.au>
2009-05-26 06:31:20 +0000

    minor json work

--- databases/JSON/test.php
+++ databases/JSON/test.php
@@ -3,7 +3,50 @@
 $myFile = "EssentialData.json";
 $json = file_get_contents($myFile, true);
 
-var_dump(json_decode($json));
+$a = json_decode($json,true);
+/*
+foreach ($a as $k => $v) 
+{
+	echo "<b>".$k. "</b>";
+	echo "<br>";
+	    foreach ($a[$k] as $v2) 
+		{
+			echo "<br>";
+				foreach ($v2 as $k3 => $v3) 
+				{
+					echo $k3 ." => ".$v3;
+					echo "<br>";
+				}
+		}
+	echo "<br>";    
+	
+}
+*/
+foreach ($a as $k => $v) 
+{
+	$table = $k;
+	echo "Table: ".$table;
+	
+	echo "<br>";
+	$columns ="";
+	    foreach ($a[$k] as $v2) 
+		{
+			echo "<br>";
+				
+				foreach ($v2 as $k3 => $v3) 
+				{
+					$columns .= ", ".$k3;	
+					echo $k3 ." => ".$v3;
+					echo "<br>";
+				}
+				
+			echo "Columns: ".$columms;
+			$sql = "INSERT into ".$table." (".$columns.") VALUES (".$values.");";
+		}
+	echo "<br>";    
+	
+}
+
 
 
 ?>
