af71a77c77433ae89ef914a58e49697ccddc3ed0
Justin Kelly <justin@kelly.org.au>
2009-07-12 05:58:13 +0000

    start work on expenses extension

---
+++ extensions/expense/modules/expense/manage.js.php
@@ -0,0 +1,50 @@
+<script type="text/javascript">
+
+{literal}
+
+var view_tooltip ="{/literal}{$LANG.quick_view_tooltip} {ldelim}1{rdelim}{literal}";
+var edit_tooltip = "{/literal}{$LANG.edit_view_tooltip} {$invoices.preference.pref_inv_wording} {ldelim}1{rdelim}{literal}";
+
+			var columns = 5;
+			var padding = 12;
+			var grid_width = $('.col').width();
+			
+			grid_width = grid_width - (columns * padding);
+			percentage_width = grid_width / 100; 
+		
+			
+			$('#manageGrid').flexigrid
+			(
+			{
+			url: 'index.php?module=products&view=xml',
+			dataType: 'xml',
+			colModel : [
+				{display: 'Actions', name : 'actions', width : 10 * percentage_width, sortable : false, align: 'center'},
+				{display: 'ID', name : 'id', width : 10 * percentage_width, sortable : true, align: 'left'},
+				{display: 'Name', name : 'description', width : 40 * percentage_width, sortable : true, align: 'left'},
+				{display: 'Unit Price', name : 'unit_price', width : 20 * percentage_width, sortable : true, align: 'left'},
+				{display: 'Enabled', name : 'enabled', width : 20 * percentage_width, sortable : true, align: 'left'}
+				
+				],
+				
+
+			searchitems : [
+				{display: 'ID', name : 'id'},
+				{display: 'Name', name : 'description', isdefault: true}
+				],
+			sortname: 'id',
+			sortorder: 'asc',
+			usepager: true,
+			/*title: 'Manage Custom Fields',*/
+			useRp: false,
+			rp: 25,
+			showToggleBtn: false,
+			showTableToggleBtn: false,
+			height: 'auto'
+			}
+			);
+
+
+{/literal}
+
+</script>
