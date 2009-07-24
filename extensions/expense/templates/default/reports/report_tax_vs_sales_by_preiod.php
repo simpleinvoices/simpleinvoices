51a77aacc4e3c5e8fc275b58abbde17a94e862ac
Justin Kelly <justin@kelly.org.au>
2009-07-24 12:43:46 +0000

    expense update

---
+++ extensions/expense/templates/default/reports/report_tax_vs_sales_by_preiod.php
@@ -0,0 +1,16 @@
+<div id="top"><h3>{$LANG.monthly_sales_per_year}</h3></div>
+ <hr />
+
+<table width="100%">
+ {foreach item=year from=$years}
+   <tr>
+   <td><b>{$year}</b></td>
+   <tr>
+   <td></td><td>Month:<br />Sales:<br />Payments:</td>
+     {foreach key=key item=item_sales from=$total_sales.$year}
+	      <td>{$key}<br />{$item_sales}<br />{$total_payments.$year.$key}  </td>
+     {/foreach}
+
+	</tr>
+ {/foreach}
+ </table>
