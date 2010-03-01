875520b6ac67ee447f0eb3f348b6a796bf79bb4c
Justin Kelly <justin@kelly.org.au>
2010-03-01 10:35:45 +0000

    cron updates

--- include/class/email/body.php
+++ include/class/email/body.php
@@ -4,11 +4,18 @@ class email_body {
 	public function create()
 	{
 
-		$email_body = "Hi ".$this->customer_name.",
+		$email_body = <<<EOT
+Hi $this->customer_name,
 <br />
 <br />
-Attached is your PDF copy of ".$this->invoice_name." from ".$this->biller_name;
-
+Attached is your PDF copy of $this->invoice_name
+<br />
+<br />
+Cheers
+<br />
+<br />
+$this->biller_name
+EOT;
 
 		return $email_body;
 	}
