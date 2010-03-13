d671346319792ab1cbbc1be3a47aa3a3377318fd
Justin Kelly <justin@kelly.org.au>
2010-03-13 09:07:58 +0000

    fix up old report issue

--- modules/reports/report_biller_by_customer.php
+++ modules/reports/report_biller_by_customer.php
@@ -3,7 +3,7 @@
 require_once("./include/reportlib.php");
 
 // Adjusted for NULL on computation with NULL values
-   $sSQL = "SELECT
+ /*  $sSQL = "SELECT
       sum(ii.total) AS sum_total,
          b.name AS bname,
          c.name AS cname
@@ -12,6 +12,12 @@ require_once("./include/reportlib.php");
       ".TB_PREFIX."customers c ON (c.id = iv.customer_id) INNER JOIN
       ".TB_PREFIX."invoice_items ii ON (iv.id = ii.invoice_id)
       GROUP BY b.name, c.name";
+*/
+	$sSQL  = "SELECT sum(ivt.total) as SUM_TOTAL, b.name as Biller, c.name as Customer ";
+	$sSQL .= "FROM ".TB_PREFIX."biller b, ".TB_PREFIX."customers c, ";
+	$sSQL .= 		 TB_PREFIX."invoice_items ivt, ".TB_PREFIX."invoices iv ";
+	$sSQL .= "WHERE iv.customer_id = c.id AND iv.biller_id = b.id AND iv.id = ivt.invoice_id ";
+	$sSQL .= "GROUP BY b.name, c.name";
 
 	
 	$oRpt->setXML("./modules/reports/report_biller_by_customer.xml");
