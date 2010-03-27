7b556926df918de4e0afdbd21f1ea9d3ea28e8dc
Justin Kelly <justin@kelly.org.au>
2010-03-27 10:31:43 +0000

    minor update

--- include/class/cron.php
+++ include/class/cron.php
@@ -276,7 +276,7 @@ class cron {
                 if ($run_cron == 'true')
                 {
                     $number_of_crons_run++;	
-                    $return[$data[$key]['cron_id']]['cron_message'] = "Cron ID: ". $data[$key]['cron_id'] .", Cron for ".$data[$key]['index_name']." with start date of ".$data[$key]['start_date'].", end date of '".$data[$key]['end_date']."' where it runs each ".$data[$key]['recurrence']." ".$data[$key]['recurrence_type']." was run today :: Info diff=".$diff;
+                    #$return[$data[$key]['cron_id']]['cron_message'] = "Cron ID: ". $data[$key]['cron_id'] .", Cron for ".$data[$key]['index_name']." with start date of ".$data[$key]['start_date'].", end date of '".$data[$key]['end_date']."' where it runs each ".$data[$key]['recurrence']." ".$data[$key]['recurrence_type']." was run today :: Info diff=".$diff;
                     $i++;
 
                     $ni = new invoice();
@@ -313,9 +313,6 @@ class cron {
                         $export -> execute();
 
                         #$attachment = file_get_contents('./tmp/cache/' . $pdf_file_name);
-
-
-
                         $email = new email();
                         $email -> format = 'cron_invoice';
 
