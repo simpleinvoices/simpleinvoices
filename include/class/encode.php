b57bb38782eba925e9931874d7736e76c3bc1afc
Justin Kelly <justin@kelly.org.au>
2010-03-31 00:20:08 +0000

    json output update

--- include/class/encode.php
+++ include/class/encode.php
@@ -56,51 +56,14 @@ class encode
         return $xml;
     }
 
-	public static function json($data)
+	public static function json($data,$format='plain')
 	{
-		return json_encode($data);
+		if($format='pretty') {
+            return Zend_Json::prettyPrint($data, array("indent" => " "));
+        } else {
+            return Zend_Json::encode($data);
+        }
 	}
 
-    public static function indent($json) {
- 
-    $result    = '';
-    $pos       = 0;
-    $strLen    = strlen($json);
-    $indentStr = '  ';
-    $newLine   = "\n";
- 
-    for($i = 0; $i <= $strLen; $i++) {
-        
-        // Grab the next character in the string
-        $char = substr($json, $i, 1);
-        
-        // If this character is the end of an element, 
-        // output a new line and indent the next line
-        if($char == '}' || $char == ']') {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        // Add the character to the result string
-        $result .= $char;
- 
-        // If the last character was the beginning of an element, 
-        // output a new line and indent the next line
-        if ($char == ',' || $char == '{' || $char == '[') {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-    }
- 
-    return $result;
-}
 } // end of class
 
--- modules/api/cron.php
+++ modules/api/cron.php
@@ -6,17 +6,17 @@ $cron = new cron();
 $cron->domain_id=1;
 $message = $cron->run();
 
-#echo encode::json($message);
-#echo encode::xml($message);
-//print_r($message);
-//echo json_encode($invoice);
-
-//header('Content-type: application/xml');
 try 
 {
-    $data = encode::json( $message );
-    echo encode::indent( $data );
+
+    ob_clean();
+    //json
+    header('Content-type: application/json')
+    echo encode::json( $message, 'pretty' );
     
+    //xml
+    //header('Content-type: application/xml');
+    //echo encode::xml( $message );
 } 
 catch (Exception $e) 
 {
