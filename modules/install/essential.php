e7c4b1864d5f38eaddc374c91d60b811843c4f03
Justin Kelly <justin@kelly.org.au>
2010-05-18 10:07:30 +0000

    fix si-index sql patch

--- modules/install/essential.php
+++ modules/install/essential.php
@@ -8,7 +8,7 @@ if ( (checkTableExists("si_customers") == true) AND ($install_data_exists == fal
 	//JSON import
 	$importjson = new importjson();
 	$importjson->file = "./databases/json/essential_data.json";
-	$importjson->debug = true;
+	//$importjson->debug = true;
 	$importjson->pattern_find = array('si_','DOMAIN-ID','LOCALE','LANGUAGE');
 	$importjson->pattern_replace = array(TB_PREFIX,'1','en_GB','en_GB');
 	//dbQuery($importjson->collate());
