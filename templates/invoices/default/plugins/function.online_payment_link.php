c610ed9a582ea2dfc5439f8785a5d98d03841c6a
Steven Roddis <google@stevenroddis.com>
2010-05-26 06:33:13 +0000

    escaped online_payment_link

--- templates/invoices/default/plugins/function.online_payment_link.php
+++ templates/invoices/default/plugins/function.online_payment_link.php
@@ -8,13 +8,13 @@ function smarty_function_online_payment_link($params, &$smarty) {
 	{
 
 		$link = "<a 
-				href=\"https://www.paypal.com/xclick/business=".$params['business']."&item_name=".$params['item_name']."&invoice=".$params['invoice']."&amount=".number_format($params['amount'], 2, '.', '')."&currency_code=".$params['currency_code']."&notify_url=".$params['notify_url']."&return=".$params['return_url']."&no_shipping=1&no_note=1&custom=domain_id:".$domain_id."; \">";
+				href=\"https://www.paypal.com/xclick/business=".urlencode($params['business'])."&item_name=".urlencode($params['item_name'])."&invoice=".urlencode($params['invoice'])."&amount=".urlencode(number_format($params['amount'], 2, '.', ''))."&currency_code=".urlencode($params['currency_code'])."&notify_url=".urlencode($params['notify_url'])."&return=".urlencode($params['return_url'])."&no_shipping=1&no_note=1&custom=domain_id:".urlencode($domain_id)."; \">";
 		
 		if($params['include_image'] == "true")
 		{
-			$link .= "<img src='".$url."/images/common/pay_with_paypal.gif'/>";
+			$link .= "<img src='".urlsafe($url)."/images/common/pay_with_paypal.gif'/>";
 		} else {
-			$link .= $params['link_wording'];
+			$link .= htmlsafe($params['link_wording']);
 		} 
 		
 		$link .= "</a>";
@@ -26,16 +26,16 @@ function smarty_function_online_payment_link($params, &$smarty) {
 	{
 
 		$link = "<a 
-				href=\"https://www.paypal.com/xclick/business=".$params['business']."
-				&item_name=".$params['item_name']."&invoice=".$params['invoice']."
-				&amount=".number_format($params['amount'], 2, '.', '')."&currency_code=".$params['currency_code']."
+				href=\"https://www.paypal.com/xclick/business=".urlencode($params['business']."
+				&item_name=".urlencode($params['item_name'])."&invoice=".urlencode($params['invoice'])."
+				&amount=".urlencode(number_format($params['amount'], 2, '.', ''))."&currency_code=".$params['currency_code'])."
 				&return=http://vcsweb.com.au&no_shipping=1&no_note=1\">";
 		
 		if($params['include_image'] == "true")
 		{
-			$link .= "<img src='".$url."/images/common/pay_with_eway.gif'/>";
+			$link .= "<img src='".urlsafe($url)."/images/common/pay_with_eway.gif'/>";
 		} else {
-			$link .= $params['link_wording'];
+			$link .= htmlsafe($params['link_wording']);
 		} 
 		
 		$link .= "</a>";
