56fd31601e138843c5add049cbf64a369b4afb55
Steven Roddis <google@stevenroddis.com>
2010-05-26 06:56:17 +0000

    added support for common typo urlescape -> urlencode

--- include/init.php
+++ include/init.php
@@ -159,6 +159,7 @@ $smarty->register_modifier('urlsafe', 'urlsafe');
 $smarty->register_modifier('urlencode', 'urlencode');
 $smarty->register_modifier('outhtml', 'outhtml');
 $smarty->register_modifier('htmlout', 'outhtml'); //common typo
+$smarty->register_modifier('urlescape', 'urlencode'); //common typo 
 $install_tables_exists = checkTableExists(TB_PREFIX."biller");
 if ($install_tables_exists == true)
 {
