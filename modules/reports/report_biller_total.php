344b2847bb33e201f22f8cd27068df96425e09ba
Justin Kelly <justin@kelly.org.au>
2010-06-04 05:31:58 +0000

    Biller sales report - status issue

--- modules/reports/report_biller_total.php
+++ modules/reports/report_biller_total.php
@@ -2,12 +2,22 @@
 //   include phpreports library
 require_once("./include/reportlib.php");
 
-   $sSQL = "SELECT b.name, sum(ii.total) AS sum_total
-      FROM ".TB_PREFIX."biller b INNER JOIN
-      ".TB_PREFIX."invoices iv ON (b.id = iv.biller_id) INNER JOIN
-      ".TB_PREFIX."invoice_items ii ON (ii.invoice_id = iv.id)
-      GROUP BY b.name";
-
+   $sSQL = "SELECT 
+                b.name, 
+                sum(ii.total) AS sum_total
+            FROM 
+                ".TB_PREFIX."biller b 
+            INNER JOIN
+              ".TB_PREFIX."invoices iv ON (b.id = iv.biller_id)
+            INNER JOIN
+              ".TB_PREFIX."invoice_items ii ON (ii.invoice_id = iv.id)
+            INNER JOIN
+              ".TB_PREFIX."preferences p ON (p.pref_id = iv.preference_id)
+            WHERE
+                p.status ='1'
+            GROUP BY 
+                b.name";
+                
    $oRpt->setXML("./modules/reports/report_biller_total.xml");
 
 //   include phpreports run code
