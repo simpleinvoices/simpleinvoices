aecd2dd6525066935d551bc133367f77a8b4abdc
Matt West <matt.west@developercity.co.uk>
2010-07-21 20:57:15 +0000

    payment print preview opens in new window/tab

--- modules/payments/xml.php
+++ modules/payments/xml.php
@@ -201,7 +201,7 @@ $count = $resultCount[0];
 		$xml .= "<row id='".$row['id']."'>";
 	$xml .= "<cell><![CDATA[
 	<a class='index_table' title='$LANG[view] ".$row['name']."' href='index.php?module=payments&view=details&id=$row[id]&action=view'><img src='images/common/view.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
-	<a class='index_table' title='$LANG[print_preview_tooltip] ".$row['id']."' href='index.php?module=payments&view=print&id=$row[id]'><img src='images/common/printer.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
+	<a class='index_table' title='$LANG[print_preview_tooltip] ".$row['id']."' href='index.php?module=payments&view=print&id=$row[id]' target='_blank'><img src='images/common/printer.png' height='16' border='-5px' padding='-4px' valign='bottom' /></a>
 	]]></cell>";
 		$xml .= "<cell><![CDATA[".$row['id']."]]></cell>";
 		$xml .= "<cell><![CDATA[".$row['index_name']."]]></cell>";		
