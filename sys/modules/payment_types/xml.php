2aa5b18da1e7ecd0cca26124268d4f7c8e7b2c80
Albert Drent <aducomsoftware@gmail.com>
2010-09-11 20:01:18 +0000

    still some images lost

--- sys/modules/payment_types/xml.php
+++ sys/modules/payment_types/xml.php
@@ -89,10 +89,10 @@ foreach ($payment_types as $row) {
 	]]></cell>";
 	$xml .= "<cell><![CDATA[".$row['pt_description']."]]></cell>";
 	if ($row['enabled']==$LANG['enabled']) {
-		$xml .= "<cell><![CDATA[<img src='images/common/tick.png' alt='".$row['enabled']."' title='".$row['enabled']."' />]]></cell>";				
+		$xml .= "<cell><![CDATA[<img src='".$include_dir."sys/images/common/tick.png' alt='".$row['enabled']."' title='".$row['enabled']."' />]]></cell>";				
 	}	
 	else {
-		$xml .= "<cell><![CDATA[<img src='images/common/cross.png' alt='".$row['enabled']."' title='".$row['enabled']."' />]]></cell>";				
+		$xml .= "<cell><![CDATA[<img src='".$include_dir."sys/images/common/cross.png' alt='".$row['enabled']."' title='".$row['enabled']."' />]]></cell>";				
 	}
 	$xml .= "</row>";		
 }
