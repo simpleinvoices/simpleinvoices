a2fa734fd552ce65b4f542bd26f7e7f7c77033ad
Albert Drent <aducomsoftware@gmail.com>
2010-09-11 14:38:17 +0000

    path changes

--- sys/modules/reports/report_debtors_owing_by_customer.php
+++ sys/modules/reports/report_debtors_owing_by_customer.php
@@ -1,6 +1,6 @@
 <?php
 //   include phpreports library
-require_once("./include/reportlib.php");
+require_once($include_dir . "sys/include/reportlib.php");
 
    if ($db_server == 'pgsql') {
       $sSQL = "SELECT
@@ -48,10 +48,10 @@ ORDER BY
    ";
 }
 
-   $oRpt->setXML("./modules/reports/report_debtors_owing_by_customer.xml");
+   $oRpt->setXML($include_dir . "sys/modules/reports/report_debtors_owing_by_customer.xml");
 
 //   include phpreports run code
-	include("./include/reportrunlib.php");
+	include($include_dir . "sys/include/reportrunlib.php");
 
 $smarty -> assign('pageActive', 'report');
 $smarty -> assign('active_tab', '#home');
