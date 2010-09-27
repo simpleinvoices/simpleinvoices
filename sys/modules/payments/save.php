f51195941930b0345b649c54bcfcec8d83138e09
Owen Dokja <owen@dokja.net>
2010-09-27 03:55:32 +0000

    Save.php now uses language files for payment note appends

--- sys/modules/payments/save.php
+++ sys/modules/payments/save.php
@@ -14,19 +14,11 @@ global $auth_session;
 if ( isset($_POST['process_payment']) ) {
 	
 	$payment = new payment();
-	
-	
-	
 	$payment->ac_inv_id = $_POST['invoice_id'];
 	$payment->ac_amount = $_POST['ac_amount'];
 	$payment->ac_notes = $_POST['ac_notes'];
 	$payment->ac_date = $_POST['ac_date'];
 	$payment->ac_payment_type = $_POST['ac_payment_type'];
-	
-	
-		/* todo:
-		 * use language file for process page text and appended notes
-		 */
 		
 		//works out any funds that are in excess of invoice total:
 		$invoicetotal = getInvoiceTotal($payment->ac_inv_id);
@@ -37,95 +29,94 @@ if ( isset($_POST['process_payment']) ) {
 		if (  $extrapayment > 0.0 && $_POST['distribute'] == '1' ) { //is there any money left over to distribute to other invoices?
                                                                              //and check for user preference
 		
-		//reduce original payment variable to the invoice total:
-		$payment->ac_amount = $invoicetotal;
-		
-		//grab the customer ID so we can look at the other invoices we can process payments for
-		$sql = "SELECT customer_id from ".TB_PREFIX."invoices where id = :ac_inv_id";
-				$sth = $db->query($sql,
-        ':ac_inv_id',$payment->ac_inv_id) or die();
-		$cust_id = $sth->fetch();
-		
-		// query to get unpaid invoices from the customer whose payment is being recorded
-		$sql = "
-				SELECT * from
+                    //reduce original payment variable to the invoice total:
+                    $payment->ac_amount = $invoicetotal;
 
-				(SELECT invoice_id, customer_id, sum(total) as all_charge FROM ".TB_PREFIX."invoices
-				inner join ".TB_PREFIX."invoice_items
-				on
-				".TB_PREFIX."invoices.id = ".TB_PREFIX."invoice_items.invoice_id
-				GROUP BY ".TB_PREFIX."invoices.id ) charge
+                    //grab the customer ID so we can look at the other invoices we can process payments for
+                    $sql = "SELECT customer_id from ".TB_PREFIX."invoices where id = :ac_inv_id";
+                                    $sth = $db->query($sql,':ac_inv_id',$payment->ac_inv_id) or die();
+                    $cust_id = $sth->fetch();
 
-				left join
+                    // query to get unpaid invoices from the customer whose payment is being recorded
+                    $sql = "
+                                    SELECT * from
 
-				(SELECT ".TB_PREFIX."invoices.id, sum(ac_amount) as all_pay FROM ".TB_PREFIX."invoices
-				inner join ".TB_PREFIX."payment
-				on
-				".TB_PREFIX."invoices.id = ".TB_PREFIX."payment.ac_inv_id
-				GROUP BY ".TB_PREFIX."invoices.id ) pay
-				on charge.invoice_id = pay.id
-				where all_charge > ifnull(all_pay,0) and customer_id=:cust_id and charge.invoice_id <> :ac_inv_id
-				";
-			
-		$sth = $db->query($sql, ':cust_id',$cust_id['customer_id'],':ac_inv_id',$payment->ac_inv_id) or die();
-		
+                                    (SELECT invoice_id, customer_id, sum(total) as all_charge FROM ".TB_PREFIX."invoices
+                                    inner join ".TB_PREFIX."invoice_items
+                                    on
+                                    ".TB_PREFIX."invoices.id = ".TB_PREFIX."invoice_items.invoice_id
+                                    GROUP BY ".TB_PREFIX."invoices.id ) charge
 
-                $paid_to = array(); //init array for tracking payment ID's
-                $inv_info = "<ul>";
-                $inv_info_format = "<li>Invoice %d (%f was paid)</li>";
+                                    left join
 
-		//loop through extra invoices
+                                    (SELECT ".TB_PREFIX."invoices.id, sum(ac_amount) as all_pay FROM ".TB_PREFIX."invoices
+                                    inner join ".TB_PREFIX."payment
+                                    on
+                                    ".TB_PREFIX."invoices.id = ".TB_PREFIX."payment.ac_inv_id
+                                    GROUP BY ".TB_PREFIX."invoices.id ) pay
+                                    on charge.invoice_id = pay.id
+                                    where all_charge > ifnull(all_pay,0) and customer_id=:cust_id and charge.invoice_id <> :ac_inv_id
+                                    ";
 
-		while ($curr_inv = $sth->fetch() ) {
-		if ($extrapayment <= 0.0) {
-		break;
-		}
-			$sub_payment = new payment();
-			
-			//work out how much we will be paying to this invoice:
-			$curr_owing = $curr_inv['all_charge'] - $curr_inv['all_pay'];
-			
-			if ( $curr_owing < $extrapayment) { //we have more money to pay than on invoice
-			$revert = 'more'; // for use if a rollback is needed
-			$sub_payment->ac_amount = $curr_owing;
-			$extrapayment = $extrapayment - $curr_owing;
-			} elseif ($curr_owing >= $extrapayment) { //no more money
-			$sub_payment->ac_amount = $extrapayment;
-			$extrapayment = 0.0;
-			$revert = 'nomore';
-			}
-			
-			$sub_payment->ac_inv_id = $curr_inv['invoice_id'];
-			$sub_payment->ac_notes = $_POST['ac_notes'];
-			$sub_payment->ac_date = $_POST['ac_date'];
-			$sub_payment->ac_payment_type = $_POST['ac_payment_type'];
-			$sub_result = $sub_payment->insert();
+                    $sth = $db->query($sql, ':cust_id',$cust_id['customer_id'],':ac_inv_id',$payment->ac_inv_id) or die();
 
 
-			//revert variables back to previous state if payment insert fails
-			$sub_saved = !empty($sub_result) ? "true" : "false";
-			if($sub_saved =='false')
-			{
-				if ($revert == 'more') {
-					//rollback amount we have to pay
-					$extrapayment = $extrapayment + $curr_owing;
-					}
-				elseif ($revert == 'nomore') {
-					$extrapayment = $sub_payment->ac_amount;
-				}
-			
-			} else { //payment succesful, record data in array and note html
-                            $paid_to[$db->lastInsertId()] = $sub_payment->ac_amount;
-                            $inv_info .= sprintf($inv_info_format,$sub_payment->ac_inv_id,$sub_payment->ac_amount);
+                    $paid_to = array(); //init array for tracking payment ID's
+                    $inv_info = "<ul>";
+                    $inv_info_format = "<li>".$LANG['invoice']." %d (%f ".$LANG['paid'].")</li>";
 
-                        }
-			
-		} //end while loop
-		
-                
+                    //loop through extra invoices
 
-		// return any unused funds to be paid to the original invoice
-		$payment->ac_amount = $payment->ac_amount + $extrapayment;
+                    while ($curr_inv = $sth->fetch() ) {
+                    if ($extrapayment <= 0.0) {
+                    break;
+                    }
+                            $sub_payment = new payment();
+
+                            //work out how much we will be paying to this invoice:
+                            $curr_owing = $curr_inv['all_charge'] - $curr_inv['all_pay'];
+
+                            if ( $curr_owing < $extrapayment) { //we have more money to pay than on invoice
+                            $revert = 'more'; // for use if a rollback is needed
+                            $sub_payment->ac_amount = $curr_owing;
+                            $extrapayment = $extrapayment - $curr_owing;
+                            } elseif ($curr_owing >= $extrapayment) { //no more money
+                            $sub_payment->ac_amount = $extrapayment;
+                            $extrapayment = 0.0;
+                            $revert = 'nomore';
+                            }
+
+                            $sub_payment->ac_inv_id = $curr_inv['invoice_id'];
+                            $sub_payment->ac_notes = $_POST['ac_notes'];
+                            $sub_payment->ac_date = $_POST['ac_date'];
+                            $sub_payment->ac_payment_type = $_POST['ac_payment_type'];
+                            $sub_result = $sub_payment->insert();
+
+
+                            //revert variables back to previous state if payment insert fails
+                            $sub_saved = !empty($sub_result) ? "true" : "false";
+                            if($sub_saved =='false')
+                            {
+                                    if ($revert == 'more') {
+                                            //rollback amount we have to pay
+                                            $extrapayment = $extrapayment + $curr_owing;
+                                            }
+                                    elseif ($revert == 'nomore') {
+                                            $extrapayment = $sub_payment->ac_amount;
+                                    }
+
+                            } else { //payment succesful, record data in array and note html
+                                $paid_to[$db->lastInsertId()] = $sub_payment->ac_amount;
+                                $inv_info .= sprintf($inv_info_format,$sub_payment->ac_inv_id,$sub_payment->ac_amount);
+
+                            }
+
+                    } //end while loop
+
+
+
+                    // return any unused funds to be paid to the original invoice
+                    $payment->ac_amount = $payment->ac_amount + $extrapayment;
                 
 	}
 
@@ -156,7 +147,7 @@ if ( isset($_POST['process_payment']) ) {
         foreach ($paid_to as $sub_pmt_id => $sub_amt)
         {
 
-        $paid_formatted = sprintf("<br /><br />This payment of %f was part of a larger payment of %f and has been automatically split over the following invoices:",$sub_amt,$orig_amt);
+        $paid_formatted = sprintf("<br /><br />%s %f %s %f %s",$LANG['payment_of'],$sub_amt,$LANG['payment_larger'],$orig_amt,$LANG['payment_split_over']);
         $paid_formatted .= $inv_info;
 
         $sql_notes = "UPDATE ".TB_PREFIX."payment SET ac_notes=concat(ac_notes,:extra_notes) WHERE id=:id;";
