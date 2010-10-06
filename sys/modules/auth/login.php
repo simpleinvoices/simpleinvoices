f91cc0d473d50a9a9a3287b1616490760476d348
Justin Kelly <justin@kelly.org.au>
2010-10-06 01:12:47 +0000

    version control - changed

--- sys/modules/auth/login.php
+++ sys/modules/auth/login.php
@@ -154,5 +154,4 @@ if($_POST['action'] == 'login' && (empty($_POST['user']) OR empty($_POST['pass']
 
 $smarty->assign("errorMessage",$errorMessage);
 $smarty->assign("value",$value);
-$smarty->assign("version_id", SI_VERSION_TITLE);
 ?>
