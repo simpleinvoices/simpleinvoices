e44959050864c5c431151f62dcfeafe00c59f2ac
Albert Drent <aducomsoftware@gmail.com>
2010-10-11 20:37:19 +0000

    fix path issue

--- sys/modules/tax_rates/add.php
+++ sys/modules/tax_rates/add.php
@@ -5,7 +5,7 @@ checkLogin();
 
 //if valid then do save
 if ($_POST['tax_description'] != "" ) { 
-	include("./modules/tax_rates/save.php");
+	include("sys/modules/tax_rates/save.php");
 }
 
 $types = getTaxTypes();
