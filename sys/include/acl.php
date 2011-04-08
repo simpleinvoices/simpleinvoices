28535e9493d0fb5ac0fa7047773891a4a5a3508a
Justin Kelly <justin@kelly.org.au>
2011-04-08 13:37:54 +0000

    minor acl work for customer login

--- sys/include/acl.php
+++ sys/include/acl.php
@@ -56,9 +56,11 @@ $acl->allow('guest', null, 'view');
 // everyone see auth page
 $acl->allow(null,'auth');
 $acl->allow(null,'api');
-//TODO: not good !!! - no acl for invoiecs as can't get html2pdf to work with zend_auth :(
-$acl->allow('administrator','invoices');
-$acl->allow('customer','invoices');      
+
+//Customer permissions
+$acl->allow('customer', 'invoices');      
+$acl->allow('customer', 'customers', 'view');
+$acl->allow('customer', 'export', 'invoice');
 
 //students only see student page
 $acl->allow('customer', 'customers', 'view');
@@ -79,7 +81,6 @@ $acl->allow('domain_administrator');
 
 // Administrator inherits nothing, but is allowed all privileges
 $acl->allow('administrator');
-$acl->allow('customer');
 //user - can do everything except anything in the Settings menu
 $acl->allow('user');
 $acl->deny('user','options');
