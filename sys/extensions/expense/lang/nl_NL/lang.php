0ae80b24d4a0830803af2227468b1ebe5d059299
Matt West <matt.west@developercity.co.uk>
2011-11-26 11:53:58 +0000

    Updated dutch translations

---
+++ sys/extensions/expense/lang/nl_NL/lang.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* 	Nederland - Dutch translation file
+*       Last update: 22-10-2011
+*
+* Authors:
+*       Bart Cockheyt (info@eumedia.nl)
+*
+* License:
+*	 GPL v3 or above
+*/
+
+/*// 1 means that the variable has been translated and // zero means it hasnt been translated - this is used by a script to calculate how much of each file has been done
+regex :%s/;/ /1/;// 1\/\/1/g - remove the spaces
+ */
+
+
+$LANG['expense'] = "Uigaven";//1
+$LANG['paid'] = "Betaald";//1
+$LANG['not_paid'] = "Openstaand";//1
+$LANG['expense_account'] = "Uitgaven rekening";//1
+$LANG['accounts'] = "Rekeningen";//1
+$LANG['save_expense_failure'] = "Er ging iets mis, Probeer nogmaals op te slaan<br />";//1
+$LANG['save_expense_success'] = "Opslaan gelukt, <br /> U wordt doorverwezen naar de uitgaven pagina";//1
+$LANG['add_new_expense'] = "Voeg nieuwe uitgave toe";//1
+$LANG['add_new_expense_account'] = "Voeg nieuwe rekening toe";//1
