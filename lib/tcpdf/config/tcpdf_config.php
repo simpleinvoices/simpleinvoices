1837ff889f9dd40ebdcd69a37a1e62e4c4616de3
Matt West <matt.west@developercity.co.uk>
2012-03-08 13:27:40 +0000

    Changed cache path of tcpdf to si cache folder

--- lib/tcpdf/config/tcpdf_config.php
+++ lib/tcpdf/config/tcpdf_config.php
@@ -91,12 +91,12 @@ if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
 	/**
 	 * cache directory for temporary files (full path)
 	 */
-	define ('K_PATH_CACHE', K_PATH_MAIN.'cache/');
+	define ('K_PATH_CACHE', K_PATH_MAIN.'../../tmp/cache/');
 
 	/**
 	 * cache directory for temporary files (url path)
 	 */
-	define ('K_PATH_URL_CACHE', K_PATH_URL.'cache/');
+	define ('K_PATH_URL_CACHE', K_PATH_URL.'../../tmp/cache/');
 
 	/**
 	 *images directory
