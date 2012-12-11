5025eb2d0f5acef60c5f8eae4821229d9bec9090
soif <wxopwx@gmail.com>
2012-12-11 10:07:17 +0100

    start removing all those details_screen css class, now replaced by TH tags

--- modules/invoices/quick_view.php
+++ modules/invoices/quick_view.php
@@ -53,7 +53,7 @@ else {
 $customFieldLabels = getCustomFieldLabels();
 
 for($i=1;$i<=4;$i++) {
-	$customField[$i] = show_custom_field("invoice_cf$i",$invoice["custom_field$i"],"read",'details_screen summary', 'details_screen','details_screen',5,':');
+	$customField[$i] = show_custom_field("invoice_cf$i",$invoice["custom_field$i"],"read",'summary', '','',5,':');
 }
 
 
