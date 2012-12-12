973efe4a9410971b767e0e5ff43ff3893efec5c3
Matt West <mattantwest@gmail.com>
2012-12-12 13:52:43 +0000

    Fixing Total by Aging Periods Report

--- modules/reports/report_debtors_aging_total.php
+++ modules/reports/report_debtors_aging_total.php
@@ -1,9 +1,6 @@
 <?php
-//   include phpreports library
-require_once("./include/reportlib.php");
-
    if ($db_server == 'pgsql') {
-      $sSQL = "SELECT
+      $sql = "SELECT
         sum(coalesce(ii.total, 0)) AS inv_total,
         sum(coalesce(ap.ac_amount, 0)) AS inv_paid,
 
@@ -31,40 +28,54 @@ ORDER BY
 	aging DESC;
 ";
    } else {
-      $sSQL = "SELECT
-        sum(coalesce(ii.total, 0)) AS inv_total,
-        sum(coalesce(ap.ac_amount, 0)) AS inv_paid,
+      $sql = "SELECT
+          sum(coalesce(ii.total, 0)) AS inv_total,
+          sum(coalesce(ap.ac_amount, 0)) AS inv_paid,
 
-        sum(coalesce(ii.total, 0)) -
-        sum(coalesce(ap.ac_amount, 0)) AS inv_owing,
+          sum(coalesce(ii.total, 0)) -
+          sum(coalesce(ap.ac_amount, 0)) AS inv_owing,
 
-        (CASE   WHEN datediff(now(),date) <= '14 days' THEN '0-14'
-                WHEN datediff(now(),date) <= '30 days' THEN '15-30'
-                WHEN datediff(now(),date) <= '60 days' THEN '31-60'
-                WHEN datediff(now(),date) <= '90 days' THEN '61-90'
-                ELSE '90+'
-        END) AS aging
+          (CASE   WHEN datediff(now(),date) <= '14 days' THEN '0-14'
+                  WHEN datediff(now(),date) <= '30 days' THEN '15-30'
+                  WHEN datediff(now(),date) <= '60 days' THEN '31-60'
+                  WHEN datediff(now(),date) <= '90 days' THEN '61-90'
+                  ELSE '90+'
+          END) AS aging
 
-FROM
-        ".TB_PREFIX."invoices iv LEFT JOIN
-        (SELECT i.invoice_id, sum(i.total) AS total
-         FROM ".TB_PREFIX."invoice_items i GROUP BY i.invoice_id
-        ) ii ON (iv.id = ii.invoice_id) LEFT JOIN
-        (SELECT p.ac_inv_id, sum(p.ac_amount) AS ac_amount
-         FROM ".TB_PREFIX."payment p GROUP BY p.ac_inv_id
-        ) ap ON (iv.id = ap.ac_inv_id)
-GROUP BY
-        aging
-ORDER BY
-	aging DESC;
-";
-}
+  FROM
+          ".TB_PREFIX."invoices iv LEFT JOIN
+          (SELECT i.invoice_id, sum(i.total) AS total
+           FROM ".TB_PREFIX."invoice_items i GROUP BY i.invoice_id
+          ) ii ON (iv.id = ii.invoice_id) LEFT JOIN
+          (SELECT p.ac_inv_id, sum(p.ac_amount) AS ac_amount
+           FROM ".TB_PREFIX."payment p GROUP BY p.ac_inv_id
+          ) ap ON (iv.id = ap.ac_inv_id)
+  GROUP BY
+          aging
+  ORDER BY
+  	aging DESC;
+  ";
+  }
 
-   $oRpt->setXML("./modules/reports/report_debtors_aging_total.xml");
+  $results = dbQuery($sql) or die(htmlsafe(end($dbh->errorInfo())));
 
-//   include phpreports run code
-	include("./include/reportrunlib.php");
+  $sum_total = 0;
+  $sum_paid = 0;
+  $sum_owing = 0;
+  $periods = array();
 
-$smarty -> assign('pageActive', 'report');
-$smarty -> assign('active_tab', '#home');
+  while($period = $results->fetch()) {
+    $sum_total += $period['inv_total'];
+    $sum_paid += $period['inv_paid'];
+    $sum_owing += $period['inv_owing'];
+    array_push($periods, $period);
+  }
+
+  $smarty -> assign('data', $periods);
+  $smarty -> assign('sum_total', $sum_total);
+  $smarty -> assign('sum_paid', $sum_paid);
+  $smarty -> assign('sum_owing', $sum_owing);
+
+  $smarty -> assign('pageActive', 'report');
+  $smarty -> assign('active_tab', '#home');
 ?>
--- templates/default/reports/report_debtors_aging_total.tpl
+++ templates/default/reports/report_debtors_aging_total.tpl
@@ -1,3 +1,33 @@
-<h3 class="si_report_title">Totals by Aging periods</h3>
+<h3 class="si_report_title">{$LANG.total_by_aging_periods}</h3>
 
-{include file=$path|cat:'inc_phpreport.tpl'}
+<table class="si_report_table">
+	<thead>
+		<tr>
+			<th colspan="4">{$LANG.total_by_aging_periods}</th>
+		</tr>
+		<tr>
+			<th>{$LANG.total}</th>
+			<th>{$LANG.paid}</th>
+			<th>{$LANG.owing}</th>
+			<th>{$LANG.aging}</th>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<td>{$sum_total|siLocal_number:'2'|default:'-'}</td>
+			<td>{$sum_paid|siLocal_number:'2'|default:'-'}</td>
+			<td>{$sum_owing|siLocal_number:'2'|default:'-'}</td>
+			<td></td>
+		</tr>
+	</tfoot>
+	<tbody>
+	{foreach item=period from=$data}
+		<tr>
+			<td>{$period.inv_total|siLocal_number:'2'|default:'-'}</td>
+			<td>{$period.inv_paid|siLocal_number:'2'|default:'-'}</td>
+			<td>{$period.inv_owing|siLocal_number:'2'|default:'-'}</td>
+			<td>{$period.aging}</td>
+		</tr>
+	{/foreach}
+	</tbody>
+</table>
