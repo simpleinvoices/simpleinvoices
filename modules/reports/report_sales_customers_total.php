4efacf9681dbfd7bac43a0a5d25bf6bb0a7ecf20
Matt West <mattantwest@gmail.com>
2012-12-12 10:28:31 +0000

    Fixing Total Sales by Customer Report

--- modules/reports/report_sales_customers_total.php
+++ modules/reports/report_sales_customers_total.php
@@ -1,30 +1,34 @@
 <?php 
-//   include phpreports library
-require_once("./include/reportlib.php");
 
-   $sSQL = "SELECT c.name, sum(ii.total) as sum_total
-      FROM 
-            ".TB_PREFIX."customers c, 
-            ".TB_PREFIX."invoices i,
-            ".TB_PREFIX."invoice_items ii, 
-            ".TB_PREFIX."preferences p
-    where
-        i.customer_id = c.id
-        AND
-        ii.invoice_id = i.id
-        AND 
-        i.preference_id = p.pref_id
-        AND
-            p.status = '1'
-      GROUP BY c.name 
+  $sql = "SELECT c.name, sum(ii.total) as sum_total
+          FROM 
+                ".TB_PREFIX."customers c, 
+                ".TB_PREFIX."invoices i,
+                ".TB_PREFIX."invoice_items ii, 
+                ".TB_PREFIX."preferences p
+          where
+            i.customer_id = c.id
+            AND
+            ii.invoice_id = i.id
+            AND 
+            i.preference_id = p.pref_id
+            AND
+                p.status = '1'
+          GROUP BY c.name";
 
-";
+  $customer_sales = dbQuery($sql) or die(htmlsafe(end($dbh->errorInfo())));
 
-   $oRpt->setXML("./modules/reports/report_sales_customers_total.xml");
+  $total_sales = 0;
+  $customers = array();
 
-//   include phpreports run code
-	include("./include/reportrunlib.php");
+  while($customer = $customer_sales->fetch()) {
+    $total_sales += $customer['sum_total'];
+    array_push($customers, $customer);
+  }
 
-$smarty -> assign('pageActive', 'report');
-$smarty -> assign('active_tab', '#home');
+  $smarty -> assign('data', $customers);
+  $smarty -> assign('total_sales', $total_sales);
+
+  $smarty -> assign('pageActive', 'report');
+  $smarty -> assign('active_tab', '#home');
 ?>
--- templates/default/reports/report_sales_customers_total.tpl
+++ templates/default/reports/report_sales_customers_total.tpl
@@ -1,4 +1,23 @@
-<h3>Sales in total by Customer</h3>
+<h3>{$LANG.total_sales_by_customer}</h3>
 <hr />
 
-{$showReport}
+<table class="si_report_table">
+	<thead>
+		<tr>
+			<th colspan="2">{$LANG.total_sales_by_customer}</th>
+		</tr>
+	</thead>
+	<tbody>
+	{foreach item=customer from=$data}
+		<tr class="tr_{cycle values="A,B"}">
+			<td>{$customer.name|htmlsafe}</td>
+			<td>{$customer.sum_total|siLocal_number:'2'|default:'-'}</td>
+		</tr>
+	{/foreach}
+
+		<tr>
+			<td align="RIGHT" class="PAGE_LAYER">{$LANG.total_sales}</td>
+			<td align="LEFT" class="PAGE_LAYER"><span class="BOLD">{$total_sales|siLocal_number:'2'|default:'-'}</span></td>
+		</tr>
+	</tbody>
+</table>
