ae6129fd839879e9993dbbc90a044344a4c97993
Ap.Muthu <apmuthu@usa.net>
2013-10-01 14:13:03 +0530

    nl_NL all missing strings added, other lang.php missing title strings added

--- lang/de_DE/lang.php
+++ lang/de_DE/lang.php
@@ -9,7 +9,7 @@
 * 	 Ali Akar [ali_akar@gmx.de]
 *
 * Last edited:
-* 	 2008-05-01
+* 	 2013-10-01
 *
 * License:
 *	 GPL v2 or above
@@ -20,8 +20,6 @@ regex :%s/;/ /1/;// 1\/\/1/g - remove the spaces
 */
 
 #all
-$title = "Rechnungsverwaltung";//1
-
 $LANG['about'] = "&Uuml;ber";//1
 $LANG['account_info'] = "Konto-Information";//1
 $LANG['actions'] = "Aktion";//1
@@ -440,4 +438,24 @@ $LANG['whats_this_page_about'] = "Wozu ist diese Seite gut";//1
 $LANG['wheres_the_edit_button'] = "Wo ist der edit Button";//1
 $LANG['yes'] = "Yes";//0
 $LANG['zip'] = "PLZ";//1
+
+$LANG['title_module_index']				="Home";//0
+$LANG['title_module_reports']			="Home / Reports";//0
+$LANG['title_module_invoices']			="Money / Invoices";//0
+$LANG['title_module_cron']				="Money / Recurence";//0
+$LANG['title_module_payments']			="Money / Payments";//0
+$LANG['title_module_billers']			="People / Billers";//0
+$LANG['title_module_customers']			="People / Customers";//0
+$LANG['title_module_user']				="People / Users";//0
+$LANG['title_module_products']			="Products";//0
+$LANG['title_module_product_attribute']	="Products / Product Attributes";//0
+$LANG['title_module_product_value']		="Products / Product Values";//0
+$LANG['title_module_options']			="Settings";//0
+$LANG['title_module_system_defaults']	="Settings / System Preferences";//0
+$LANG['title_module_custom_fields']		="Settings / Custom Fields";//0
+$LANG['title_module_tax_rates']			="Settings / Tax Rates";//0
+$LANG['title_module_preferences']		="Settings / Invoice Preferences";//0
+$LANG['title_module_payment_types']		="Settings / Payment Types";//0
+$LANG['title_view_index']				="Dashboard";//0
+
 ?>
--- lang/sk_SK/lang.php
+++ lang/sk_SK/lang.php
@@ -5,12 +5,10 @@
 *   Slovencina - Slovak translation file
 *
 * Authors:
-*  	Adastra-net s.r.o.
-*       Samuel Steinhubel, lnx@lnx.sk
+*  	Adastra-net s.r.o., Samuel Steinhubel, lnx@lnx.sk
 *
 * Last edited:
-*    2000-01-10
-*    2010-04-13
+*    2013-10-01
 *
 * License:
 *    GPL v2 or above
@@ -21,8 +19,6 @@ regex :%s/;/ /1/;// 1\/\/1/g - remove the spaces
  */
 
 #all
-$title = "Simple Invoices";//1
-
 $LANG['about'] = "O programe";//1
 $LANG['account_info'] = "Informácie o účte";//1
 $LANG['actions'] = "Akcia";//1
@@ -573,4 +569,24 @@ $LANG['years'] = "roky";//1
 $LANG['yes'] = "Áno";//1
 $LANG['your_reports'] = "Tvoje reporty";//1
 $LANG['zip'] = "PSČ";//1
+
+$LANG['title_module_index']				="Home";//0
+$LANG['title_module_reports']			="Home / Reports";//0
+$LANG['title_module_invoices']			="Money / Invoices";//0
+$LANG['title_module_cron']				="Money / Recurence";//0
+$LANG['title_module_payments']			="Money / Payments";//0
+$LANG['title_module_billers']			="People / Billers";//0
+$LANG['title_module_customers']			="People / Customers";//0
+$LANG['title_module_user']				="People / Users";//0
+$LANG['title_module_products']			="Products";//0
+$LANG['title_module_product_attribute']	="Products / Product Attributes";//0
+$LANG['title_module_product_value']		="Products / Product Values";//0
+$LANG['title_module_options']			="Settings";//0
+$LANG['title_module_system_defaults']	="Settings / System Preferences";//0
+$LANG['title_module_custom_fields']		="Settings / Custom Fields";//0
+$LANG['title_module_tax_rates']			="Settings / Tax Rates";//0
+$LANG['title_module_preferences']		="Settings / Invoice Preferences";//0
+$LANG['title_module_payment_types']		="Settings / Payment Types";//0
+$LANG['title_view_index']				="Dashboard";//0
+
 ?>
