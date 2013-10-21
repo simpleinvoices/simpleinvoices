d815e57b050a91c0ee178a5af48c8615e7911ff0
Ap.Muthu <apmuthu@usa.net>
2013-10-21 20:24:24 +0530

    Update define.php
    Typo in comments in config/define.php setting fixed

--- config/define.php
+++ config/define.php
@@ -21,7 +21,7 @@ define("LOGGING",true);
 This allows you to have another local config file for your dev or other purposes
 ie. dev.config.php 
 any config.php setting in this extra file(which wont be kept in svn) will overwrite config.php values
-- this way everyone can have there own conf setting without messing with anyones setting
+- this way everyone can have their own conf setting without messing with anyone else's setting
 RELEASE TODO: make sure $environment is set back to live
 */
 $environment = "production"; //test,staging,dev,live etc..
