80a89a2083e8b9f8aa6bfa6da1d28ee30057c5f8
Ap.Muthu <apmuthu@usa.net>
2013-10-26 19:47:30 +0530

    Regularised Patch 240 to accommodate state changes as it were then

--- include/sql_patches.php
+++ include/sql_patches.php
@@ -1423,8 +1423,13 @@ ADD `language` VARCHAR( 255 ) NULL ;";
     $patch['239']['date'] = "20100321";
 
     $patch['240']['name'] = "si_system_defaults - add composite primary key";
-    #$patch['240']['patch'] = "ALTER TABLE  `".TB_PREFIX."system_defaults` DROP INDEX `name`, DROP PRIMARY KEY, ADD PRIMARY KEY(`domain_id`, `id`)";
-    $patch['240']['patch'] = "select +1 from `".TB_PREFIX."system_defaults`";
+    $patch['240']['patch'] = "
+		ALTER TABLE  `".TB_PREFIX."system_defaults` ADD `new_id` INT( 11 ) NOT NULL FIRST; 
+		UPDATE `".TB_PREFIX."system_defaults` SET new_id = id; 
+		ALTER TABLE  `".TB_PREFIX."system_defaults` DROP  `id` ; 
+		ALTER TABLE  `".TB_PREFIX."system_defaults` DROP INDEX `name` ; 
+		ALTER TABLE  `".TB_PREFIX."system_defaults` CHANGE  `new_id`  `id` INT( 11 ) NOT NULL; 
+		ALTER TABLE  `".TB_PREFIX."system_defaults` ADD PRIMARY KEY(`domain_id`,`id` );";
     $patch['240']['date'] = "20100305";
 
     $patch['241']['name'] = "si_system_defaults - add composite primary key";
