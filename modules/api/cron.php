12aebb3a46340c18173ef070787fd7886d4fb92d
Ap.Muthu <apmuthu@usa.net>
2013-11-09 21:30:35 +0530

    Enabled multi-domain feature by removing hardcoded $domain_id=1

--- modules/api/cron.php
+++ modules/api/cron.php
@@ -1,9 +1,18 @@
 <?php
+/* 
+// Typical Cron Job to run each day:
+// #Simple Invoices recurrence - run each day at 1AM
+// 0 1 * * * /usr/bin/wget -q -O - http://localhost/api-cron  >/dev/null 2>&1
+//
+// Typical expansion of mod rewrite using the .htaccess file
+// api-cron => index.php?module=api&view=cron
+*/
 
 ini_set('max_execution_time', 600); //600 seconds = 10 minutes
 
 $cron = new cron();
-$cron->domain_id=1;
+// remove hardcoding for multi-domain usage
+// $cron->domain_id=1;
 $message = $cron->run();
 
 try 
