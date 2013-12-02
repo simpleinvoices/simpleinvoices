f9fcbc03d97f4a16768e9136353dc916ec3324f6
Ap.Muthu <apmuthu@usa.net>
2013-12-02 15:33:48 +0530

    Fixed variable in previous commit

--- include/check_permission.php
+++ include/check_permission.php
@@ -5,7 +5,7 @@ $auth_session = new Zend_Session_Namespace('Zend_Auth');
 //print_r($_SESSION);
 
 $acl_action = (isset($_GET['action']) ? $_GET['action'] : null);
-$checkPermission = $acl->isAllowed($auth_session->role_name, $module, $_GET['action']) ?  "allowed" : "denied"; // allowed
+$checkPermission = $acl->isAllowed($auth_session->role_name, $module, $acl_action ?  "allowed" : "denied"; // allowed
 
 //sbasic customer page check 
 if( ($auth_session->role_name =='customer') AND ($module == 'customers') AND ($_GET['id'] != $auth_session->user_id) )
