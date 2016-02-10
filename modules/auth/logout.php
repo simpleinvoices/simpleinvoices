7bd9394aa6958cbee8b9a3609296216eaf5ae012
Rich Rowley <fearless359@comcast.net>
2016-02-09 19:09:59 -0800

    Fix to define BROWSE constant only if not already defined.

--- modules/auth/logout.php
+++ modules/auth/logout.php
@@ -6,14 +6,11 @@
 * License:
 *	 GPL v3 or above
 */
-
 $menu = false;
 // we must never forget to start the session
 //so config.php works ok without using index.php define browse
-define("BROWSE","browse");
+if (!defined("BROWSE")) define("BROWSE", "browse");
 
-	Zend_Session::start();
-	Zend_Session::destroy(true);
-	header('Location: .');
-
-?>
+Zend_Session::start();
+Zend_Session::destroy(true);
+header('Location: .');
