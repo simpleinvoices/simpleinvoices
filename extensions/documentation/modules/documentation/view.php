8b7078adaabcb27fb34b43b13b282d606e984f05
Rich Rowley <fearless359@comcast.net>
2016-02-15 18:35:22 -0800

    Add support for field help via the "help" keyword.
    Example:  ..."index.php?module=documentation&amp;view=view&amp;help={$cflg.field_help}"

---
+++ extensions/documentation/modules/documentation/view.php
@@ -0,0 +1,12 @@
+<?php
+$menu = false;
+
+if (isset($_GET['help'])) {
+    $page = $_GET['help'];
+}
+else {
+    $get_page = $_GET['page'];
+    $page = isset($LANG[$get_page]) ? $LANG[$get_page] : $LANG['no_help_page'];
+}
+
+$smarty->assign("page", $page);
