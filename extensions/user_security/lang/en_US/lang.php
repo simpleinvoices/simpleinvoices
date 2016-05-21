8f5a0bf4317d6e38eda553728d7567406af983d9
Rich Rowley <fearless359@comcast.net>
2016-05-20 20:03:34 -0700

    user_security extension change
    Made default value of  simple_invoices be SimpleInvoices.

--- extensions/user_security/lang/en_US/lang.php
+++ extensions/user_security/lang/en_US/lang.php
@@ -4,6 +4,7 @@ $LCL_LANG = array(
   'help_session_timeout' => "This field, specified in minutes, is the inactive session timeout period. The period is renewed each
                              time the user submits an entry.",
   'session_timeout' => "Session Timeout",
+  'simple_invoices' => "SimpleInvoices"
 );
 // @formatter:on
 $LANG = array_merge($LANG,$LCL_LANG);
