1a7cd830534dd8bb28f5e91209a6f472479cffc3
Rich Rowley <fearless359@comcast.net>
2016-07-25 16:44:14 -0700

    Change logging default to false.

--- config/define.php
+++ config/define.php
@@ -2,24 +2,24 @@
 
 define("TB_PREFIX","si_"); // default table prefix - old var $tb_prefix = "si_";
 
-define("ENABLED","1"); // 
-define("DISABLED","0"); // 
+define("ENABLED","1"); //
+define("DISABLED","0"); //
 
 //invoice styles
-define("total_invoice","1"); // 
-define("itemised_invoice","2"); // 
-define("consulting_invoice","3"); // 
+define("total_invoice","1"); //
+define("itemised_invoice","2"); //
+define("consulting_invoice","3"); //
 
 //To turn logging on set the below to true - not needed as it is set in System Defaults
-#define("LOGGING",false);
-define("LOGGING",true);
+define("LOGGING",false);
+//define("LOGGING",true);
 
 ####################
 /* Environment*/
 ####################
 /*
 This allows you to have another local config file for your dev or other purposes
-ie. dev.config.php 
+ie. dev.config.php
 any config.php setting in this extra file(which wont be kept in svn) will overwrite config.php values
 - this way everyone can have their own conf setting without messing with anyone else's setting
 RELEASE TODO: make sure $environment is set back to live
