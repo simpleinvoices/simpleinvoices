20d934f46e4260cc0fbb4be07829d4e6ca4c8dc5
fearless359 <Rich@FearlessTechComp.com>
2018-09-07 10:25:01 -0700

    Fixed to create custom_flags table on first access after being enabled.

--- extensions/custom_flags/include/init.php
+++ extensions/custom_flags/include/init.php
@@ -1,2 +1,4 @@
 <?php
 include_once './extensions/custom_flags/include/sql_queries.php';
+include_once './extensions/custom_flags/include/class/CreateCustomFlagsTable.php';
+CreateCustomFlagsTable::createTable();
