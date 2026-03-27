70768b0072e25426033a09e9847c15bf303e1195
Justin Kelly <justin@kelly.org.au>
2026-03-27 14:18:31 +1100

    new UI update - update languages

--- lang/en_US/lang.php
+++ lang/en_US/lang.php
@@ -103,7 +103,6 @@ $LANG['customer'] = "Customer";//1
 $LANG['customer_account'] = "Customer Account";//1
 $LANG['customer_add'] = "Add Customer";//1
 $LANG['customer_contact'] = "Customer Contact (Attn)";//1
-$LANG['contacts'] = "Contacts";//0
 $LANG['contacts'] = "Contacts";//1
 $LANG['customer_department'] = "Department";//1
 $LANG['customer_details'] = "Customer details";//1
