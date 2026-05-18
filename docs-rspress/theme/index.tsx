3f564439860e232fa6d5765af4e9c83c32eabcea
Justin Kelly <justin@kelly.org.au>
2026-05-18 22:13:19 +1000

    docs update

---
+++ docs-rspress/theme/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HomeLayout as BasicHomeLayout, getCustomMDXComponent } from '@rspress/core/theme-original';
+import { Content } from '@rspress/core/runtime';
+import { MDXProvider } from '@mdx-js/react';
+
+export function HomeLayout(props: any) {
+  const mdxComponents = getCustomMDXComponent();
+  return (
+    <BasicHomeLayout
+      {...props}
+      afterHeroActions={
+        <>
+          {props.afterHeroActions}
+          <MDXProvider components={mdxComponents}>
+            <Content />
+          </MDXProvider>
+        </>
+      }
+    />
+  );
+}
+
+export * from '@rspress/core/theme-original';
